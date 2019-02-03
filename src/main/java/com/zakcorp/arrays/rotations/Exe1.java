44b2d5af508714c03c6d5f18410fd461a37dd748
zakirkone <zakir@kloudone.com>
2019-02-03 17:00:39 +0530

    Initial Commit-2

---
+++ src/main/java/com/zakcorp/arrays/rotations/Exe1.java
@@ -0,0 +1,11 @@
+package com.zakcorp.arrays.rotations;
+
+public class Exe1 {
+  public static void main(String[] args) {
+    int[] arr = {1,2,3,4,5,6,7};
+    int k = 2;
+    for (int i=0;i<arr.length;i++) {
+      System.out.println(arr[i + 1]);
+    }
+  }
+}
