31a8b7c3aed5ee3909295e71d5e180eb00deaa9c
zakirhussainb <zakirhussainb2693@gmail.com>
2019-12-29 22:15:08 +0530

    Added few more problems

--- src/main/java/com/zakcorp/linkedlists/doubly/DoublyLLProblems.java
+++ src/main/java/com/zakcorp/linkedlists/doubly/DoublyLLProblems.java
@@ -5,13 +5,13 @@ import java.util.*;
 public class DoublyLLProblems {
     public static void main(String[] args) {
         DoublyLLIntImpl dll = new DoublyLLIntImpl();
-        dll.add(1);
-        dll.add(2);
-        dll.add(4);
-        dll.add(5);
         dll.add(6);
         dll.add(8);
         dll.add(9);
+        dll.add(5);
+        dll.add(1);
+        dll.add(3);
+        dll.add(2);
 //        dll.printListCustom(dll.head);
 //        dll.addFirst(59);
         dll.printListCustom(dll.head);
@@ -26,19 +26,82 @@ public class DoublyLLProblems {
 //        DoublyLLIntImpl.Node head = dllProblems.reverseDoublyLinkedList(dll.head);
 //        dll.printListCustom(head);
 
-//        System.out.println("tail..." + dll.tail.data);
-//        dllProblems.quickSort(dll.head, dll.tail);
-//        dll.printListCustom(dll.head);
+        dllProblems.quickSort(dll.head, dll.tail);
+        dll.printListCustom(dll.head);
 
 
-        HashSet<HashSet<Integer>> pairList = findPairsWithGivenSum(dll.head, 7);
-        System.out.println(pairList.toString());
+//        HashSet<HashSet<Integer>> pairList = findPairsWithGivenSum_1(dll.head, 7);
+//        System.out.println(pairList.toString());
+
+//        HashSet<HashSet<Integer>> pairList = findPairsWithGivenSum_2(dll.head, 7);
+//        System.out.println(pairList.toString());
+
+//        HashSet<HashSet<Integer>> pairSet = findPairsWithGivenSum_3(dll.head, dll.tail, 7);
+//        System.out.println(pairSet.toString());
+
+        insertInASortedDLL(dll.head, 3);
+        DoublyLLIntImpl.Node head = insertInASortedDLL(dll.head, 7);
+        dll.printListCustom(head);
+
 
     }
 
-    private static HashSet<HashSet<Integer>> findPairsWithGivenSum(DoublyLLIntImpl.Node head, int x) {
-        /* This implementation uses extra space -> O(n) by storing the given node.data into a hashSet
-        * Time Complexity:- O(n)*/
+    private static DoublyLLIntImpl.Node insertInASortedDLL(DoublyLLIntImpl.Node head, int x) {
+        // https://www.geeksforgeeks.org/insert-value-sorted-way-sorted-doubly-linked-list/
+        /* Time Complexity:- O(n)
+         * Space Complexity:- O(1) */
+        DoublyLLIntImpl.Node curr = head;
+        DoublyLLIntImpl.Node follow = null;
+        DoublyLLIntImpl.Node t = new DoublyLLIntImpl.Node(x);
+        while (curr != null){
+            if(curr.data >= x){
+                if(follow == null){
+                    t.next = curr;
+                    curr.prev = t;
+                    return t;
+                }
+                t.next = follow.next;
+                curr.prev = t;
+                follow.next = t;
+                t.prev = follow;
+                break;
+            }
+            follow = curr;
+            curr = curr.next;
+        }
+        return head;
+    }
+
+    private static HashSet<HashSet<Integer>> findPairsWithGivenSum_3(DoublyLLIntImpl.Node head,
+                                                                     DoublyLLIntImpl.Node tail, int x) {
+        /* This doubly linked list is in sorted condition so we can use the 2-pointer method
+        * Time Complexity:- O(n)
+        * Space Complexity:- O(1) */
+        HashSet<HashSet<Integer>> pairSet = new HashSet<>();
+        DoublyLLIntImpl.Node first = head;
+        DoublyLLIntImpl.Node last = tail;
+        while (first != null && last != null && first != last && last.next != first) {
+            HashSet<Integer> set = new HashSet<>();
+            if(first.data + last.data < x){
+                first = first.next;
+            } else if(first.data + last.data > x){
+                last = last.prev;
+            } else {
+                set.add(first.data);
+                set.add(last.data);
+                pairSet.add(set);
+                first = first.next;
+                last = last.prev;
+            }
+        }
+        return pairSet;
+    }
+
+    private static HashSet<HashSet<Integer>> findPairsWithGivenSum_1(DoublyLLIntImpl.Node head, int x) {
+        // https://www.geeksforgeeks.org/find-pairs-given-sum-doubly-linked-list/
+        /* This implementation uses extra space by storing the given node.data into a hashSet
+        * Time Complexity:- O(n)
+        * Space Complexity:- O(n) */
         DoublyLLIntImpl.Node curr = head;
         HashSet<Integer> store = new HashSet<>();
         while (curr != null){
@@ -60,7 +123,31 @@ public class DoublyLLProblems {
         return pairSet;
     }
 
+
+    private static HashSet<HashSet<Integer>> findPairsWithGivenSum_2(DoublyLLIntImpl.Node head, int x) {
+        /* Time Complexity:- O(n^2)
+         * Space Complexity:- O(1) */
+        HashSet<HashSet<Integer>> pairSet = new HashSet<>();
+        DoublyLLIntImpl.Node p = head;
+        while(p != null){
+            HashSet<Integer> hSet = new HashSet<>();
+            DoublyLLIntImpl.Node q = p.next;
+            while (q != null){
+                if(x == p.data + q.data){
+                    hSet.add(p.data);
+                    hSet.add(q.data);
+                    pairSet.add(hSet);
+                }
+                q = q.next;
+            }
+            p = p.next;
+        }
+        return pairSet;
+    }
+
+
     private void quickSort(DoublyLLIntImpl.Node start, DoublyLLIntImpl.Node end) {
+        /* https://www.geeksforgeeks.org/quicksort-for-linked-list/ */
         if(end != null && start != end && start != end.next){
             DoublyLLIntImpl.Node pi = qsPartition(start, end);
             quickSort(start, pi.prev);
