ce24fec959dc7a82add0921a7a0172ff15eea3f9
user.email <zakirhussainb2693@gmail.com>
2020-07-10 22:08:39 +0530

    LeetCode Daily

--- src/main/java/com/zakcorp/leetcodemaster/Problem_1299.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_1299.java
@@ -1,15 +1,27 @@
 package com.zakcorp.leetcodemaster;
 
 public class Problem_1299 {
-    public int[] replaceElements(int[] arr) {
-        int n = arr.length;
-        int[] res = new int[n];
-        int max = Integer.MIN_VALUE;
-        res[n - 1] = -1;
-        for(int i = n - 1; i > 0; i--) {
-            max = Math.max(max, arr[i]);
-            res[i - 1] = max;
+    static class Solver {
+        public int[] solve1(int[] arr) {
+            int n = arr.length;
+            int[] res = new int[n];
+            int max = Integer.MIN_VALUE;
+            res[n - 1] = -1;
+            for(int i = n - 1; i > 0; i--) {
+                max = Math.max(max, arr[i]);
+                res[i - 1] = max;
+            }
+            return res;
         }
-        return res;
+        public int[] solve2(int[] arr) {
+            int n = arr.length;
+            int max = -1;
+            for(int i = n - 1; i >= 0; --i) {
+                int temp = arr[i];
+                arr[i] = max;
+                max = Math.max(max, temp);
+            }
+            return arr;
+        }
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_1299_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_1299_Test.java
@@ -6,11 +6,13 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class Problem_1299_Test {
-    Problem_1299 p = new Problem_1299();
+    Problem_1299.Solver p = new Problem_1299.Solver();
     @Test
     public void test1() {
-        assertArrayEquals(new int[]{18,6,6,6,1,-1}, p.replaceElements(new int[]{17,18,5,4,6,1}));
+//        assertArrayEquals(new int[]{18,6,6,6,1,-1}, p.solve1(new int[]{17,18,5,4,6,1}));
+        assertArrayEquals(new int[]{18,6,6,6,1,-1}, p.solve2(new int[]{17,18,5,4,6,1}));
     }
+
     /* All these passed
      * [1]
      * [100000]
