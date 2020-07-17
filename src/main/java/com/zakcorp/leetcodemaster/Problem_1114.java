7b5f22afe1d420082bcdf0e4ee0224d66cf5490b
user.email <zakirhussainb2693@gmail.com>
2020-07-17 23:31:05 +0530

    LeetCode Dailty

---
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_1114.java
@@ -0,0 +1,32 @@
+package com.zakcorp.leetcodemaster;
+
+import java.util.concurrent.Semaphore;
+
+public class Problem_1114  {
+    private Semaphore run2;
+    private Semaphore run3;
+    public Problem_1114() {
+        run2 = new Semaphore(0);
+        run3 = new Semaphore(0);
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        run2.release();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        run2.acquire();
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        run3.release();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        run3.acquire();
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+    }
+}
