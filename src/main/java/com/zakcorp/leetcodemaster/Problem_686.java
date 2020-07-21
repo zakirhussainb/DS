15f848262880326256537c50ce5f597f1d651a72
user.email <zakirhussainb2693@gmail.com>
2020-07-21 14:46:03 +0530

    LeetCode Daily

--- src/main/java/com/zakcorp/leetcodemaster/Problem_686.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_686.java
@@ -3,23 +3,39 @@ package com.zakcorp.leetcodemaster;
 import java.util.*;
 
 public class Problem_686 {
-    public int repeatedStringMatch(String A, String B) {
-        int result = 1;
-        if(A.contains(B)) {
-            return result;
-        }
-        int m = A.length() + B.length();
-        String str = A;
-        for(int i = 1; i <= m; i++) {
-            result++;
-            str += A;
-            if(str.contains(B)) {
+    static class Solver {
+        public int solve1(String A, String B) {
+            int result = 1;
+            if(A.contains(B)) {
                 return result;
             }
-            if(str.length() > m) {
-                break;
+            int m = A.length() + B.length();
+            String str = A;
+            for(int i = 1; i <= m; i++) {
+                result++;
+                str += A;
+                if(str.contains(B)) {
+                    return result;
+                }
+                if(str.length() > m) {
+                    break;
+                }
             }
+            return -1;
         }
-        return -1;
+        public int solve2(String A, String B) {
+            int result = 1;
+            StringBuilder sb = new StringBuilder(A);
+            for(; sb.length() < B.length(); result++) {
+                sb.append(A);
+            }
+            if(sb.indexOf(B) >= 0) {
+                return result;
+            }
+            if(sb.append(A).indexOf(B) >= 0){
+                return result+1;
+            }
+            return -1;
+        }
     }
 }
