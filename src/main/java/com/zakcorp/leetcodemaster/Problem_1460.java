0dff1284123957ea7d79f0569b8b352d6c241730
user.email <zakirhussainb2693@gmail.com>
2020-07-25 21:10:26 +0530

    LeetCode Daily

--- src/main/java/com/zakcorp/leetcodemaster/Problem_1460.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_1460.java
@@ -20,5 +20,34 @@ public class Problem_1460 {
             }
             return map.size() == 0;
         }
+        public boolean solve2(int[] target, int[] arr) {
+            // Time and Space Complexities:- O(n) and O(n)
+            // Based on the constraints given in the problem.
+            int[] countArr = new int[1001];
+            for(int i = 0; i < target.length; i++) {
+                countArr[target[i]]++;
+                countArr[arr[i]]--;
+            }
+            for(int i = 0; i < 1001; i++) {
+                if(countArr[i] != 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public boolean wrongSolution(int[] target, int[] arr) {
+            // TODO:- This is failing for the testcase 7.
+            // Time and Space Complexities:- O(n) and O(1)
+            // We just need to check whether two arrays are equal.
+            // So take their sum and check.
+            int targetSum = 0, arrSum = 0;
+            for(int num : target) {
+                targetSum += num;
+            }
+            for(int num : arr) {
+                arrSum += num;
+            }
+            return targetSum == arrSum;
+        }
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_1460_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_1460_Test.java
@@ -5,29 +5,48 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class Problem_1460_Test {
-    Problem_1460 p = new Problem_1460();
+    Problem_1460.Solver p = new Problem_1460.Solver();
     @Test
     public void test1() {
-        assertTrue(p.canBeEqual(new int[]{1, 2, 3, 4}, new int[]{2, 4, 1, 3}));
+        assertTrue(p.solve1(new int[]{1, 2, 3, 4}, new int[]{2, 4, 1, 3}));
+        assertTrue(p.solve2(new int[]{1, 2, 3, 4}, new int[]{2, 4, 1, 3}));
+        assertTrue(p.wrongSolution(new int[]{1, 2, 3, 4}, new int[]{2, 4, 1, 3}));
     }
     @Test
     public void test2() {
-        assertTrue(p.canBeEqual(new int[]{7}, new int[]{7}));
+        assertTrue(p.solve1(new int[]{7}, new int[]{7}));
+        assertTrue(p.solve2(new int[]{7}, new int[]{7}));
+        assertTrue(p.wrongSolution(new int[]{7}, new int[]{7}));
     }
     @Test
     public void test3() {
-        assertTrue(p.canBeEqual(new int[]{1, 12}, new int[]{12, 1}));
+        assertTrue(p.solve1(new int[]{1, 12}, new int[]{12, 1}));
+        assertTrue(p.solve2(new int[]{1, 12}, new int[]{12, 1}));
+        assertTrue(p.wrongSolution(new int[]{1, 12}, new int[]{12, 1}));
     }
     @Test
     public void test4() {
-        assertFalse(p.canBeEqual(new int[]{3,7,9}, new int[]{3,7,11}));
+        assertFalse(p.solve1(new int[]{3,7,9}, new int[]{3,7,11}));
+        assertFalse(p.solve2(new int[]{3,7,9}, new int[]{3,7,11}));
+        assertFalse(p.wrongSolution(new int[]{3,7,9}, new int[]{3,7,11}));
     }
     @Test
     public void test5() {
-        assertTrue(p.canBeEqual(new int[]{1,1,1,1,1}, new int[]{1,1,1,1,1}));
+        assertTrue(p.solve1(new int[]{1,1,1,1,1}, new int[]{1,1,1,1,1}));
+        assertTrue(p.solve2(new int[]{1,1,1,1,1}, new int[]{1,1,1,1,1}));
+        assertTrue(p.wrongSolution(new int[]{1,1,1,1,1}, new int[]{1,1,1,1,1}));
     }
     @Test
     public void test6() {
-        assertFalse(p.canBeEqual(new int[]{1,2,2,3}, new int[]{1,1,2,3}));
+        assertFalse(p.solve1(new int[]{1,2,2,3}, new int[]{1,1,2,3}));
+        assertFalse(p.solve2(new int[]{1,2,2,3}, new int[]{1,1,2,3}));
+        assertFalse(p.wrongSolution(new int[]{1,2,2,3}, new int[]{1,1,2,3}));
+    }
+    @Test
+    public void test7() {
+        assertFalse(p.solve1(new int[]{1, 2, 3}, new int[]{2,2,2}));
+        assertFalse(p.solve2(new int[]{1, 2, 3}, new int[]{2,2,2}));
+        // Here it will fail.
+        assertFalse(p.wrongSolution(new int[]{1, 2, 3}, new int[]{2,2,2}));
     }
 }
