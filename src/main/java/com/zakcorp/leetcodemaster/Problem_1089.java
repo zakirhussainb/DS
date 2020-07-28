893295b07b35b4b1ce9a11db0188082f73382a0e
user.email <zakirhussainb2693@gmail.com>
2020-07-28 23:06:52 +0530

    LeetCode Daily

--- src/main/java/com/zakcorp/leetcodemaster/Problem_1089.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_1089.java
@@ -4,25 +4,39 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Problem_1089 {
-    public int[] duplicateZeros(int[] arr) {
-        int n = arr.length;
-        int m = n - 1;
-        int i = 0, j = 1;
-        while(i < n) {
-            if(arr[i] == 0) {
-                while(m > j) {
-                    arr[m] = arr[m - 1];
-                    m--;
+    static class Solver {
+        public int[] solve1(int[] arr) {
+            int n = arr.length;
+            int m = n - 1;
+            int i = 0, j = 1;
+            while(i < n) {
+                if(arr[i] == 0) {
+                    while(m > j) {
+                        arr[m] = arr[m - 1];
+                        m--;
+                    }
+                    arr[m] = 0;
+                    i = i + 2;
+                    j = i + 1;
+                    m = n - 1;
+                } else {
+                    i = j;
+                    j++;
                 }
-                arr[m] = 0;
-                i = i + 2;
-                j = i + 1;
-                m = n - 1;
-            } else {
-                i = j;
-                j++;
             }
+            return arr;
         }
-        return arr;
+        public int[] solve2(int[] arr) {
+            int n = arr.length;
+            for(int i = 0; i < n; i++) {
+                if(arr[i] == 0) {
+                    for(int j = n - 1; j > i; j--) {
+                        arr[j] = arr[ j - 1];
+                    }
+                    i++;
+                }
+            }
+            return arr;
+        }
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_1089_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_1089_Test.java
@@ -6,17 +6,20 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class Problem_1089_Test {
-    Problem_1089 p = new Problem_1089();
+    Problem_1089.Solver p = new Problem_1089.Solver();
     @Test
     public void test1() {
-        assertArrayEquals(new int[]{1, 0, 0, 2, 3, 0, 0, 4}, p.duplicateZeros(new int[]{1,0,2,3,0,4,5,0}));
+        assertArrayEquals(new int[]{1, 0, 0, 2, 3, 0, 0, 4}, p.solve1(new int[]{1,0,2,3,0,4,5,0}));
+        assertArrayEquals(new int[]{1, 0, 0, 2, 3, 0, 0, 4}, p.solve2(new int[]{1,0,2,3,0,4,5,0}));
     }
     @Test
     public void test2() {
-        assertArrayEquals(new int[]{1, 2, 3}, p.duplicateZeros(new int[]{1, 2, 3}));
+        assertArrayEquals(new int[]{1, 2, 3}, p.solve1(new int[]{1, 2, 3}));
+        assertArrayEquals(new int[]{1, 2, 3}, p.solve2(new int[]{1, 2, 3}));
     }
     @Test
     public void test3() {
-        assertArrayEquals(new int[]{1, 2, 3}, p.duplicateZeros(new int[]{1, 2, 3}));
+        assertArrayEquals(new int[]{1, 2, 3}, p.solve1(new int[]{1, 2, 3}));
+        assertArrayEquals(new int[]{1, 2, 3}, p.solve2(new int[]{1, 2, 3}));
     }
 }
