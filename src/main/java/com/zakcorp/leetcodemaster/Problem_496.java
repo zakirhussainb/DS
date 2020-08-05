57c15508e7130926e132ff79a822245b6ad4228f
user.email <zakirhussainb2693@gmail.com>
2020-08-06 01:40:07 +0530

    LeetCode Daily

--- src/main/java/com/zakcorp/leetcodemaster/Problem_496.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_496.java
@@ -3,23 +3,36 @@ package com.zakcorp.leetcodemaster;
 import java.util.*;
 
 public class Problem_496 {
-    public int[] nextGreaterElement(int[] arr1, int[] arr2) {
-        int m = arr1.length;
-        int n = arr2.length;
-        int[] result = new int[m];
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; j++) {
-                if(arr2[j] > arr2[i]) {
-                    map.put(arr2[i], arr2[j]);
-                    break;
+    static class Solver {
+        public int[] solve1(int[] arr1, int[] arr2) {
+            Map<Integer, Integer> map = new HashMap<>();
+            for(int i = 0; i < arr2.length; i++) {
+                for(int j = i + 1; j < arr2.length; j++) {
+                    if(arr2[j] > arr2[i]) {
+                        map.put(arr2[i], arr2[j]);
+                        break;
+                    }
                 }
             }
+            for (int i = 0; i < arr1.length; i++) {
+                arr1[i] = map.getOrDefault(arr1[i], -1);
+            }
+            return arr1;
         }
-        int k = 0;
-        for (int num : arr1) {
-            result[k++] = map.getOrDefault(num, -1);
+
+        public int[] solve2(int[] arr1, int[] arr2) {
+            Stack<Integer> stack = new Stack<>();
+            Map<Integer, Integer> map = new HashMap<>();
+            for(int num : arr2) {
+                while( !stack.isEmpty() && stack.peek() < num) {
+                    map.put(stack.pop(), num);
+                }
+                stack.push(num);
+            }
+            for(int i = 0; i < arr1.length; i++) {
+                arr1[i] = map.getOrDefault(arr1[i], -1);
+            }
+            return arr1;
         }
-        return result;
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_496_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_496_Test.java
@@ -5,20 +5,33 @@ import org.junit.Test;
 import static org.junit.Assert.assertArrayEquals;
 
 public class Problem_496_Test {
-    Problem_496 p = new Problem_496();
+    Problem_496.Solver p = new Problem_496.Solver();
     @Test
     public void test1() {
-        assertArrayEquals(new int[]{-1, 3, -1}, p.nextGreaterElement(new int[]{4, 1, 2},
+        assertArrayEquals(new int[]{-1, 3, -1}, p.solve1(new int[]{4, 1, 2},
+                new int[]{1, 3, 4, 2}));
+        assertArrayEquals(new int[]{-1, 3, -1}, p.solve2(new int[]{4, 1, 2},
                 new int[]{1, 3, 4, 2}));
     }
     @Test
     public void test2() {
-        assertArrayEquals(new int[]{3, -1}, p.nextGreaterElement(new int[]{2, 4},
+        assertArrayEquals(new int[]{3, -1}, p.solve1(new int[]{2, 4},
+                new int[]{1, 2, 3, 4}));
+        assertArrayEquals(new int[]{3, -1}, p.solve2(new int[]{2, 4},
                 new int[]{1, 2, 3, 4}));
     }
     @Test
     public void test3() {
-        assertArrayEquals(new int[]{5, 5, 5, -1}, p.nextGreaterElement(new int[]{4,2,3,5},
+        assertArrayEquals(new int[]{5, 5, 5, -1}, p.solve1(new int[]{4,2,3,5},
                 new int[]{4,3,2,1,5}));
+        assertArrayEquals(new int[]{5, 5, 5, -1}, p.solve2(new int[]{4,2,3,5},
+                new int[]{4,3,2,1,5}));
+    }
+    @Test
+    public void test4() {
+        assertArrayEquals(new int[]{6,6,6,-1}, p.solve1(new int[]{3,2,1,7},
+                new int[]{9, 8, 7, 3, 2, 1, 6}));
+        assertArrayEquals(new int[]{6,6,6,-1}, p.solve2(new int[]{3,2,1,7},
+                new int[]{9, 8, 7, 3, 2, 1, 6}));
     }
 }
