337bd4be886b0d589b32ed5800aef1d6e34c5d88
user.email <zakirhussainb2693@gmail.com>
2020-08-12 01:05:59 +0530

    LeetCode Daily

--- src/main/java/com/zakcorp/leetcodemaster/Problem_849.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_849.java
@@ -1,21 +1,48 @@
 package com.zakcorp.leetcodemaster;
 
 
+import java.util.*;
+
 public class Problem_849 {
-    public int maxDistToClosest(int[] arr) {
-        int n = arr.length;
-        int max = 1;
-        for(int i = 0; i < n; i++) {
-            if(arr[i] == 0) {
-                if(i > 0 && i < n - 1) {
-                    if(arr[i - 1] == 0 && arr[i + 1] == 0) {
-                        max = Math.max(max, i);
-                    }
+    static class Solver {
+        public int solve1(int[] arr) {
+            int n = arr.length;
+            int max = 0;
+            int i = 0;
+            while(i < n) {
+                while(i < n && arr[i] == 1) {
+                    i++;
+                }
+                int j = i;
+                while(i < n && arr[i] == 0) {
+                    i++;
+                }
+                if(j == 0 || i == n) {
+                    max = Math.max(max, i - j);
                 } else {
-                    max = Math.max(max, i);
+                    max = Math.max(max, (i - j + 1) / 2 );
                 }
             }
+            return max;
         }
-        return max;
+        public int solve2(int[] arr) {
+            TreeSet<Integer> tSet = new TreeSet<>();
+            int max = 0;
+            int n = arr.length;
+            for(int i = 0; i < n; i++) {
+                if(arr[i] == 1)
+                    tSet.add(i);
+            }
+            for(int i = 0; i < n; i++) {
+                if(arr[i] == 1)
+                    continue;
+                Integer leftSeat = tSet.lower(i);
+                Integer rightSeat = tSet.higher(i);
+                int left = leftSeat == null ? n : i - leftSeat;
+                int right = rightSeat == null ? n : rightSeat - i;
+                max = Math.max(max, Math.min(left, right));
+            }
+            return max;
+        }
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_849_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_849_Test.java
@@ -6,25 +6,30 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class Problem_849_Test {
-    Problem_849 p = new Problem_849();
+    Problem_849.Solver p = new Problem_849.Solver();
     @Test
     public void test1() {
-        assertEquals(2, p.maxDistToClosest(new int[]{1,0,0,0,1,0,1}));
+        assertEquals(2, p.solve1(new int[]{1,0,0,0,1,0,1}));
+        assertEquals(2, p.solve2(new int[]{1,0,0,0,1,0,1}));
     }
     @Test
     public void test2() {
-        assertEquals(3, p.maxDistToClosest(new int[]{1,0,0,0}));
+        assertEquals(3, p.solve1(new int[]{1,0,0,0}));
+        assertEquals(3, p.solve2(new int[]{1,0,0,0}));
     }
     @Test
     public void test3() {
-        assertEquals(1, p.maxDistToClosest(new int[]{0,1,0,0,1}));
+        assertEquals(1, p.solve1(new int[]{0,1,0,0,1}));
+        assertEquals(1, p.solve2(new int[]{0,1,0,0,1}));
     }
     @Test
     public void test4() {
-        assertEquals(1, p.maxDistToClosest(new int[]{0,1}));
+        assertEquals(1, p.solve1(new int[]{0,1}));
+        assertEquals(1, p.solve2(new int[]{0,1}));
     }
     @Test
     public void test5() {
-        assertEquals(3 , p.maxDistToClosest(new int[]{1,0,0,0,0,0,0,1}));
+        assertEquals(3 , p.solve1(new int[]{1,0,0,0,0,0,0,1}));
+        assertEquals(3 , p.solve2(new int[]{1,0,0,0,0,0,0,1}));
     }
 }
