0056df664208d01942cf0a60d15bbc48370ddab5
user.email <zakirhussainb2693@gmail.com>
2020-08-21 11:24:23 +0530

    Graph Daily

--- src/main/java/com/zakcorp/graphs/DijkstraAlgorithm.java
+++ src/main/java/com/zakcorp/graphs/DijkstraAlgorithm.java
@@ -10,53 +10,61 @@ import java.util.*;
  * @sub-topic:
  * @platform:
  * @problem_link:
+ * @pseudocode: DijkstraAlgorithm using PriorityQueue
+ * dist[] // result array of minimum distance, output
+ * PriorityQueue<> pq // To maintain vertices
+ * settled<set> // mark nodes that are already settled/relaxed
+ * dijkstra(src):
+ *      for all u in V:
+ *          dist(u) = Infinity
+ *      pq.add(src, 0)
+ *      dist[src] = 0
+ *      while(settled.size != V)
+ *         u = pq.remove()
+ *         settled.add(u)
+ *         explore(u)
+ *
+ *  explore(u):
+ *      for all v in u:
+ *          if( settled does not contain "v" )
+ *              dist[v] = min(dist[v], dist[u] + length(u, v))
+ *              pq.add(v)
  */
 public class DijkstraAlgorithm {
     private Graph g;
-    public int dist[];
-    private Set<Integer> settled;
+    public int[] dist;
+    private Set<Integer> settledNodes;
     private PriorityQueue<Vertex> pq;
     public DijkstraAlgorithm(Graph g) {
         this.g = g;
         dist = new int[g.vertex];
-        settled = new HashSet<>();
+        settledNodes = new HashSet<>();
         pq = new PriorityQueue<>(g.vertex, new Vertex());
     }
     public void findShortestPath(int src) {
-        for (int i = 0; i < g.vertex; i++)
+        for (int i = 0; i < g.vertex; i++) {
             dist[i] = Integer.MAX_VALUE;
+        }
         pq.add(new Vertex(src, 0));
         dist[src] = 0;
-        while( settled.size() != g.vertex ) {
-            // remove the minimum distance node
-            // from the priority queue
+        while( settledNodes.size() != g.vertex ) {
+            // remove a node from the PQ (The node with minimum distance will be removed)
             int u = pq.remove().node;
-
-            // adding the node whose distance is
-            // finalized
-            settled.add(u);
-
+            // adding the node whose distance is finalized
+            settledNodes.add(u);
+            // explore "u"
             exploreNeighbours(u);
         }
     }
 
     private void exploreNeighbours(int u) {
-        int edgeDistance = -1;
-        int newDistance = -1;
-
-        // All the neighbors of u
-        for (int i = 0; i < g.adjListArrayWithWeights[u].size(); i++) {
-            Vertex v = g.adjListArrayWithWeights[u].get(i);
-
-            // If current node hasn't already been processed
-            if ( !settled.contains(v.node) ) {
-                edgeDistance = v.cost;
-                newDistance = dist[u] + edgeDistance;
-
-                // If new distance is cheaper in cost
-                dist[v.node] = Math.min(dist[v.node], newDistance);
-
-                // Add the current node to the queue
+        // All the neighbors of "u"
+        for(Vertex v : g.adjListArrayWithWeights[u]) {
+            // If current node of the neighbour "v" hasn't already been settled/relaxed
+            if ( !settledNodes.contains(v.node) ) {
+                // If the new distance is cheaper in cost, update the distance
+                dist[v.node] = Math.min(dist[v.node], dist[u] + v.cost);
+                // Add the current node to the PQ.
                 pq.add(new Vertex(v.node, dist[v.node]));
             }
         }
@@ -65,20 +73,13 @@ public class DijkstraAlgorithm {
 class Vertex implements Comparator<Vertex> {
     public int node;
     public int cost;
-
-    public Vertex()
-    {
-    }
-
-    public Vertex(int node, int cost)
-    {
+    public Vertex() { }
+    public Vertex(int node, int cost) {
         this.node = node;
         this.cost = cost;
     }
-
     @Override
-    public int compare(Vertex node1, Vertex node2)
-    {
+    public int compare(Vertex node1, Vertex node2) {
         return Integer.compare(node1.cost, node2.cost);
     }
 }
