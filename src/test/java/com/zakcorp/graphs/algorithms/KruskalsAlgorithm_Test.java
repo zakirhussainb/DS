2d1d2cc65ba880e9a24512d204af6b997980e1ec
user.email <zakirhussainb2693@gmail.com>
2020-08-26 19:25:50 +0530

    Kruskals Algo Tests

--- src/test/java/com/zakcorp/graphs/algorithms/KruskalsAlgorithm_Test.java
+++ src/test/java/com/zakcorp/graphs/algorithms/KruskalsAlgorithm_Test.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 
 public class KruskalsAlgorithm_Test {
     @Test
-    public void testKruskalsMST() {
+    public void testKruskalsMST_NaiveImpl() {
         KruskalsAlgorithm.Graph g = new KruskalsAlgorithm.Graph(4);
         g.addEdge(0, 1, 10);
         g.addEdge(0, 2, 6);
@@ -13,14 +13,23 @@ public class KruskalsAlgorithm_Test {
         g.addEdge(1, 3, 15);
         g.addEdge(2, 3, 4);
         g.edgeList.sort(Comparator.comparingInt(e -> e.weight));
-//        KruskalsAlgorithm.EfficientImpl krslEff = new KruskalsAlgorithm.EfficientImpl(g);
-//        for( KruskalsAlgorithm.Graph.Edge edge : krsl.kruskalsMST() ) {
-//            System.out.println(edge.u + "->" + edge.v + "=" + edge.weight);
-//        }
-//        System.out.println();
-
         KruskalsAlgorithm.NaiveImpl krslNaive = new KruskalsAlgorithm.NaiveImpl(g);
         for( KruskalsAlgorithm.Graph.Edge edge : krslNaive.kruskalsMST() ) {
+            System.out.println(edge.u + "->" + edge.v + "=" + edge.weight);
+        }
+        System.out.println();
+    }
+    @Test
+    public void testKruskalsMST_EfficientImpl() {
+        KruskalsAlgorithm.Graph g = new KruskalsAlgorithm.Graph(4);
+        g.addEdge(0, 1, 10);
+        g.addEdge(0, 2, 6);
+        g.addEdge(0, 3, 5);
+        g.addEdge(1, 3, 15);
+        g.addEdge(2, 3, 4);
+        g.edgeList.sort(Comparator.comparingInt(e -> e.weight));
+        KruskalsAlgorithm.EfficientImpl krslEff = new KruskalsAlgorithm.EfficientImpl(g);
+        for( KruskalsAlgorithm.Graph.Edge edge : krslEff.kruskalsMST() ) {
             System.out.println(edge.u + "->" + edge.v + "=" + edge.weight);
         }
         System.out.println();
