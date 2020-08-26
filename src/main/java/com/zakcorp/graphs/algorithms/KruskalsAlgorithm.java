95f9a45832249bf3909435fcab9fad8d83167f2a
user.email <zakirhussainb2693@gmail.com>
2020-08-26 09:17:32 +0530

    Kruskals Algorithm - 2

--- src/main/java/com/zakcorp/graphs/algorithms/KruskalsAlgorithm.java
+++ src/main/java/com/zakcorp/graphs/algorithms/KruskalsAlgorithm.java
@@ -1,5 +1,9 @@
 package com.zakcorp.graphs.algorithms;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * Created by Zakir Hussain B on 26/08/20.
  *
@@ -11,5 +15,43 @@ package com.zakcorp.graphs.algorithms;
  * @pseudocode:
  */
 public class KruskalsAlgorithm {
+
+    static class NaiveImpl {
+
+    }
+
+    static class EfficientImpl {
+        int[] parent;
+        int[] rank;
+        Graph g;
+        static class Graph {
+            int V;
+            List<Edge> edgeList;
+            static class Edge implements Comparator<Edge> {
+                int u;
+                int v;
+                int weight;
+                public Edge(int u, int v, int weight) {
+                    this.u = u;
+                    this.v = v;
+                    this.weight = weight;
+                }
+                @Override
+                public int compare(Edge e1, Edge e2) {
+                    return Integer.compare(e1.weight, e2.weight);
+                }
+            }
+            public Graph(int V) {
+                this.V = V;
+                edgeList = new ArrayList<>();
+            }
+        }
+        public EfficientImpl(Graph g) {
+            this.g = g;
+            parent = new int[g.V];
+            rank = new int[g.V];
+        }
+
+    }
 
 }
