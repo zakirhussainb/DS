24cde80c6c37b4be4b0dc4ddcff1d64d92e16a3f
user.email <zakirhussainb2693@gmail.com>
2020-08-26 08:42:18 +0530

    Kruskals Algorithm - 1

---
+++ src/main/java/com/zakcorp/graphs/algorithms/KruskalsAlgorithm.java
@@ -0,0 +1,15 @@
+package com.zakcorp.graphs.algorithms;
+
+/**
+ * Created by Zakir Hussain B on 26/08/20.
+ *
+ * @source:
+ * @topic:
+ * @sub-topic:
+ * @platform:
+ * @problem_link:
+ * @pseudocode:
+ */
+public class KruskalsAlgorithm {
+
+}
