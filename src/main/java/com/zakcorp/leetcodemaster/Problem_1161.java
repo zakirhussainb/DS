7e12f67e5fad12381840bccab5e44ab8042221b9
user.email <zakirhussainb2693@gmail.com>
2020-08-28 17:38:18 +0530

    LC - Graph Practice - 1161 - Two methods

--- src/main/java/com/zakcorp/leetcodemaster/Problem_1161.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_1161.java
@@ -17,46 +17,71 @@ public class Problem_1161 {
             this.right = right;
         }
     }
-    static class LevelSum {
-        private int level;
-        private int sum;
-        public LevelSum(int level, int sum) {
-            this.level = level;
-            this.sum = sum;
+
+    /**
+     * Using BreadthFirst Search and Maintaining a list to store the maxSum.
+     */
+    static class Solver {
+        public int maxLevelSum(TreeNode root) {
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+            List<Integer> resultList = new ArrayList<>();
+            while(!queue.isEmpty()) {
+                int size = queue.size();
+                int sum = 0;
+                TreeNode t;
+                while(size --> 0) {
+                    t = queue.poll();
+                    if(t.left != null) {
+                        queue.add(t.left);
+                    }
+                    if(t.right != null) {
+                        queue.add(t.right);
+                    }
+                    sum += t.val;
+                }
+                resultList.add(sum);
+            }
+            int maxSum = Integer.MIN_VALUE;
+            int index = -1;
+            for(int i = 0; i < resultList.size(); i++) {
+                if(resultList.get(i) > maxSum) {
+                    maxSum = resultList.get(i);
+                    index = i + 1;
+                }
+            }
+            return index;
         }
     }
-    public int maxLevelSum(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        int level = 0;
-        List<LevelSum> list = new ArrayList<>();
-        while(!queue.isEmpty()) {
-            int size = queue.size();
-            LevelSum levelSum;
-            int sum = 0;
-            TreeNode t;
-            while(size --> 0) {
-                t = queue.poll();
-                if(t.left != null) {
-                    queue.add(t.left);
+    /**
+     * Using DepthFirst Search(PreOrder) and Maintaining a list to store the maxSum.
+     */
+    static class Solver1 {
+        public int maxLevelSum(TreeNode root) {
+            List<Integer> resultList = new ArrayList<>();
+            dfs(root, 1, resultList);
+            int maxSum = Integer.MIN_VALUE;
+            int index = -1;
+            for(int i = 0; i < resultList.size(); i++) {
+                if(resultList.get(i) > maxSum) {
+                    maxSum = resultList.get(i);
+                    index = i + 1;
                 }
-                if(t.right != null) {
-                    queue.add(t.right);
-                }
-                sum += t.val;
             }
-            level++;
-            levelSum = new LevelSum(level, sum);
-            list.add(levelSum);
+            return index;
         }
-        int maxSum = Integer.MIN_VALUE;
-        int index = -1;
-        for (LevelSum levelSum : list) {
-            if (levelSum.sum > maxSum) {
-                maxSum = levelSum.sum;
-                index = levelSum.level;
+        public void dfs(TreeNode root, int level, List<Integer> resultList){
+            if(root == null) {
+                return;
             }
+            if(resultList.size() < level) {
+                resultList.add(root.val);
+            } else {
+                resultList.set(level - 1, resultList.get(level - 1) + root.val);
+            }
+            dfs(root.left, level+1, resultList);
+            dfs(root.right, level+1, resultList);
         }
-        return index;
+
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_1161_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_1161_Test.java
@@ -12,7 +12,8 @@ public class Problem_1161_Test {
      * [1,0,1]
      * [1,1,1,0,1,1,0]
      */
-    Problem_1161 p = new Problem_1161();
+    Problem_1161.Solver p = new Problem_1161.Solver();
+    Problem_1161.Solver1 p1 = new Problem_1161.Solver1();
     @Test
     public void test1() {
         Problem_1161.TreeNode treeNode = new Problem_1161.TreeNode(1);
@@ -21,6 +22,7 @@ public class Problem_1161_Test {
         treeNode.left.left = new Problem_1161.TreeNode(7);
         treeNode.left.right = new Problem_1161.TreeNode(-8);
         assertEquals(2, p.maxLevelSum(treeNode));
+        assertEquals(2, p1.maxLevelSum(treeNode));
     }
     @Test
     public void test2() {
@@ -28,5 +30,6 @@ public class Problem_1161_Test {
         treeNode.left = new Problem_1161.TreeNode(1);
         treeNode.right = new Problem_1161.TreeNode(1);
         assertEquals(1, p.maxLevelSum(treeNode));
+        assertEquals(1, p1.maxLevelSum(treeNode));
     }
 }
