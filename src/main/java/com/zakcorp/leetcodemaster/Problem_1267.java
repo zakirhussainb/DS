6000008a0f073c74fb9bd0cead729b6250fbac41
user.email <zakirhussainb2693@gmail.com>
2020-08-30 22:56:51 +0530

    LC - 1267 - 2

--- src/main/java/com/zakcorp/leetcodemaster/Problem_1267.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_1267.java
@@ -3,12 +3,16 @@ package com.zakcorp.leetcodemaster;
 /**
  * Created by Zakir Hussain B on 30/08/20.
  *
- * @source:
- * @topic:
- * @sub-topic:
- * @platform:
- * @problem_link:
+ * @source: LeetCode
+ * @topic: Graphs
+ * @sub-topic: Graphs
+ * @platform: LeetCode
+ * @problem_link: https://leetcode.com/problems/count-servers-that-communicate
  * @pseudocode:
+ *  1. Calculate all the servers present in a row
+ *  2. Calculate all the servers present in a column
+ *  3. Traverse the grid to calculate the no. of servers connected, if there are more than 1 servers present in that
+ *  row or column.
  */
 public class Problem_1267 {
     public int countServers(int[][] grid) {
--- src/test/java/com/zakcorp/leetcodemaster/Problem_1267_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_1267_Test.java
@@ -2,9 +2,13 @@ package com.zakcorp.leetcodemaster;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
+/**
+ * [[1,0],[0,1]]
+ * [[1,0],[1,1]]
+ * [[1,1,0,0],[0,0,1,0],[0,0,1,0],[0,0,0,1]]
+ */
 public class Problem_1267_Test {
     Problem_1267 p = new Problem_1267();
     @Test
