c9269ece71b8a3570bcc9e74069e6f67e9fe794c
user.email <zakirhussainb2693@gmail.com>
2020-08-30 18:46:32 +0530

    LC - 1306 - 5

--- src/main/java/com/zakcorp/leetcodemaster/Problem_1306.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_1306.java
@@ -13,70 +13,89 @@ import java.util.*;
  * @pseudocode:
  */
 public class Problem_1306 {
-    static class Solver {
-        static class Graph{
-            private int V;
-            private LinkedList<Integer>[] adjListArr;
-            public Graph(int V){
-                this.V = V;
-                adjListArr = new LinkedList[V];
-                for(int i = 0; i < V; i++) {
-                    adjListArr[i] = new LinkedList<>();
-                }
-            }
-            public void addEdge(int u, int v){
-                adjListArr[u].add(v);
-                adjListArr[v].add(u);
-            }
-        }
-        public boolean canReach(int[] arr, int start) {
-            if(start == 0) {
-                return true;
-            }
-            int V = arr.length;
-            Graph g = new Graph(V);
-//            int dest = -1;
-//            for(int i = 0; i < V; i++) {
-//                if(arr[i] == 0) {
-//                    if(start == arr[i]) {
-//                        return true;
-//                    } else {
-//                        dest = i;
-//                    }
-//                }
-//            }
-//            if(start == dest) {
-//                return true;
-//            }
+    // To remember my initial thought process and the mistakes I made
+    /*static class Graph{
+        private int V;
+        private LinkedList<Integer>[] adjListArr;
+        public Graph(int V){
+            this.V = V;
+            adjListArr = new LinkedList[V];
             for(int i = 0; i < V; i++) {
-                if(i + arr[i] < V) {
-                    g.addEdge(i, i + arr[i]);
-                }
-                if(i - arr[i] > -1) {
-                    g.addEdge(i, i - arr[i]);
-                }
+                adjListArr[i] = new LinkedList<>();
             }
-            return bfs(start, g, arr);
         }
-        public boolean bfs(int start, Graph g, int[] arr) {
-            boolean[] visited = new boolean[g.V];
-            Queue<Integer> queue = new LinkedList<>();
-            queue.add(start);
-            visited[start] = true;
-            while(!queue.isEmpty()) {
-                int s = queue.poll();
-                for(int i : g.adjListArr[s]) {
-                    if(!visited[i]) {
-                        visited[i] = true;
-                        queue.add(i);
-                        if(arr[i] == 0){
-                            return true;
-                        }
+        public void addEdge(int u, int v){
+            adjListArr[u].add(v);
+        }
+    }
+    public boolean canReach(int[] arr, int start) {
+        if(start == 0) {
+            return true;
+        }
+        int V = arr.length;
+        Graph g = new Graph(V);
+        for(int i = 0; i < V; i++) {
+            if(i + arr[i] < V) {
+                g.addEdge(i, i + arr[i]);
+            }
+            if(i - arr[i] > -1) {
+                g.addEdge(i, i - arr[i]);
+            }
+        }
+        return bfs(start, g, arr);
+    }
+    public boolean bfs(int start, Graph g, int[] arr) {
+        boolean[] visited = new boolean[g.V];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        visited[start] = true;
+        while(!queue.isEmpty()) {
+            int s = queue.poll();
+            for(int i : g.adjListArr[s]) {
+                if(!visited[i]) {
+                    visited[i] = true;
+                    queue.add(i);
+                    if(arr[i] == 0){
+                        return true;
                     }
                 }
             }
+        }
+        return false;
+    }*/
+    static class Solver {
+        public boolean canReach(int[] arr, int start) {
+            int n = arr.length;
+            Queue<Integer> queue = new LinkedList<>();
+            queue.add(start);
+            while( !queue.isEmpty() ) {
+                int s = queue.poll();
+                if(arr[s] == 0) {
+                    return true;
+                }
+                if(arr[s] < 0) {
+                    continue;
+                }
+                if(s + arr[s] < n) {
+                    queue.add(s + arr[s]);
+                }
+                if(s - arr[s] > -1 ) {
+                    queue.add(s - arr[s]);
+                }
+                arr[s] = -arr[s];
+            }
             return false;
         }
-
+    }
+    static class Solver1 {
+        public boolean canReach(int[] arr, int start) {
+            if(start >= 0 && start < arr.length && arr[start] >= 0) {
+                if(arr[start] == 0)
+                    return true;
+                arr[start] = -arr[start];
+                return canReach(arr, start + arr[start]) || canReach(arr, start - arr[start]);
+            }
+            return false;
+        }
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_1306_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_1306_Test.java
@@ -10,21 +10,26 @@ import static org.junit.Assert.*;
  */
 public class Problem_1306_Test {
     Problem_1306.Solver p = new Problem_1306.Solver();
+    Problem_1306.Solver1 p1 = new Problem_1306.Solver1();
     @Test
     public void test1() {
         assertTrue(p.canReach(new int[]{4,2,3,0,3,1,2}, 5));
+        assertTrue(p1.canReach(new int[]{4,2,3,0,3,1,2}, 5));
     }
     @Test
     public void test2() {
         assertTrue(p.canReach(new int[]{4,2,3,0,3,1,2}, 0));
+        assertTrue(p1.canReach(new int[]{4,2,3,0,3,1,2}, 0));
     }
     @Test
     public void test3() {
         assertFalse(p.canReach(new int[]{3,0,2,1,2}, 2));
+        assertFalse(p1.canReach(new int[]{3,0,2,1,2}, 2));
     }
     @Test
     public void test4() {
         assertTrue(p.canReach(new int[]{0,0}, 0));
+        assertTrue(p1.canReach(new int[]{0,0}, 0));
     }
     @Test
     public void test5() {
