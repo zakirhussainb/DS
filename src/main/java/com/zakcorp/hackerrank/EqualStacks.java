d901e2525cc7db7e5c80a2dc7f478fff0107a93e
user.email <zakirhussainb2693@gmail.com>
2020-09-10 00:59:32 +0530

    HackerRank Daily

---
+++ src/main/java/com/zakcorp/hackerrank/EqualStacks.java
@@ -0,0 +1,83 @@
+package com.zakcorp.hackerrank;
+
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class EqualStacks {
+
+    /*
+     * Complete the equalStacks function below.
+     */
+    static int equalStacks(int[] h1, int[] h2, int[] h3) {
+        int sum1 = Arrays.stream(h1).sum();
+        int sum2 = Arrays.stream(h2).sum();
+        int sum3 = Arrays.stream(h3).sum();
+        int i = 0, j = 0, k = 0;
+        while( !(sum1 == sum2 && sum2 == sum3) ) {
+            if(sum1 >= sum2 && sum1 >= sum3) {
+                sum1 = sum1 - h1[i];
+                i++;
+            } else if(sum2 >= sum1 && sum2 >= sum3) {
+                sum2 = sum2 - h2[j];
+                j++;
+            } else {
+                sum3 = sum3 - h3[k];
+                k++;
+            }
+        }
+        return sum1;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] n1N2N3 = scanner.nextLine().split(" ");
+
+        int n1 = Integer.parseInt(n1N2N3[0].trim());
+
+        int n2 = Integer.parseInt(n1N2N3[1].trim());
+
+        int n3 = Integer.parseInt(n1N2N3[2].trim());
+
+        int[] h1 = new int[n1];
+
+        String[] h1Items = scanner.nextLine().split(" ");
+
+        for (int h1Itr = 0; h1Itr < n1; h1Itr++) {
+            int h1Item = Integer.parseInt(h1Items[h1Itr].trim());
+            h1[h1Itr] = h1Item;
+        }
+
+        int[] h2 = new int[n2];
+
+        String[] h2Items = scanner.nextLine().split(" ");
+
+        for (int h2Itr = 0; h2Itr < n2; h2Itr++) {
+            int h2Item = Integer.parseInt(h2Items[h2Itr].trim());
+            h2[h2Itr] = h2Item;
+        }
+
+        int[] h3 = new int[n3];
+
+        String[] h3Items = scanner.nextLine().split(" ");
+
+        for (int h3Itr = 0; h3Itr < n3; h3Itr++) {
+            int h3Item = Integer.parseInt(h3Items[h3Itr].trim());
+            h3[h3Itr] = h3Item;
+        }
+
+        int result = equalStacks(h1, h2, h3);
+        System.out.println("result...." + result);
+
+//        bufferedWriter.write(String.valueOf(result));
+//        bufferedWriter.newLine();
+
+//        bufferedWriter.close();
+    }
+}
+
