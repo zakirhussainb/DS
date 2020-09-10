86f03a4c4efe7fd9bd9983e49f25177e6ca45f06
user.email <zakirhussainb2693@gmail.com>
2020-09-10 06:08:50 +0530

    Grokking Coding - Chapter_5

---
+++ src/main/java/com/zakcorp/grokking/coding/slidingwindow/Chapter_5.java
@@ -0,0 +1,23 @@
+package com.zakcorp.grokking.coding.slidingwindow;
+
+/**
+ * Created by Zakir Hussain B on 10/09/20.
+ *
+ * @source: Grokking Coding
+ * @topic: Sliding Window
+ * @sub-topic: Fruits into Baskets
+ * @platform: LeetCode
+ * @problem_link: https://leetcode.com/problems/fruit-into-baskets/
+ * @pseudocode:
+ */
+public class Chapter_5 {
+    /**
+     * Please refer Problem_904 FROM leetcodemaster
+     *
+     * This problem is similar to finding the Longest substring with K distinct characters, except here you need
+     * to find it for two distinct characters
+     *
+     * com/zakcorp/leetcodemaster/Problem_904.java
+     */
+
+}
