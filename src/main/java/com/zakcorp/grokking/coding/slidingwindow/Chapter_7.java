347aa7f78c1b353123d089daaee712cabb70f7ef
user.email <zakirhussainb2693@gmail.com>
2020-09-13 16:40:20 +0530

    Grokking Coding - Chapter 7 - 2

--- src/main/java/com/zakcorp/grokking/coding/slidingwindow/Chapter_7.java
+++ src/main/java/com/zakcorp/grokking/coding/slidingwindow/Chapter_7.java
@@ -14,12 +14,29 @@ import java.io.*;
  * @pseudocode:
  */
 public class Chapter_7 {
+    public static void main(String[] args) {
+        Chapter_7 ch7 = new Chapter_7();
+        System.out.println(ch7.findLength("ABAB", 2));
+        System.out.println(ch7.findLength("aabccbb", 2));
+        System.out.println(ch7.findLength("abbcb", 1));
+        System.out.println(ch7.findLength("abccde", 1));
+    }
     public int findLength(String str, int K) {
         int n = str.length();
-        int start = 0, maxLength = 0;
+        int start = 0, maxRepeatLetterCount = 0, maxLength = 0;
+        Map<Character, Integer> charFreqMap = new HashMap<>();
         for(int end = 0; end < n; end++) {
-            char startCh = str.charAt(start);
-            char endCh = str.charAt(end);
+            char endChar = str.charAt(end);
+            charFreqMap.put(endChar, charFreqMap.getOrDefault(endChar, 0) + 1);
+            maxRepeatLetterCount = Math.max(maxRepeatLetterCount, charFreqMap.get(endChar));
+
+            int charactersToChange = (end - start + 1) - maxRepeatLetterCount;
+            if(charactersToChange > K) {
+                char startChar = str.charAt(start);
+                charFreqMap.put(startChar, charFreqMap.get(startChar) - 1);
+                start++;
+            }
+            maxLength = Math.max(maxLength, end - start + 1);
         }
         return maxLength;
     }
