8174d20b255068cee39f0e0215984c4ac3c905ee
user.email <zakirhussainb2693@gmail.com>
2020-09-13 14:51:57 +0530

    Grokking Coding - Chapter 7 - 1

---
+++ src/main/java/com/zakcorp/grokking/coding/slidingwindow/Chapter_7.java
@@ -0,0 +1,26 @@
+package com.zakcorp.grokking.coding.slidingwindow;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * Created by Zakir Hussain B on 13/09/20.
+ *
+ * @source:
+ * @topic:
+ * @sub-topic:
+ * @platform:
+ * @problem_link:
+ * @pseudocode:
+ */
+public class Chapter_7 {
+    public int findLength(String str, int K) {
+        int n = str.length();
+        int start = 0, maxLength = 0;
+        for(int end = 0; end < n; end++) {
+            char startCh = str.charAt(start);
+            char endCh = str.charAt(end);
+        }
+        return maxLength;
+    }
+}
