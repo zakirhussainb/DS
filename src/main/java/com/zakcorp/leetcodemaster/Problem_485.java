6b11f03cd290c113499fa913d8171f86615b6a06
user.email <zakirhussainb2693@gmail.com>
2020-09-14 09:32:40 +0530

    LeetCode Daily - Problem 485 - 2

--- src/main/java/com/zakcorp/leetcodemaster/Problem_485.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_485.java
@@ -13,7 +13,16 @@ package com.zakcorp.leetcodemaster;
 public class Problem_485 {
     static class Solver {
         public int solve1(int[] arr) {
-            return 0;
+            int n = arr.length;
+            int i = 0, maxOnes = 0;
+            for(int j = 0; j < n; j++) {
+                if(arr[i] == arr[j] && arr[j] == 1) {
+                    maxOnes = Math.max(maxOnes, j - i + 1);
+                } else {
+                    i = j;
+                }
+            }
+            return maxOnes;
         }
     }
 }
