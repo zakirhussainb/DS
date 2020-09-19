4f8e30d9d6e079d74121c421da8a182451f64004
user.email <zakirhussainb2693@gmail.com>
2020-09-20 00:42:19 +0530

    Hackerrank Daily - Drawing Book 1

---
+++ src/main/java/com/zakcorp/hackerrank/DrawingBook.java
@@ -0,0 +1,70 @@
+package com.zakcorp.hackerrank;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * Created by Zakir Hussain B on 19/09/20.
+ *
+ * @source:
+ * @topic:
+ * @sub-topic:
+ * @platform:
+ * @problem_link:
+ * @pseudocode:
+ */
+public class DrawingBook {
+    static class Pair {
+        int first;
+        int second;
+        public Pair(int first, int second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
+    /*
+     * Complete the pageCount function below.
+     */
+    static int pageCount(int n, int p) {
+        int minTurns = 0;
+        List<Pair> arrList = new ArrayList<>();
+        arrList.add(new Pair(1, 1));
+        for(int i = 2; i <= n; i+=2) {
+            if(i + 1 > n) {
+                arrList.add(new Pair(i, i));
+            } else {
+                arrList.add(new Pair(i, i + 1));
+            }
+        }
+        int x = 0, y = arrList.size() - 1;
+        int fromStart = 0, fromLast = 0, turns = 0;
+        while(x < arrList.size() && y > 0) {
+            if(arrList.get(x).first == p || arrList.get(x).second == p) {
+                fromStart = turns;
+            }
+            if(arrList.get(y).first == p || arrList.get(y).second == p) {
+                fromLast = turns;
+            }
+            turns++;
+            x++;
+            y--;
+            minTurns = Math.min(fromStart, fromLast);
+        }
+        return minTurns;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])*");
+
+        int p = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])*");
+
+        int result = pageCount(n, p);
+        System.out.println(result);
+        scanner.close();
+    }
+}
