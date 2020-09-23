451742ecec74d36c4a87fad6e710ac75af449197
user.email <zakirhussainb2693@gmail.com>
2020-09-23 08:33:25 +0530

    LeetCode Problem 26 - New Test Cases Added

--- src/test/java/com/zakcorp/leetcodemaster/Problem_26_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_26_Test.java
@@ -15,4 +15,12 @@ public class Problem_26_Test {
     public void test2() {
         assertEquals(5, p.removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4}));
     }
+    @Test
+    public void test3() {
+        assertEquals(2, p.removeDuplicates(new int[]{2, 2, 2, 11}));
+    }
+    @Test
+    public void test4() {
+        assertEquals(4, p.removeDuplicates(new int[]{2, 3, 3, 3, 6, 9, 9}));
+    }
 }
