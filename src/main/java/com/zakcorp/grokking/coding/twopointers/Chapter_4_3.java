54e48dc4a0e31d3bf95c80e1663cf7551503363d
user.email <zakirhussainb2693@gmail.com>
2020-09-23 13:16:29 +0530

    Grokking Coding Chapter 4

---
+++ src/main/java/com/zakcorp/grokking/coding/twopointers/Chapter_4_3.java
@@ -0,0 +1,43 @@
+package com.zakcorp.grokking.coding.twopointers;
+
+/**
+ * Created by Zakir Hussain B on 23/09/20.
+ *
+ * @source:
+ * @topic:
+ * @sub-topic:
+ * @platform:
+ * @problem_link:
+ * @pseudocode:
+ */
+public class Chapter_4_3 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+    public ListNode findCycleStart(ListNode head) {
+        if(head == null)
+            return null;
+        ListNode slow = head, fast = head;
+        boolean cycleDetected = false;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) {
+                cycleDetected = true;
+                break;
+            }
+        }
+        if(cycleDetected) {
+            slow = head;
+            while(slow != fast) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+        }
+        return slow;
+    }
+}
--- src/test/java/com/zakcorp/leetcodemaster/Problem_141_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_141_Test.java
@@ -1,6 +1,6 @@
 package com.zakcorp.leetcodemaster;
 
-import com.zakcorp.grokking.coding.twopointers.Chapter_4_2;
+import com.zakcorp.grokking.coding.twopointers.Chapter_4_2_1;
 import com.zakcorp.linkedlists.singly.SinglyLLIntImpl;
 import org.junit.Test;
 
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertTrue;
 
 public class Problem_141_Test {
     Problem_141.Solver p = new Problem_141.Solver();
-    Chapter_4_2 p1 = new Chapter_4_2();
+    Chapter_4_2_1 p1 = new Chapter_4_2_1();
     @Test
     public void test1() {
         SinglyLLIntImpl.Node head = new SinglyLLIntImpl.Node(1);
@@ -22,12 +22,12 @@ public class Problem_141_Test {
     }
     @Test
     public void test2() {
-        Chapter_4_2.ListNode head = new Chapter_4_2.ListNode(1);
-        head.next = new Chapter_4_2.ListNode(2);
-        head.next.next = new Chapter_4_2.ListNode(3);
-        head.next.next.next = new Chapter_4_2.ListNode(4);
-        head.next.next.next.next = new Chapter_4_2.ListNode(5);
-        head.next.next.next.next.next = new Chapter_4_2.ListNode(6);
+        Chapter_4_2_1.ListNode head = new Chapter_4_2_1.ListNode(1);
+        head.next = new Chapter_4_2_1.ListNode(2);
+        head.next.next = new Chapter_4_2_1.ListNode(3);
+        head.next.next.next = new Chapter_4_2_1.ListNode(4);
+        head.next.next.next.next = new Chapter_4_2_1.ListNode(5);
+        head.next.next.next.next.next = new Chapter_4_2_1.ListNode(6);
         head.next.next.next.next.next.next = head.next.next;
         assertTrue(p1.hasCycle(head));
     }
