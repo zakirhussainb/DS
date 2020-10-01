cc97a728dc5fcfd3359ae5b7bf933a0f6efbe272
user.email <zakirhussainb2693@gmail.com>
2020-10-01 09:45:23 +0530

    Grokking Coding - Chapter 7 - 3

--- src/test/java/com/zakcorp/grokking/coding/twopointers/Chapter_7_Test.java
+++ src/test/java/com/zakcorp/grokking/coding/twopointers/Chapter_7_Test.java
@@ -10,4 +10,8 @@ public class Chapter_7_Test {
     public void test1() {
         assertEquals(4, p.solve1(new int[]{-1, 4, 2, 1, 3}, 5));
     }
+    @Test
+    public void test2() {
+        assertEquals(2, p.solve1(new int[]{-1, 0, 2, 3}, 3));
+    }
 }
