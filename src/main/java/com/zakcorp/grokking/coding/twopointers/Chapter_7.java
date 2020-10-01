82f0c5e1d1175052f1d001b4193efe00db399043
user.email <zakirhussainb2693@gmail.com>
2020-10-01 09:34:00 +0530

    Grokking Coding - Chapter 7 - 2

--- src/main/java/com/zakcorp/grokking/coding/twopointers/Chapter_7.java
+++ src/main/java/com/zakcorp/grokking/coding/twopointers/Chapter_7.java
@@ -19,12 +19,15 @@ public class Chapter_7 {
             Arrays.sort(arr);
             int tripletCounter = 0;
             for(int i = 0; i < n - 2; i++) {
-                int j = i + 1;
-                while(j < n - 1) {
-                    int sum = arr[i] + arr[j] + arr[j + 1];
-                    if(sum < target) {
-                        tripletCounter++;
-                        j++;
+                int left = i + 1, right = n - 1;
+                while(left < right) {
+                    if(arr[left] + arr[right] + arr[i] < target) {
+                        // Important:- Since arr[right] >= arr[left], therefore we can include/count all the numbers
+                        // between this range of numbers.
+                        tripletCounter += right - left;
+                        left++;
+                    } else {
+                        right--;
                     }
                 }
             }
