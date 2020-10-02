4313f1f61edce965f768ecb9cc6ea5388c842d1d
user.email <zakirhussainb2693@gmail.com>
2020-10-02 22:18:46 +0530

    LeetCode Problem 75 - 3

--- src/main/java/com/zakcorp/leetcodemaster/Problem_75.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_75.java
@@ -11,20 +11,22 @@ package com.zakcorp.leetcodemaster;
  * @pseudocode:
  */
 public class Problem_75 {
-    public int[] sortColors(int[] arr) {
-        int[] counter = new int[3];
-        for (int num : arr) {
-            counter[num]++;
+    static class Solver {
+        public int[] solve1(int[] arr) {
+            int[] counter = new int[3];
+            for (int num : arr) {
+                counter[num]++;
+            }
+            int[] k = {0};
+            sortArray(arr, counter[0], 0, k);
+            sortArray(arr, counter[1], 1, k);
+            sortArray(arr, counter[2], 2, k);
+            return arr;
         }
-        int[] k = {0};
-        sortArray(arr, counter[0], 0, k);
-        sortArray(arr, counter[1], 1, k);
-        sortArray(arr, counter[2], 2, k);
-        return arr;
-    }
-    private void sortArray(int[] arr, int count, int num, int[] k) {
-        while(count --> 0) {
-            arr[k[0]++] = num;
+        private void sortArray(int[] arr, int count, int num, int[] k) {
+            while(count --> 0) {
+                arr[k[0]++] = num;
+            }
         }
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_75_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_75_Test.java
@@ -5,21 +5,21 @@ import org.junit.Test;
 import static org.junit.Assert.assertArrayEquals;
 
 public class Problem_75_Test {
-    Problem_75 p = new Problem_75();
+    Problem_75.Solver p = new Problem_75.Solver();
     @Test
     public void test1() {
-        assertArrayEquals(new int[]{0,0,1,1,2,2}, p.sortColors(new int[]{2,0,2,1,1,0}));
+        assertArrayEquals(new int[]{0,0,1,1,2,2}, p.solve1(new int[]{2,0,2,1,1,0}));
     }
     @Test
     public void test2() {
-        assertArrayEquals(new int[]{0,1,2}, p.sortColors(new int[]{2,0,1}));
+        assertArrayEquals(new int[]{0,1,2}, p.solve1(new int[]{2,0,1}));
     }
     @Test
     public void test3() {
-        assertArrayEquals(new int[]{0}, p.sortColors(new int[]{0}));
+        assertArrayEquals(new int[]{0}, p.solve1(new int[]{0}));
     }
     @Test
     public void test4() {
-        assertArrayEquals(new int[]{1}, p.sortColors(new int[]{1}));
+        assertArrayEquals(new int[]{1}, p.solve1(new int[]{1}));
     }
 }
