f9373a47cf21b4ef237c2221dfd25b2f7069e56b
user.email <zakirhussainb2693@gmail.com>
2020-10-02 12:40:25 +0530

    Hackerank Daily - Sub Array Division

---
+++ src/main/java/com/zakcorp/hackerrank/SubArrayDivision.java
@@ -0,0 +1,32 @@
+package com.zakcorp.hackerrank;
+
+import java.util.*;
+
+/**
+ * Created by Zakir Hussain B on 02/10/20.
+ *
+ * @source: Hackerrank
+ * @topic: Algorithms
+ * @sub-topic: Implementation
+ * @platform: Hackerrank
+ * @problem_link: https://www.hackerrank.com/challenges/the-birthday-bar/problem
+ * @pseudocode:
+ */
+public class SubArrayDivision {
+    public int birthday(List<Integer> s, int d, int m) {
+        int start = 0, ways = 0, sum = 0;
+        for(int end = 0; end < s.size(); end++) {
+            sum += s.get(end);
+            if(end - start + 1 > m) {
+                if(sum - s.get(end) == d) {
+                    ways++;
+                }
+                sum = sum - s.get(start);
+                start++;
+            }
+        }
+        if(sum == d)
+            ways++;
+        return ways;
+    }
+}
---
+++ src/test/java/com/zakcorp/hackerrank/SubArrayDivisionTest.java
@@ -0,0 +1,29 @@
+package com.zakcorp.hackerrank;
+
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class SubArrayDivisionTest {
+    SubArrayDivision p = new SubArrayDivision();
+    @Test
+    public void test1() {
+        assertEquals(2, p.birthday(Arrays.asList(1,2,1,3,2), 3, 2));
+    }
+    @Test
+    public void test2() {
+        assertEquals(0, p.birthday(Arrays.asList(1,1,1,1,1,1), 3, 2));
+    }
+    @Test
+    public void test3() {
+        assertEquals(1, p.birthday(Collections.singletonList(4), 4, 1));
+    }
+    @Test
+    public void test4() {
+        assertEquals(3, p.birthday(Arrays.asList(2,5,1,3,4,4,3,5,1,1,2,1,4,1,3,3,
+                4,2,1), 18, 7));
+    }
+
+}
