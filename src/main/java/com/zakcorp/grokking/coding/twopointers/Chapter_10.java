222d8246ff2180c338600a0cd8f1db66aa4c8406
user.email <zakirhussainb2693@gmail.com>
2020-10-03 15:34:04 +0530

    Grokking Coding - Chapter 10

---
+++ src/main/java/com/zakcorp/grokking/coding/twopointers/Chapter_10.java
@@ -0,0 +1,16 @@
+package com.zakcorp.grokking.coding.twopointers;
+
+/**
+ * Created by Zakir Hussain B on 03/10/20.
+ *
+ * @source: Grokking Coding
+ * @topic: Two-Pointers
+ * @sub-topic: Quadruple Sum to Target (medium) #
+ * @platform: LeetCode
+ * @problem_link: https://leetcode.com/problems/4sum/
+ * @pseudocode:
+ */
+public class Chapter_10 {
+    // Refer problem 18 from leetcodemaster
+
+}
