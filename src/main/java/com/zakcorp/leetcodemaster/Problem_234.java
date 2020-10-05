a06749bc645b034207213aee090a3477cefa8ed5
user.email <zakirhussainb2693@gmail.com>
2020-10-05 18:29:21 +0530

    LeetCode Problem - 234 - 1

---
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_234.java
@@ -0,0 +1,49 @@
+package com.zakcorp.leetcodemaster;
+
+/**
+ * Created by Zakir Hussain B on 05/10/20.
+ *
+ * @source:
+ * @topic:
+ * @sub-topic:
+ * @platform:
+ * @problem_link:
+ * @pseudocode:
+ */
+public class Problem_234 {
+    static class ListNode {
+        int val;
+        ListNode next;
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+    static class Solver {
+        public boolean solve1(ListNode head) {
+            if(head == null)
+                return true;
+            return toString(head).equals(toString(reverseLinkedList(head)));
+        }
+        public String toString(ListNode head) {
+            StringBuilder sb = new StringBuilder();
+            ListNode curr = head;
+            while(curr != null) {
+                sb.append(curr.val);
+                curr = curr.next;
+            }
+            return sb.toString();
+        }
+        private ListNode reverseLinkedList(ListNode head) {
+            ListNode p = head;
+            ListNode q = null;
+            ListNode r = null;
+            while(p != null) {
+                r = q;
+                q = p;
+                p = p.next;
+                q.next = r;
+            }
+            return q;
+        }
+    }
+}
---
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_234_Test.java
@@ -0,0 +1,23 @@
+package com.zakcorp.leetcodemaster;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Problem_234_Test {
+    Problem_234.Solver p = new Problem_234.Solver();
+    @Test
+    public void test1() {
+        Problem_234.ListNode head = new Problem_234.ListNode(1);
+        head.next = new Problem_234.ListNode(2);
+        assertFalse(p.solve1(head));
+    }
+    @Test
+    public void test2() {
+        Problem_234.ListNode head = new Problem_234.ListNode(1);
+        head.next = new Problem_234.ListNode(2);
+        head.next.next = new Problem_234.ListNode(2);
+        head.next.next.next = new Problem_234.ListNode(1);
+        assertTrue(p.solve1(head));
+    }
+}
