35ee205ae43f7087a44d28b052dd3d320517675d
user.email <zakirhussainb2693@gmail.com>
2020-10-07 01:06:25 +0530

    HRK - Java - Java Currency Formatter

---
+++ src/main/java/com/zakcorp/hackerrank/java/JavaCurrencyFormatter.java
@@ -0,0 +1,38 @@
+package com.zakcorp.hackerrank.java;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+/**
+ * Created by Zakir Hussain B on 07/10/20.
+ *
+ * @source:
+ * @topic:
+ * @sub-topic:
+ * @platform:
+ * @problem_link:
+ * @pseudocode:
+ */
+
+public class JavaCurrencyFormatter {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        String india = NumberFormat.getCurrencyInstance(Locale.ENGLISH).format(payment);
+        String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+        india = "Rs." + india.substring(1);
+
+        // Write your code here.
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
