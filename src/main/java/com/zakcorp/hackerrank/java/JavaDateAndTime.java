d182e8c29508f4184f44c302a2fbf419198a80db
user.email <zakirhussainb2693@gmail.com>
2020-10-07 00:50:59 +0530

    HRK - Java - Java Date And Time

---
+++ src/main/java/com/zakcorp/hackerrank/java/JavaDateAndTime.java
@@ -0,0 +1,67 @@
+package com.zakcorp.hackerrank.java;
+
+/**
+ * Created by Zakir Hussain B on 07/10/20.
+ *
+ * @source:
+ * @topic:
+ * @sub-topic:
+ * @platform:
+ * @problem_link:
+ * @pseudocode:
+ */
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+
+    public static String findDay(int month, int day, int year) {
+        LocalDate ld = LocalDate.of(year, month, day);
+        return ld.getDayOfWeek().toString();
+    }
+
+}
+
+public class JavaDateAndTime {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+        System.out.println(res);
+
+//        bufferedWriter.write(res);
+//        bufferedWriter.newLine();
+//
+//        bufferedReader.close();
+//        bufferedWriter.close();
+    }
+}
