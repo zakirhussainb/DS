73d9457ae0c22c053262b985f4f026bd7d7c97c4
user.email <zakirhussainb2693@gmail.com>
2020-10-11 17:42:55 +0530

    Cyclic Sort First Lesson - 1

--- src/test/java/com/zakcorp/grokking/coding/cyclicsort/Chapter_2_Test.java
+++ src/test/java/com/zakcorp/grokking/coding/cyclicsort/Chapter_2_Test.java
@@ -10,4 +10,12 @@ public class Chapter_2_Test {
     public void test1() {
         assertArrayEquals(new int[]{1,2,3,4,5}, p.solve1(new int[]{3, 1, 5, 4, 2}));
     }
+    @Test
+    public void test2() {
+        assertArrayEquals(new int[]{1,2,3,4,5,6}, p.solve1(new int[]{2, 6, 4, 3, 1, 5}));
+    }
+    @Test
+    public void test3() {
+        assertArrayEquals(new int[]{1,2,3,4,5,6}, p.solve1(new int[]{1, 5, 6, 4, 3, 2}));
+    }
 }
