3f832acdde854a51d9cd3df50c52444a8a505389
user.email <zakirhussainb2693@gmail.com>
2020-10-13 09:02:29 +0530

    LC - Problem 442

--- src/test/java/com/zakcorp/leetcodemaster/Problem_442_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_442_Test.java
@@ -3,6 +3,7 @@ package com.zakcorp.leetcodemaster;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
 
@@ -13,5 +14,12 @@ public class Problem_442_Test {
         assertEquals(Arrays.asList(2, 3) ,p.solve1(new int[]{4,3,2,7,8,2,3,1}));
         assertEquals(Arrays.asList(2, 3) ,p.solve2(new int[]{4,3,2,7,8,2,3,1}));
         assertEquals(Arrays.asList(2, 3) ,p.solve3(new int[]{4,3,2,7,8,2,3,1}));
+    }
+    @Test
+    public void test2() {
+        assertEquals(Collections.singletonList(2),p.solve1(new int[]{2,2}));
+        assertEquals(Collections.singletonList(2),p.solve2(new int[]{2,2}));
+        assertEquals(Collections.singletonList(2),p.solve3(new int[]{2,2}));
+
     }
 }
