d66ecd44f27e3dd728987b25457d9269f2b7fa8d
user.email <zakirhussainb2693@gmail.com>
2020-10-17 17:06:37 +0530

    Problem 202 A

---
+++ src/main/java/com/zakcorp/codeforces/practice/Problem_202_A.java
@@ -0,0 +1,140 @@
+package com.zakcorp.codeforces.practice;
+
+import java.io.*;
+import java.util.*;
+
+public class Problem_202_A {
+    public static void main(String[] args) throws Exception {
+        InputReader in = new InputReader(System.in);
+        OutputWriter out = new OutputWriter(System.out);
+        try {
+            String str = in.readString();
+            Solver p = new Solver();
+            out.printLine(p.solve1(str));
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+        }
+    }
+    static class Solver {
+        public String solve1(String str) {
+            StringBuilder sb = new StringBuilder();
+            TreeMap<Character, Integer> tM = new TreeMap<>(Collections.reverseOrder());
+            for(int i = 0; i < str.length(); i++) {
+                tM.put(str.charAt(i), tM.getOrDefault(str.charAt(i), 0) + 1);
+            }
+            Map.Entry<Character, Integer> entry = tM.firstEntry();
+            int m = entry.getValue();
+            while(m --> 0) {
+                sb.append(entry.getKey());
+            }
+            return sb.toString();
+        }
+    }
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int readInt() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public String readString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null)
+                return filter.isSpaceChar(c);
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public String next() {
+            return readString();
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+        }
+    }
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object...objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0)
+                    writer.print(' ');
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object...objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+        public void flush() {
+            writer.flush();
+        }
+
+    }
+}
