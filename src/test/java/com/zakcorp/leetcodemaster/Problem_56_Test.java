a534e2ea19cda031f7366d4ed40cc908ef1d7f49
user.email <zakirhussainb2693@gmail.com>
2020-10-19 16:58:35 +0530

    Problem 978 A

--- src/test/java/com/zakcorp/leetcodemaster/Problem_56_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_56_Test.java
@@ -11,13 +11,16 @@ public class Problem_56_Test {
     @Test
     public void test1() {
         System.out.println(Arrays.deepToString(p.solve1(new int[][]{{1, 4}, {2, 6}, {3, 5}})));
+        System.out.println(Arrays.deepToString(p.solve2(new int[][]{{1, 4}, {2, 6}, {3, 5}})));
     }
     @Test
     public void test2() {
         System.out.println(Arrays.deepToString(p.solve1(new int[][]{{1,3},{2,6},{8,10},{15,18}})));
+        System.out.println(Arrays.deepToString(p.solve2(new int[][]{{1,3},{2,6},{8,10},{15,18}})));
     }
     @Test
     public void test3() {
         System.out.println(Arrays.deepToString(p.solve1(new int[][]{{1,4},{4,5}})));
+        System.out.println(Arrays.deepToString(p.solve2(new int[][]{{1,4},{4,5}})));
     }
 }
