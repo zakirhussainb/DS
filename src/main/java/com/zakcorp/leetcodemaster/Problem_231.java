7334d540c2afa08b5cdc8e52f71d3f600027471f
user.email <zakirhussainb2693@gmail.com>
2020-10-25 19:45:09 +0530

    Problem_231

--- src/main/java/com/zakcorp/leetcodemaster/Problem_231.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_231.java
@@ -21,5 +21,11 @@ public class Problem_231 {
 //            time complexity = O(1)
             return new HashSet<>(Arrays.asList(1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608,16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824)).contains(n);
         }
+        public boolean isPowerOfTwo_Bit_Manipulation(int n) {
+            if(n <= 0) {
+                return false;
+            }
+            return (n & (n - 1)) == 0;
+        }
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_231_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_231_Test.java
@@ -10,20 +10,24 @@ public class Problem_231_Test {
     public void test1() {
         assertTrue(p.isPowerOfTwo_approach1(1));
         assertTrue(p.isPowerOfTwo_approach2(1));
+        assertTrue(p.isPowerOfTwo_Bit_Manipulation(1));
     }
     @Test
     public void test2() {
         assertTrue(p.isPowerOfTwo_approach1(16));
         assertTrue(p.isPowerOfTwo_approach2(16));
+        assertTrue(p.isPowerOfTwo_Bit_Manipulation(16));
     }
     @Test
     public void test3() {
         assertFalse(p.isPowerOfTwo_approach1(218));
         assertFalse(p.isPowerOfTwo_approach2(218));
+        assertFalse(p.isPowerOfTwo_Bit_Manipulation(218));
     }
     @Test
     public void test4() {
         assertFalse(p.isPowerOfTwo_approach1(2147483647));
         assertFalse(p.isPowerOfTwo_approach2(2147483647));
+        assertFalse(p.isPowerOfTwo_Bit_Manipulation(2147483647));
     }
 }
