100e5a1648e3ebfd1a412f6debf50c2f8a06bf77
user.email <zakirhussainb2693@gmail.com>
2020-11-11 13:04:18 +0530

    LC - Problem 323 - 2

--- src/main/java/com/zakcorp/leetcodemaster/Problem_323.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_323.java
@@ -1,10 +1,9 @@
 package com.zakcorp.leetcodemaster;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Problem_323 {
-    static class Solver {
+    static class Solver1 {
         static class Graph {
             int V;
             int[][] adjMatrix;
@@ -37,6 +36,46 @@ public class Problem_323 {
             visited[src] = true;
             for(int i = 0; i < g.V; i++) {
                 if(g.adjMatrix[src][i] == 1 && !visited[i]) {
+                    dfs(i, visited, g);
+                }
+            }
+        }
+    }
+    static class Solver2 {
+        static class Graph {
+            private int V;
+            private LinkedList<Integer>[] adjListArr;
+            public Graph(int V) {
+                this.V = V;
+                adjListArr = new LinkedList[V];
+                for(int i = 0; i < V; i++) {
+                    adjListArr[i] = new LinkedList<>();
+                }
+            }
+            private void addEdge(int u, int v) {
+                adjListArr[u].add(v);
+                adjListArr[v].add(u);
+            }
+        }
+        public int solve1(int n, int[][] edges) {
+            Graph g = new Graph(n);
+            for(int[] edge : edges) {
+                g.addEdge(edge[0], edge[1]);
+            }
+            int cc = 0;
+            boolean[] visited = new boolean[g.V];
+            for(int i = 0; i < g.V; i++) {
+                if( !visited[i] ) {
+                    dfs(i, visited, g);
+                    cc++;
+                }
+            }
+            return cc;
+        }
+        private void dfs(int src, boolean[] visited, Graph g) {
+            visited[src] = true;
+            for(Integer i : g.adjListArr[src]) {
+                if( !visited[i] ) {
                     dfs(i, visited, g);
                 }
             }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_323_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_323_Test.java
@@ -5,9 +5,11 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class Problem_323_Test {
-    Problem_323.Solver p = new Problem_323.Solver();
+    Problem_323.Solver1 p1 = new Problem_323.Solver1();
+    Problem_323.Solver2 p2 = new Problem_323.Solver2();
     @Test
     public void test1() {
-        assertEquals(2, p.solve1(5, new int[][]{ {0, 1}, {1, 2}, {3, 4} }));
+        assertEquals(2, p1.solve1(5, new int[][]{ {0, 1}, {1, 2}, {3, 4} }));
+        assertEquals(2, p2.solve1(5, new int[][]{ {0, 1}, {1, 2}, {3, 4} }));
     }
 }
