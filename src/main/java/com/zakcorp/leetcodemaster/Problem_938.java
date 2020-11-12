1108a824354e8890c9fa25b66ddd89a5e8529a10
user.email <zakirhussainb2693@gmail.com>
2020-11-12 22:16:47 +0530

    LC - Problem 938 - 2

--- src/main/java/com/zakcorp/leetcodemaster/Problem_938.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_938.java
@@ -4,18 +4,40 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Problem_938 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {
+        }
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
     static class Solver {
-        public int[] solve1(int[] arr, int target) {
-            if(arr.length == 0)
-                return new int[]{0, 0};
-            Map<Integer, Integer> map = new HashMap<>();
-            for(int i = 0; i < arr.length; i++) {
-                if(map.containsKey(target - arr[i])) {
-                    return new int[]{map.get(target - arr[i]), i};
-                }
-                map.put(arr[i], i);
+        public int solve1(TreeNode root, int low, int high) {
+            int[] sum = {0};
+            preOrder(root, low, high, sum);
+            return sum[0];
+        }
+        private void preOrder(TreeNode root, int low, int high, int[] sum) {
+            if(root == null) {
+                return;
             }
-            throw new IllegalArgumentException("No two sum solution");
+            if(root.val >= low && root.val <= high) {
+                sum[0] += root.val;
+            }
+            if(root.val > low) {
+                preOrder(root.left, low, high, sum);
+            }
+            if(root.val < high) {
+                preOrder(root.right, low, high, sum);
+            }
         }
     }
 }
