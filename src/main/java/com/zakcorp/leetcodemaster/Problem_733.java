1b4626feba318bab20b5e9e415d43b5fd1632437
user.email <zakirhussainb2693@gmail.com>
2020-11-14 14:52:39 +0530

    LC - Problem - 1

---
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_733.java
@@ -0,0 +1,29 @@
+package com.zakcorp.leetcodemaster;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Problem_733 {
+    static class Solver {
+        public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+            if(image[sr][sc] == newColor) {
+                return image;
+            }
+            fillImage(image, sr, sc, image[sr][sc], newColor);
+            return image;
+        }
+        private void fillImage(int[][] image, int sr, int sc, int color, int newColor) {
+            if(sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length) {
+                return;
+            }
+            if(image[sr][sc] != color) {
+                return;
+            }
+            image[sr][sc] = newColor;
+            fillImage(image, sr + 1, sc, color, newColor);
+            fillImage(image, sr - 1, sc, color, newColor);
+            fillImage(image, sr, sc + 1, color, newColor);
+            fillImage(image, sr, sc - 1, color, newColor);
+        }
+    }
+}
