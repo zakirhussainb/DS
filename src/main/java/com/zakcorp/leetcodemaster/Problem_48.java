c3ed90cc6c40da4ac7d5481de4420a51e54ccf9d
user.email <zakirhussainb2693@gmail.com>
2020-12-14 01:24:49 +0530

    LC - Problem 48 - 1

--- src/main/java/com/zakcorp/leetcodemaster/Problem_48.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_48.java
@@ -20,5 +20,23 @@ public class Problem_48 {
             }
             return matrix;
         }
+        public int[][] solve2(int[][] matrix) {
+            int n = matrix.length;
+            for(int i = 0; i < n; i++) {
+                for(int j = i; j < n; j++) {
+                    int temp = matrix[j][i];
+                    matrix[j][i] = matrix[i][j];
+                    matrix[i][j] = temp;
+                }
+            }
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n / 2; j++) {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[i][n - 1 - j];
+                    matrix[i][n - 1 - j] = temp;
+                }
+            }
+            return matrix;
+        }
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_48_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_48_Test.java
@@ -10,5 +10,8 @@ public class Problem_48_Test {
     public void test1() {
         assertArrayEquals(new int[][]{ {7,4,1},{8,5,2},{9,6,3} },
                 p.solve1(new int[][]{ {1,2,3},{4,5,6},{7,8,9} }));
+
+        assertArrayEquals(new int[][]{ {7,4,1},{8,5,2},{9,6,3} },
+                p.solve2(new int[][]{ {1,2,3},{4,5,6},{7,8,9} }));
     }
 }
