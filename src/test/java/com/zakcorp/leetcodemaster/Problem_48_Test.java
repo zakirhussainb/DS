7493fde8b8d4e734aec81dc354eaff3c9a24be57
user.email <zakirhussainb2693@gmail.com>
2020-12-14 01:51:48 +0530

    LC - Problem 48 - 2

--- src/test/java/com/zakcorp/leetcodemaster/Problem_48_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_48_Test.java
@@ -13,5 +13,8 @@ public class Problem_48_Test {
 
         assertArrayEquals(new int[][]{ {7,4,1},{8,5,2},{9,6,3} },
                 p.solve2(new int[][]{ {1,2,3},{4,5,6},{7,8,9} }));
+
+        assertArrayEquals(new int[][]{ {7,4,1},{8,5,2},{9,6,3} },
+                p.solve3(new int[][]{ {1,2,3},{4,5,6},{7,8,9} }));
     }
 }
