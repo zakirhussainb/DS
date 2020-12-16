ecb02c57c8bc09e7dd9cd7c3969e397eea5399ec
user.email <zakirhussainb2693@gmail.com>
2020-12-16 09:10:46 +0530

    LC - Problem 139 - 1

--- src/main/java/com/zakcorp/leetcodemaster/Problem_139.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_139.java
@@ -2,30 +2,41 @@ package com.zakcorp.leetcodemaster;
 
 import java.util.*;
 
+/*
+TODO:- Need to implement the Bottom-Up approach via Dynamic Programming.
+ */
 public class Problem_139 {
+    // Throws a TLE error
     static class Solver {
         public boolean solve1(String str, List<String> wordDict) {
-            StringBuilder sb = new StringBuilder(str);
-            int i = 0, n = wordDict.size(), notfound = 0;
-            if(n == 0)
-                return false;
-            Collections.sort(wordDict);
-            while(sb.length() != 0) {
-                int start = sb.indexOf(wordDict.get(i));
-                int end = start + wordDict.get(i).length();
-                if(sb.indexOf(wordDict.get(i)) >= 0) {
-                    sb.delete(start, end);
-                    notfound--;
-                } else {
-                    notfound++;
-                }
-                i++;
-                i = i % n;
-                if(notfound == n && sb.length() != 0) {
-                    return false;
-                }
+            return wordBreak(str, new HashSet<>(wordDict), 0);
+        }
+        private boolean wordBreak(String str, Set<String> wordDict, int start) {
+            if(start == str.length())
+                return true;
+            for(int end = start + 1; end <= str.length(); end++) {
+                if( wordDict.contains(str.substring(start, end)) && wordBreak(str, wordDict, end) )
+                    return true;
             }
-            return true;
+            return false;
+        }
+    }
+
+    // Recursion with Memoization - Top Down Approach
+    static class Solver1 {
+        public boolean solve1(String str, List<String> wordDict) {
+            return wordBreak(str, new HashSet<>(wordDict), 0, new Boolean[str.length()]);
+        }
+        private boolean wordBreak(String str, Set<String> wordDict, int start, Boolean[] memo) {
+            if(start == str.length())
+                return true;
+            if(memo[start] != null)
+                return memo[start];
+            for(int end = start + 1; end <= str.length(); end++) {
+                if( wordDict.contains(str.substring(start, end)) && wordBreak(str, wordDict, end, memo) )
+                    return memo[start] = true;
+            }
+            return memo[start] = false;
         }
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_139_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_139_Test.java
@@ -8,24 +8,30 @@ import static org.junit.Assert.*;
 
 public class Problem_139_Test {
     Problem_139.Solver p = new Problem_139.Solver();
+    Problem_139.Solver1 p1 = new Problem_139.Solver1();
     @Test
     public void test1() {
         assertTrue( p.solve1("leetcode", Arrays.asList("code", "leet") ));
+        assertTrue( p1.solve1("leetcode", Arrays.asList("code", "leet") ));
     }
     @Test
     public void test2() {
         assertTrue( p.solve1("applepenapple", Arrays.asList("apple", "pen") ));
+        assertTrue( p1.solve1("applepenapple", Arrays.asList("apple", "pen") ));
     }
     @Test
     public void test3() {
         assertFalse( p.solve1("catsandog", Arrays.asList("cats", "dog", "sand", "and", "cat") ));
+        assertFalse( p1.solve1("catsandog", Arrays.asList("cats", "dog", "sand", "and", "cat") ));
     }
     @Test
     public void test4() {
         assertTrue( p.solve1("bb", Arrays.asList("a","b","bbb","bbbb") ));
+        assertTrue( p1.solve1("bb", Arrays.asList("a","b","bbb","bbbb") ));
     }
     @Test
     public void test5() {
         assertTrue( p.solve1("cars", Arrays.asList("car","ca","rs") ));
+        assertTrue( p1.solve1("cars", Arrays.asList("car","ca","rs") ));
     }
 }
