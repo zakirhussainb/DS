90e6658975bb9310527c49268921be3026910a15
user.email <zakirhussainb2693@gmail.com>
2020-12-16 19:48:33 +0530

    LC - 451 - 1

---
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_236.java
@@ -0,0 +1,27 @@
+package com.zakcorp.leetcodemaster;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Problem_236 {
+    static class TreeNode {
+        int val;
+        Problem_108.TreeNode left;
+        Problem_108.TreeNode right;
+        TreeNode() {
+        }
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, Problem_108.TreeNode left, Problem_108.TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    static class Solver {
+//        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//
+//        }
+    }
+}
---
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_451_Test.java
@@ -0,0 +1,21 @@
+package com.zakcorp.leetcodemaster;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Problem_451_Test {
+    Problem_451.Solver p = new Problem_451.Solver();
+    @Test
+    public void test1() {
+        assertEquals("eert", p.solve1("tree"));
+    }
+    @Test
+    public void test2() {
+        assertEquals("aaaccc", p.solve1("cccaaa"));
+    }
+    @Test
+    public void test3() {
+        assertEquals("bbAa", p.solve1("Aabb"));
+    }
+}
