8f9be5c43ca209820c0d3e43d6a4499d0b4f8cbd
user.email <zakirhussainb2693@gmail.com>
2021-01-03 02:22:53 +0530

    DP - HowSum - 1

--- src/main/java/com/zakcorp/dynamicprogramming/HowSum.java
+++ src/main/java/com/zakcorp/dynamicprogramming/HowSum.java
@@ -1,5 +1,7 @@
 package com.zakcorp.dynamicprogramming;
 
+import java.util.*;
+
 /**
  * Created by Zakir Hussain B on 02/01/21.
  *
@@ -13,13 +15,53 @@ package com.zakcorp.dynamicprogramming;
 /*
 Write a function `howSum(targetSum, arr)` that takes in a targetSum and an array of numbers as arguments.
 
-The function should return an array containing any combination of elements that add u[ to exactly the targetSum. If there
+The function should return an array containing any combination of elements that add up to exactly the targetSum. If there
 is no combination that add up to the targetSum, then return null.
 
 If there are  multiple combinations possible, you may return any single one.
  */
 public class HowSum {
-    public boolean howSum(int target, int[] arr) {
-        return false;
+    static class Solver {
+        public int[] recursive(int target, int[] arr) {
+            LinkedList<Integer> ll = howSum(target, arr);
+            int[] res = null;
+            if( !ll.isEmpty() ) {
+                res = new int[ll.size()];
+            }
+            for(int i = 0; i < ll.size(); i++) {
+                res[i] = ll.get(i);
+            }
+            return res;
+        }
+        private LinkedList<Integer> howSum(int target, int[] arr) {
+            if(target == 0)
+                return new LinkedList<>();
+            if(target < 0)
+                return null;
+            for(int num : arr) {
+                LinkedList<Integer> res = howSum(target - num, arr);
+                if( res != null ) {
+                    res.add(num);
+                    return res;
+                }
+            }
+            return null;
+        }
+    }
+
+    static class Solver1 {
+        public int[] memoized(int target, int[] arr) {
+            return canSum(target, arr, new int[target + 1]);
+        }
+        private int[] canSum(int target, int[] arr, int[] memo) {
+            return null;
+        }
+    }
+
+    static class Solver2 {
+        public int[] tabulation(int target, int[] arr) {
+
+            return null;
+        }
     }
 }
---
+++ src/test/java/com/zakcorp/dynamicprogramming/HowSum_Test.java
@@ -0,0 +1,41 @@
+package com.zakcorp.dynamicprogramming;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HowSum_Test {
+    HowSum.Solver p = new HowSum.Solver();
+    HowSum.Solver1 p1 = new HowSum.Solver1();
+    HowSum.Solver2 p2 = new HowSum.Solver2();
+    @Test
+    public void test1() {
+        assertArrayEquals(new int[]{3, 2, 2}, p.recursive(7, new int[]{2, 3}));
+//        assertArrayEquals(new int[]{3, 2, 2}, p1.memoized(7, new int[]{2, 3}));
+//        assertArrayEquals(new int[]{3, 2, 2}, p2.tabulation(7, new int[]{2, 3}));
+    }
+    /*@Test
+    public void test2() {
+        assertTrue(p.recursive(7, new int[]{5, 3, 4 ,7}));
+        assertTrue(p1.memoized(7, new int[]{5, 3, 4 ,7}));
+        assertTrue(p2.tabulation(7, new int[]{5, 3, 4 ,7}));
+    }
+    @Test
+    public void test3() {
+        assertFalse(p.recursive(7, new int[]{2, 4}));
+        assertFalse(p1.memoized(7, new int[]{2, 4}));
+        assertFalse(p2.tabulation(7, new int[]{2, 4}));
+    }
+    @Test
+    public void test4() {
+        assertTrue(p.recursive(8, new int[]{2, 3, 5}));
+        assertTrue(p1.memoized(8, new int[]{2, 3, 5}));
+        assertTrue(p2.tabulation(8, new int[]{2, 3, 5}));
+    }
+    @Test
+    public void test5() {
+//        assertFalse(p.recursive(300, new int[]{7, 14})); // TLE
+        assertFalse(p1.memoized(300, new int[]{7, 14})); // 776 ms
+//        assertFalse(p2.tabulation(300, new int[]{7, 14})); // 2 ms
+    }*/
+}
