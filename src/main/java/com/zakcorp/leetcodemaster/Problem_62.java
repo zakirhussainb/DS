d103c898a7b5c7ee9f0027d4a26d3420950ff0d3
user.email <zakirhussainb2693@gmail.com>
2021-01-02 17:35:11 +0530

    LC - Problem 62 - 1

--- src/main/java/com/zakcorp/leetcodemaster/Problem_62.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_62.java
@@ -1,15 +1,28 @@
 package com.zakcorp.leetcodemaster;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Problem_62 {
-    static class Solver {
+    static class Solver { // Time: O(2 ^ (m+n)); Space: O(m + n)
         public int gridRecursive(int m, int n) {
             if(m == 1 || n == 1) {
                 return 1;
             }
             return gridRecursive(m, n - 1) + gridRecursive(m - 1, n);
+        }
+    }
+    static class Solver1 { // Time: O(m * n); Space: O(m + n)
+        public int gridMemoized(int m, int n) {
+            int[][] memo = new int[m + 1][n + 1];
+            return gridSol(m, n, memo);
+        }
+        private int gridSol(int m, int n, int[][] memo) {
+            if(memo[m][n] != 0) {
+                return memo[m][n];
+            }
+            if(m == 1 || n == 1) {
+                return 1;
+            }
+            memo[m][n] = gridSol(m, n - 1, memo) + gridSol(m - 1, n, memo);
+            return memo[m][n];
         }
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_62_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_62_Test.java
@@ -6,8 +6,19 @@ import static org.junit.Assert.*;
 
 public class Problem_62_Test {
     Problem_62.Solver p = new Problem_62.Solver();
+    Problem_62.Solver1 p1 = new Problem_62.Solver1();
     @Test
     public void test1() {
         assertEquals(20, p.gridRecursive(4, 4));
+        assertEquals(20, p1.gridMemoized(4, 4));
+    }
+    @Test
+    public void test2() {
+        assertEquals(1287, p.gridRecursive(6, 9));
+        assertEquals(1287, p1.gridMemoized(6, 9));
+    }@Test
+    public void test3() {
+//        assertEquals(1916797311, p.gridRecursive(51, 9));
+        assertEquals(1916797311, p1.gridMemoized(51, 9));
     }
 }
