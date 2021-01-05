119f38d3b5058eeb42ecc0a1a0b33631de431986
user.email <zakirhussainb2693@gmail.com>
2021-01-05 23:28:50 +0530

    LC Problem 64 - 3

--- src/main/java/com/zakcorp/leetcodemaster/Problem_64.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_64.java
@@ -66,12 +66,65 @@ public class Problem_64 {
         }
     }
 
-    // Good Solution Tabulation DP
+    // Good Solution Memoized DP, passed - Alternative memoization solution
     static class Solver2 {
         public int solve1(int[][] grid) {
             int m = grid.length;
             int n = grid[0].length;
-            return 0;
+            int[][] memo = new int[m][n];
+            return memoized(m - 1, n - 1, grid, memo);
+        }
+        private int memoized(int m, int n, int[][] grid, int[][] memo) {
+            if(memo[m][n] != 0) {
+                return memo[m][n];
+            }
+            if(m == 0 && n == 0) {
+                return grid[m][n];
+            }
+            if(m == 0) {
+                return grid[m][n] + memoized(m, n - 1, grid, memo);
+            }
+            if(n == 0) {
+                return grid[m][n] + memoized(m - 1, n, grid, memo);
+            }
+            memo[m][n] = Math.min( memoized(m, n - 1, grid, memo) , memoized(m - 1, n, grid, memo) ) + grid[m][n];
+            return memo[m][n];
+        }
+    }
+    // Good Solution Tabulation DP
+    static class Solver3 {
+        public int solve1(int[][] grid) {
+            int m = grid.length;
+            int n = grid[0].length;
+            for (int i = m - 1; i >= 0; i--) {
+                for (int j = n - 1; j >= 0; j--) {
+                    if(i == m - 1 && j != n - 1)
+                        grid[i][j] = grid[i][j] +  grid[i][j + 1];
+                    else if(j == n - 1 && i != m - 1)
+                        grid[i][j] = grid[i][j] + grid[i + 1][j];
+                    else if(j != n - 1 && i != m - 1)
+                        grid[i][j] = grid[i][j] + Math.min(grid[i + 1][j],grid[i][j + 1]);
+                }
+            }
+            return grid[0][0];
+        }
+        public int solve2(int[][] grid) {
+            int m = grid.length;
+            int n = grid[0].length;
+            for(int i = 0; i < m; i++) {
+                for(int j = 0; j < n; j++) {
+                    if(i == 0 && j == 0) {
+                        grid[i][j] = grid[i][j];
+                    } else if(i == 0 && j != 0) {
+                        grid[i][j] += grid[i][j - 1];
+                    } else if(i != 0 && j == 0) {
+                        grid[i][j] += grid[i - 1][j];
+                    } else {
+                        grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+                    }
+                }
+            }
+            return grid[m - 1][n - 1];
         }
     }
 }
