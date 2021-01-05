18685c5f93f35b785c2bf1eb54abf3471a03d49a
user.email <zakirhussainb2693@gmail.com>
2021-01-05 20:10:39 +0530

    LC Problem 63 - 2

--- src/main/java/com/zakcorp/leetcodemaster/Problem_63.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_63.java
@@ -1,7 +1,9 @@
 package com.zakcorp.leetcodemaster;
 
+import java.util.Arrays;
+
 public class Problem_63 {
-    // Good Solution but Brute Force, so TLE
+    // Good Solution but Brute Force - Recursive , so TLE
     static class Solver {
         public int solve1(int[][] obstacleGrid) {
             int m = obstacleGrid.length;
@@ -19,6 +21,36 @@ public class Problem_63 {
             if(obstacleGrid[m][n] != 1) {
                 res = recursive(m, n - 1, obstacleGrid) + recursive(m - 1, n, obstacleGrid);
             }
+            return res;
+        }
+    }
+
+    // Good Solution Memoized DP, passed
+    static class Solver1 {
+        public int solve1(int[][] obstacleGrid) {
+            int m = obstacleGrid.length;
+            int n = obstacleGrid[0].length;
+            int[][] memo = new int[m][n];
+            for(int[] kl : memo) {
+                Arrays.fill(kl, -1);
+            }
+            return memoized(m - 1, n - 1, obstacleGrid, memo);
+        }
+        private int memoized(int m, int n, int[][] grid, int[][] memo) {
+            if(m < 0 || n < 0) {
+                return 0;
+            }
+            if(memo[m][n] != -1) {
+                return memo[m][n];
+            }
+            if(m == 0 && n == 0 && grid[m][n] != 1) {
+                return 1;
+            }
+            int res = 0;
+            if(grid[m][n] != 1) {
+                res = memoized(m, n - 1, grid, memo) + memoized(m - 1, n, grid, memo);
+            }
+            memo[m][n] = res;
             return res;
         }
     }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_63_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_63_Test.java
@@ -6,16 +6,20 @@ import static org.junit.Assert.*;
 
 public class Problem_63_Test {
     Problem_63.Solver p = new Problem_63.Solver();
+    Problem_63.Solver1 p1 = new Problem_63.Solver1();
     @Test
     public void test1() {
         assertEquals(2, p.solve1(new int[][]{{0,0,0},{0,1,0},{0,0,0}}));
+        assertEquals(2, p1.solve1(new int[][]{{0,0,0},{0,1,0},{0,0,0}}));
     }
     @Test
     public void test2() {
         assertEquals(1, p.solve1(new int[][]{{0,1},{0,0}}));
+        assertEquals(1, p1.solve1(new int[][]{{0,1},{0,0}}));
     }
     @Test
     public void test3() {
         assertEquals(0, p.solve1(new int[][]{{1}}));
+        assertEquals(0, p1.solve1(new int[][]{{1}}));
     }
 }
