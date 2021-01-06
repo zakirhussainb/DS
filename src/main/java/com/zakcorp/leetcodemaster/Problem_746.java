49b8329e2bfa44931585020d452133d71189ff33
user.email <zakirhussainb2693@gmail.com>
2021-01-06 22:36:11 +0530

    LC - Problem 746 - 1

--- src/main/java/com/zakcorp/leetcodemaster/Problem_746.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_746.java
@@ -12,4 +12,38 @@ public class Problem_746 {
             return Math.min(f1, f2);
         }
     }
+    static class Solver1 {
+        public int solve1(int[] cost) {
+            return Math.min( recursive(0, cost), recursive(1, cost) );
+        }
+        private int recursive(int n, int[] cost) {
+            if(n >= cost.length) {
+                return 0;
+            }
+            if(n == cost.length - 1) {
+                return cost[n];
+            }
+            return Math.min( recursive(n + 1, cost) , recursive(n + 2, cost) ) + cost[n];
+        }
+    }
+    static class Solver2 {
+        public int solve1(int[] cost) {
+            int n = cost.length;
+            int[] memo = new int[n + 1];
+            return Math.min( memoized(0, cost, memo), memoized(1, cost, memo) );
+        }
+        private int memoized(int n, int[] cost, int[] memo) {
+            if(memo[n] != 0) {
+                return memo[n];
+            }
+            if(n >= cost.length) {
+                return 0;
+            }
+            if(n == cost.length - 1) {
+                return cost[n];
+            }
+            memo[n] = Math.min(memoized(n + 1, cost, memo), memoized(n + 2, cost, memo) ) + cost[n];
+            return memo[n];
+        }
+    }
 }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_746_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_746_Test.java
@@ -7,8 +7,19 @@ import static org.junit.Assert.assertEquals;
 
 public class Problem_746_Test {
     Problem_746.Solver p = new Problem_746.Solver();
+    Problem_746.Solver1 p1 = new Problem_746.Solver1();
+    Problem_746.Solver2 p2 = new Problem_746.Solver2();
     @Test
     public void test1() {
-        assertEquals(6, p.solve1(new int[]{1, 100, 1, 1, 1, 100, 1, 1, 100, 1}));
+//        assertEquals(6, p.solve1(new int[]{1, 100, 1, 1, 1, 100, 1, 1, 100, 1}));
+        assertEquals(6, p1.solve1(new int[]{1, 100, 1, 1, 1, 100, 1, 1, 100, 1}));
+        assertEquals(6, p2.solve1(new int[]{1, 100, 1, 1, 1, 100, 1, 1, 100, 1}));
     }
+    @Test
+    public void test2() {
+//        assertEquals(6, p.solve1(new int[]{1, 100, 1, 1, 1, 100, 1, 1, 100, 1}));
+        assertEquals(15, p1.solve1(new int[]{10, 15, 20}));
+        assertEquals(15, p2.solve1(new int[]{10, 15, 20}));
+    }
+
 }
