ee79cb0bc9388dab8841425281677c059e904a4a
user.email <zakirhussainb2693@gmail.com>
2021-01-12 01:12:47 +0530

    LC - 198 - 1

--- src/main/java/com/zakcorp/leetcodemaster/Problem_198.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_198.java
@@ -1,10 +1,9 @@
 package com.zakcorp.leetcodemaster;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Problem_198 {
-    // Simpl Brute Force - TLE
+    // Simple Brute Force - TLE
     static class Solver {
         public int solve1(int[] arr) {
             return recursive(arr.length - 1, arr);
@@ -13,6 +12,23 @@ public class Problem_198 {
             if(n < 0)
                 return 0;
             return Math.max( recursive(n - 2, arr) + arr[n], recursive(n - 1, arr) );
+        }
+    }
+
+    // Good Solution
+    static class Solver1 {
+        public int solve1(int[] arr) {
+            int[] memo = new int[arr.length + 1];
+            Arrays.fill(memo, -1);
+            return memoized(arr.length - 1, arr, memo);
+        }
+        private int memoized(int n, int[] arr, int[] memo) {
+            if(n < 0)
+                return 0;
+            if(memo[n] >= 0)
+                return memo[n];
+            memo[n] = Math.max( memoized(n - 2, arr, memo) + arr[n], memoized(n - 1, arr, memo) );;
+            return memo[n];
         }
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_198_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_198_Test.java
@@ -6,16 +6,25 @@ import static org.junit.Assert.*;
 
 public class Problem_198_Test {
     Problem_198.Solver p = new Problem_198.Solver();
+    Problem_198.Solver1 p1 = new Problem_198.Solver1();
     @Test
     public void test1() {
         assertEquals(4, p.solve1(new int[]{1,2,3,1}));
+        assertEquals(4, p1.solve1(new int[]{1,2,3,1}));
     }
     @Test
     public void test2() {
         assertEquals(12, p.solve1(new int[]{2,7,9,3,1}));
+        assertEquals(12, p1.solve1(new int[]{2,7,9,3,1}));
     }
     @Test
     public void test3() {
         assertEquals(120, p.solve1(new int[]{100,50,20}));
+        assertEquals(120, p1.solve1(new int[]{100,50,20}));
+    }
+    @Test
+    public void test4() {
+        assertEquals(4, p.solve1(new int[]{2,1,1,2}));
+        assertEquals(4, p1.solve1(new int[]{2,1,1,2}));
     }
 }
