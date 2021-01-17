bf7c4281b00f3372e56498c0d8a141b4f94235de
user.email <zakirhussainb2693@gmail.com>
2021-01-17 20:42:27 +0530

    LC - 402 - 2

--- src/main/java/com/zakcorp/leetcodemaster/Problem_402.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_402.java
@@ -7,20 +7,21 @@ public class Problem_402 {
     static class Solver {
         public String solve1(String str, int k) {
             int n = str.length();
-            StringBuilder sb = new StringBuilder(str);
-            for(int i = 1; i < n; i++) {
-                while( k > 0 && i < sb.length() && sb.charAt(i - 1) > sb.charAt(i) ) {
-                    sb.deleteCharAt(i - 1);
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < n; i++) {
+                while( k > 0 && sb.length() > 0 && sb.charAt(sb.length() - 1) > str.charAt(i) ) {
+                    sb.deleteCharAt(sb.length() - 1);
                     k--;
                 }
+                sb.append(str.charAt(i));
             }
             // Corner case for Monotonically increasing sequence -> eg. 112, 1111, 337
-            while( k > 0 && sb.length() >= 1 ) {
+            while( k > 0 && sb.length() > 0 ) {
                 sb.deleteCharAt(sb.length() - 1);
                 k--;
             }
             // Corner case with leading zeros
-            while( sb.length() >= 1 && sb.charAt(0) == '0' ) {
+            while( sb.length() > 0 && sb.charAt(0) == '0' ) {
                 sb.deleteCharAt(0);
             }
             return sb.length() == 0 ? "0" : sb.toString();
