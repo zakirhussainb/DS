5a88ba44c08dac266ee886cadd07b33078c8d28a
user.email <zakirhussainb2693@gmail.com>
2021-01-17 22:37:47 +0530

    LC - 1029 - 1

--- src/main/java/com/zakcorp/leetcodemaster/Problem_1029.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_1029.java
@@ -27,4 +27,14 @@ public class Problem_1029 {
             return o1[0] - o1[1] - ( o2[0] - o2[1] );
         }
     }
+    static class Solver1 {
+        public int solve1(int[][] costs) {
+            Arrays.sort( costs, Comparator.comparingInt( a -> (a[0] - a[1]) ) );
+            int total = 0;
+            for(int i = 0; i < costs.length / 2; i++) {
+                total += costs[i][0] + costs[i + costs.length / 2][1];
+            }
+            return total;
+        }
+    }
 }
