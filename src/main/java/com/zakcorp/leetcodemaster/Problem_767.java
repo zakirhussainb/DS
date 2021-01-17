c173137235701fefcb84dbf4ff4f0774bc1460f6
user.email <zakirhussainb2693@gmail.com>
2021-01-17 16:12:21 +0530

    LC - 767

--- src/main/java/com/zakcorp/leetcodemaster/Problem_767.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_767.java
@@ -1,30 +1,48 @@
 package com.zakcorp.leetcodemaster;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem_767 {
     static class Solver {
         public String solve1(String str) {
-            StringBuilder sb = new StringBuilder();
-            char[] chArr = str.toCharArray();
-            Arrays.sort(chArr);
-            str = new String(chArr);
             int n = str.length();
-            int i = 0, j = n - 1;
-            while(i <= j) {
-                if(str.charAt(i) != str.charAt(j)) {
-                    sb.append(str.charAt(i)).append(str.charAt(j));
-                    i++; j--;
-                } else {
-                    if(sb.length() != 0 && i == j) {
-                        sb.append(str.charAt(i));
-                        i++; j--;
-                    } else {
-                        return "";
+            char[] resCh = new char[n];
+            Map<Character, Integer> map = new HashMap<>();
+            for(int i = 0; i < str.length(); i++) {
+                map.put(str.charAt(i), map.getOrDefault(str.charAt(i), 0) + 1);
+            }
+            final Map<Character, Integer> sortedByValueMap = map.entrySet()
+                    .stream()
+                    .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new ));
+
+            int j = 0;
+            for(Map.Entry<Character, Integer> entry : sortedByValueMap.entrySet()) {
+                if(j >= n) {
+                    j = j % n;
+                    if(j == 0)
+                        j = 1;
+                }
+                int size = entry.getValue();
+                while(size > 0) {
+                    for(; j < n; ) {
+                        if(size > 0) {
+                            if(resCh[j] == '\u0000') {
+                                resCh[j] = entry.getKey();
+                            }
+                        }
+                        j = j + 2;
+                        size--;
                     }
                 }
             }
-            return sb.toString();
+            for(char ch : resCh) {
+                if(ch == '\u0000') {
+                    return "";
+                }
+            }
+            return new String(resCh);
         }
     }
 }
