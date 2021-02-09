02ddde6431a17511305f4607a2c9c357749735a4
user.email <zakirhussainb2693@gmail.com>
2021-02-09 20:22:35 +0530

    LC - 647

--- src/main/java/com/zakcorp/leetcodemaster/Problem_647.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_647.java
@@ -33,6 +33,36 @@ public class Problem_647 {
         }
     }
     static class Solver1 {
-
+        public int solve1(String str) {
+            int[] sum = {0};
+            return recursive(0, 1, str, sum);
+        }
+        private int recursive(int i, int j, String str, int[] sum) {
+            if(i >= str.length())
+                return 0;
+            if(j > str.length())
+                return 0;
+            String substr = str.substring(i, j);
+            if( !substr.isEmpty() && isPalindrome( substr ) ) {
+                sum[0] += 1;
+            }
+            return recursive(i, j + 1, str, sum) + recursive(i + 1, j, str, sum);
+        }
+        private boolean isPalindrome(String str) {
+            if(str.length() == 0 || str.length() == 1) {
+                return true;
+            }
+            int left = 0, right = str.length() - 1;
+            while(left <= right) {
+                char leftCh = str.charAt(left);
+                char rightCh = str.charAt(right);
+                if(leftCh != rightCh) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_647_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_647_Test.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.*;
 
 public class Problem_647_Test {
     Problem_647.Solver p = new Problem_647.Solver();
+    Problem_647.Solver1 p1 = new Problem_647.Solver1();
     @Test
     public void test1() {
         assertEquals(1087, p.solve1("aecoepvucbaufmjpcehsmtilafkrgymqhhvxchqupoliakbbhwuqx" +
@@ -23,9 +24,11 @@ public class Problem_647_Test {
     @Test
     public void test2() {
         assertEquals(3, p.solve1("abc"));
+        assertEquals(3, p1.solve1("abc"));
     }
     @Test
     public void test3() {
         assertEquals(6, p.solve1("aaa"));
+        assertEquals(6, p1.solve1("aaa"));
     }
 }
