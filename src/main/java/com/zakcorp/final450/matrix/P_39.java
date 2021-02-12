6cad05ac4eea05b38c51a7201d6abda9e80f2971
user.email <zakirhussainb2693@gmail.com>
2021-02-13 01:46:19 +0530

    FINAL - 39

---
+++ src/main/java/com/zakcorp/final450/matrix/P_39.java
@@ -0,0 +1,15 @@
+package com.zakcorp.final450.matrix;
+
+import java.util.ArrayList;
+
+public class P_39 {
+    static class Solver {
+        /* https://www.geeksforgeeks.org/program-for-mean-and-median-of-an-unsorted-array/
+           https://practice.geeksforgeeks.org/problems/median-in-a-row-wise-sorted-matrix1527/1
+        */
+        public ArrayList<Integer> solve1(int[][] matrix, int r, int c) {
+            // Refer  problem 74 from leetcode
+            return null;
+        }
+    }
+}
