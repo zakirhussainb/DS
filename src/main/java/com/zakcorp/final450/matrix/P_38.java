ea81d5df179108b23c05356d70df80f728bca977
user.email <zakirhussainb2693@gmail.com>
2021-02-13 01:24:54 +0530

    LC - 38

---
+++ src/main/java/com/zakcorp/final450/matrix/P_38.java
@@ -0,0 +1,13 @@
+package com.zakcorp.final450.matrix;
+
+import java.util.ArrayList;
+
+public class P_38 {
+    static class Solver {
+        // https://leetcode.com/problems/search-a-2d-matrix/
+        public ArrayList<Integer> solve1(int[][] matrix, int r, int c) {
+            // Refer  problem 74 from leetcode
+            return null;
+        }
+    }
+}
