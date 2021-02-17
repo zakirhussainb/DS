ab73beb11bd9ba8f8c76886d85a57615f3b61adf
user.email <zakirhussainb2693@gmail.com>
2021-02-17 20:23:14 +0530

    FINAL 21 - 1

--- src/main/java/com/zakcorp/final450/array/P_21.java
+++ src/main/java/com/zakcorp/final450/array/P_21.java
@@ -1,6 +1,7 @@
 package com.zakcorp.final450.array;
 
 import java.math.BigInteger;
+import java.util.*;
 
 /* Reverse the array */
 public class P_21 {
@@ -19,8 +20,19 @@ public class P_21 {
             }
             return false;
         }
+        // This below code can be re-written to find the sub array sum for a given K.
         public boolean solve2(int[] arr, int n) {
-
+            int k = 0;
+            int sum = 0;
+            Set<Integer> hSet = new HashSet<>();
+            for(int i = 0; i < n; i++) {
+                sum += arr[i];
+                if(arr[i] == k || sum == k || hSet.contains(sum)) {
+                    return true;
+                }
+                hSet.add(sum);
+            }
+            return false;
         }
     }
 }
--- src/test/java/com/zakcorp/final450/array/P_21_Test.java
+++ src/test/java/com/zakcorp/final450/array/P_21_Test.java
@@ -9,10 +9,12 @@ public class P_21_Test {
     @Test
     public void test1() {
         assertTrue(p.solve1(new int[]{4,2,-3,1,6}, 5));
+        assertTrue(p.solve2(new int[]{4,2,-3,1,6}, 5));
     }
 
     @Test
     public void test2() {
         assertTrue(p.solve1(new int[]{4,2,0,1,6}, 5));
+        assertTrue(p.solve2(new int[]{4,2,0,1,6}, 5));
     }
 }
