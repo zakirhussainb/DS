57b41f258d4edbe5767255bd6b468e772f4bc5d3
user.email <zakirhussainb2693@gmail.com>
2021-04-07 22:24:55 +0530

    P_196 2

--- src/main/java/com/zakcorp/final450/binarytree/P_196.java
+++ src/main/java/com/zakcorp/final450/binarytree/P_196.java
@@ -4,7 +4,11 @@ import com.zakcorp.trees.binarytree.BinaryTree;
 
 public class P_196 {
     /* Check if two tree are isomorphic
-     Note:- Being isomorphic is different from mirror/symmetric tree */
+    What is meant by isomorphism ?
+    1. Two trees are called isomorphic if one of them can be obtained from other by a series of flips,
+    i.e. by swapping left and right children of a number of nodes.
+    2. Even when two trees are same in structure, they are still isomorphic in nature.
+    Note:- Being isomorphic is different from mirror/symmetric tree */
     static class Solver {
         public boolean solve1(BinaryTree.Node t1, BinaryTree.Node t2) {
             if(t1 == null && t2 == null)
@@ -16,6 +20,8 @@ public class P_196 {
             if(t1.data != t2.data)
                 return false;
 
+            // Case 1: The subtrees rooted at these nodes have NOT been "Flipped".
+            // Case 2: The subtrees rooted at these nodes have been "Flipped"
             return ( ( solve1(t1.left, t2.left) && solve1(t1.right, t2.right) )
                     || ( solve1(t1.left, t2.right) && solve1(t1.right, t2.left) )
             );
--- src/test/java/com/zakcorp/final450/binarytree/P_196_Test.java
+++ src/test/java/com/zakcorp/final450/binarytree/P_196_Test.java
@@ -6,7 +6,9 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class P_196_Test {
+
     P_196.Solver p = new P_196.Solver();
+
     @Test
     public void test1() {
         BinaryTree.Node root1 = new BinaryTree.Node(1);
@@ -31,5 +33,21 @@ public class P_196_Test {
         root2.right.right.right = new BinaryTree.Node(7);
 
         assertTrue( p.solve1( root1, root2 ) );
+    }
+
+    @Test
+    public void test2() {
+        BinaryTree.Node root1 = new BinaryTree.Node(1);
+        root1.left = new BinaryTree.Node(2);
+        root1.right = new BinaryTree.Node(3);
+
+        root1.left.left = new BinaryTree.Node(4);
+        root1.left.right = new BinaryTree.Node(5);
+        root1.right.left = new BinaryTree.Node(6);
+
+        root1.left.right.left = new BinaryTree.Node(7);
+        root1.left.right.right = new BinaryTree.Node(8);
+
+        assertTrue( p.solve1( root1, root1 ) );
     }
 }
