6442e55982c790af8241b9a8e6279822d19aa0ba
user.email <zakirhussainb2693@gmail.com>
2021-04-14 09:37:14 +0530

    LC-767 1

--- src/main/java/com/zakcorp/leetcodemaster/Problem_767.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_767.java
@@ -14,11 +14,13 @@ public class Problem_767 {
         4. Finally fill all the other remaining characters in that array and return it.
         */
         public String solve1(String str) {
+            // Creating a hash array, containing the character and its count
             int n = str.length();
             int[] hash = new int[26];
             for(int i = 0; i < n; i++) {
                 hash[str.charAt(i) - 'a']++;
             }
+            // Find the maximum occurring character in the hash array.
             int max = 0, letterIndex = 0;
             for(int i = 0; i < hash.length; i++) {
                 if(hash[i] > max) {
@@ -26,24 +28,24 @@ public class Problem_767 {
                     letterIndex = i;
                 }
             }
-            if(max > (str.length() + 1) / 2) {
+
+            // If the max value is greater than this condition, then it is not possible to reorganize string in such alternating pattern.
+            if(max > (n + 1) / 2)
                 return "";
-            }
+            // Creating a result character array and inserting the maximum occurring character in alternating positions in this array.
             char[] resCharArr = new char[n];
             int idx = 0;
-            while(hash[letterIndex] > 0) {
+            while(hash[letterIndex] --> 0) {
                 resCharArr[idx] = (char)(letterIndex + 'a');
                 idx+=2;
-                hash[letterIndex]--;
             }
+            // Doing the above same process for the rest of the characters
             for(int i = 0; i < hash.length; i++) {
-                while(hash[i] > 0) {
-                    if(idx >= resCharArr.length) {
+                while(hash[i] --> 0) {
+                    if(idx >= resCharArr.length)
                         idx = 1;
-                    }
                     resCharArr[idx] = (char)(i + 'a');
                     idx+=2;
-                    hash[i]--;
                 }
             }
             return new String(resCharArr);
