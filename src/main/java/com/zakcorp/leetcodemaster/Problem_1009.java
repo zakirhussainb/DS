fef5a9614ffd714540a79026a439eff91739d032
user.email <zakirhussainb2693@gmail.com>
2021-04-16 07:24:55 +0530

    LC - 1009

--- src/main/java/com/zakcorp/leetcodemaster/Problem_1009.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_1009.java
@@ -21,16 +21,11 @@ public class Problem_1009 {
             // TODO:- Did not understand this method.
             if(N == 0)
                 return 1;
-            int ans = 0;
-            int curr = 1;
-            while(N != 0) {
-                if(N % 2 == 0) {
-                    ans += curr;
-                }
-                N /= 2;
-                curr *= 2;
+            int m = 0;
+            while(m < N) {
+                m = (m * 2) + 1; // is same as m = (m << 2) | 1
             }
-            return ans;
+            return m - N;
         }
     }
 }
