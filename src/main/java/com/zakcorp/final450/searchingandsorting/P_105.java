4e4836f1fb4d52a554b00a3c471da9272220b099
user.email <zakirhussainb2693@gmail.com>
2021-05-27 22:30:42 +0530

    P_105 1

--- src/main/java/com/zakcorp/final450/searchingandsorting/P_105.java
+++ src/main/java/com/zakcorp/final450/searchingandsorting/P_105.java
@@ -16,5 +16,63 @@ public class P_105 {
             }
             return pro;
         }
+
+        public long[] solve2(int[] arr, int n)
+        {
+            long[] L = new long[n];
+            long[] R = new long[n];
+            long[] result = new long[n];
+
+            L[0] = 1;
+            for(int i = 1; i < n; i++) {
+                L[i] = L[i - 1] * arr[i - 1];
+            }
+
+            R[n - 1] = 1;
+            for(int i = n - 2; i >= 0; i--) {
+                R[i] = R[i + 1] * arr[i + 1];
+            }
+
+            for(int i = 0; i < n; i++) {
+                result[i] = L[i] * R[i];
+            }
+
+            return result;
+        }
+
+        public long[] solve3(int[] arr, int n)
+        {
+            // The length of the input array
+            int length = arr.length;
+
+            // Final answer array to be returned
+            long[] answer = new long[length];
+
+            // answer[i] contains the product of all the elements to the left
+            // Note: for the element at index '0', there are no elements to the left,
+            // so the answer[0] would be 1
+            answer[0] = 1;
+            for (int i = 1; i < length; i++) {
+
+                // answer[i - 1] already contains the product of elements to the left of 'i - 1'
+                // Simply multiplying it with nums[i - 1] would give the product of all
+                // elements to the left of index 'i'
+                answer[i] = arr[i - 1] * answer[i - 1];
+            }
+
+            // R contains the product of all the elements to the right
+            // Note: for the element at index 'length - 1', there are no elements to the right,
+            // so the R would be 1
+            int R = 1;
+            for (int i = length - 1; i >= 0; i--) {
+
+                // For the index 'i', R would contain the
+                // product of all elements to the right. We update R accordingly
+                answer[i] = answer[i] * R;
+                R *= arr[i];
+            }
+
+            return answer;
+        }
     }
 }
--- src/test/java/com/zakcorp/final450/searchingandsorting/P_105_Test.java
+++ src/test/java/com/zakcorp/final450/searchingandsorting/P_105_Test.java
@@ -9,6 +9,19 @@ public class P_105_Test {
     @Test
     public void test1() {
         assertArrayEquals(new long[]{180,600,360,300,900}, p.solve1(new int[]{10, 3, 5, 6, 2}, 5));
-//        assertArrayEquals(new long[]{180,600,360,300,900}, p.solve2(new int[]{10, 3, 5, 6, 2}, 5));
+        assertArrayEquals(new long[]{180,600,360,300,900}, p.solve2(new int[]{10, 3, 5, 6, 2}, 5));
+        assertArrayEquals(new long[]{180,600,360,300,900}, p.solve3(new int[]{10, 3, 5, 6, 2}, 5));
+    }
+    @Test
+    public void test2() {
+        assertArrayEquals(new long[]{24,12,8,6}, p.solve1(new int[]{1,2,3,4}, 4));
+        assertArrayEquals(new long[]{24,12,8,6}, p.solve2(new int[]{1,2,3,4}, 4));
+        assertArrayEquals(new long[]{24,12,8,6}, p.solve3(new int[]{1,2,3,4}, 4));
+    }
+    @Test
+    public void test3() {
+        assertArrayEquals(new long[]{0,0,9,0,0}, p.solve1(new int[]{-1,1,0,-3,3}, 5));
+        assertArrayEquals(new long[]{0,0,9,0,0}, p.solve2(new int[]{-1,1,0,-3,3}, 5));
+        assertArrayEquals(new long[]{0,0,9,0,0}, p.solve3(new int[]{-1,1,0,-3,3}, 5));
     }
 }
