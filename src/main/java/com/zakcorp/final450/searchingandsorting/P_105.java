ec6da9a80df347bbc243415b2049e2a50e653450
user.email <zakirhussainb2693@gmail.com>
2021-05-27 21:35:52 +0530

    P_105

--- src/main/java/com/zakcorp/final450/searchingandsorting/P_105.java
+++ src/main/java/com/zakcorp/final450/searchingandsorting/P_105.java
@@ -2,37 +2,19 @@ package com.zakcorp.final450.searchingandsorting;
 
 public class P_105 {
     static class Solver {
+        // O(N ^ 2) time and O(1) space
         public long[] solve1(int[] arr, int n)
         {
-            long[] output = new long[n];
+            long[] pro = new long[n];
             for(int i = 0; i < n; i++) {
-                long prod = 1;
+                pro[i] = 1L;
                 for(int j = 0; j < n; j++) {
                     if(i != j) {
-                        prod *= arr[j];
+                        pro[i] *= arr[j];
                     }
                 }
-                output[i] = prod;
             }
-            return output;
+            return pro;
         }
-        public long[] solve2(int[] arr, int n)
-        {
-//            long[] outputArr = new long[n];
-//            long[] leftArr = new long[n];
-//            int prod = 1;
-//            for(int i = 0; i < n; i++) {
-//                prod *= arr[i];
-//                leftArr[i] = prod;
-//            }
-//            long[] rightArr = new long[n];
-//            prod = 1;
-//            for(int i = n - 1; i >= 0; i--) {
-//                prod *= arr[i];
-//                rightArr[i] = prod;
-//            }
-            return null;
-        }
-
     }
 }
--- src/test/java/com/zakcorp/final450/searchingandsorting/P_105_Test.java
+++ src/test/java/com/zakcorp/final450/searchingandsorting/P_105_Test.java
@@ -9,6 +9,6 @@ public class P_105_Test {
     @Test
     public void test1() {
         assertArrayEquals(new long[]{180,600,360,300,900}, p.solve1(new int[]{10, 3, 5, 6, 2}, 5));
-        assertArrayEquals(new long[]{180,600,360,300,900}, p.solve2(new int[]{10, 3, 5, 6, 2}, 5));
+//        assertArrayEquals(new long[]{180,600,360,300,900}, p.solve2(new int[]{10, 3, 5, 6, 2}, 5));
     }
 }
