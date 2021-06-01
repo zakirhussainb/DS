557966b99baf59e1f29089604324b506394f0095
user.email <zakirhussainb2693@gmail.com>
2021-06-02 03:04:38 +0530

    LC - 377 - 2

--- src/main/java/com/zakcorp/leetcodemaster/Problem_377.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_377.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Problem_377 {
+    // TLE for large inputs
     static class Solver1 {
         private int counter = 0;
         public int solve1(int[] arr, int target) {
@@ -29,6 +30,31 @@ public class Problem_377 {
                 }
             }
             return -1;
+        }
+    }
+    // Use a HashMap instead of an memo[] arr, because this is more efficient
+    static class Solver2 {
+        private HashMap<Integer, Integer> memo;
+        public int solve1(int[] arr, int target) {
+            memo = new HashMap<>();
+            return comb(arr, target);
+        }
+        private int comb(int[] arr, int target) {
+            if(target == 0) {
+                return 1;
+            }
+            if (memo.containsKey(target))
+                return memo.get(target);
+
+            int result = 0;
+            for(int num : arr) {
+                int diff = target - num;
+                if(diff >= 0) {
+                    result += comb(arr, diff);
+                }
+            }
+            memo.put(target, result);
+            return result;
         }
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_377_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_377_Test.java
@@ -6,8 +6,15 @@ import static org.junit.Assert.*;
 
 public class Problem_377_Test {
     Problem_377.Solver1 p = new Problem_377.Solver1();
+    Problem_377.Solver2 p2 = new Problem_377.Solver2();
     @Test
     public void test1() {
         assertEquals(7, p.solve1(new int[]{1, 2, 3}, 4));
+        assertEquals(7, p2.solve1(new int[]{1, 2, 3}, 4));
+    }
+    @Test
+    public void test2() {
+//        assertEquals(7, p.solve1(new int[]{1, 2, 3}, 4));
+        assertEquals(545813094, p2.solve1(new int[]{5, 2}, 100));
     }
 }
