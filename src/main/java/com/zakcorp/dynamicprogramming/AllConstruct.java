ebc2fac9d0a01d5be40fe1e6c8b45052acabbf30
user.email <zakirhussainb2693@gmail.com>
2021-06-04 01:32:04 +0530

    DP - AllConstruct

---
+++ src/main/java/com/zakcorp/dynamicprogramming/AllConstruct.java
@@ -0,0 +1,51 @@
+package com.zakcorp.dynamicprogramming;
+
+import java.util.*;
+
+public class AllConstruct {
+    static class Solver {
+        public List<List<String>> solve1(String targetStr, List<String> wordDict) {
+            if(targetStr.isEmpty()) {
+                return Collections.singletonList(new ArrayList<>());
+            }
+            List<List<String>> resultList = new ArrayList<>();
+            for(String word : wordDict) {
+                if(targetStr.startsWith(word)) {
+                    String suffix = targetStr.substring(word.length());
+                    List<List<String>> ways = solve1(suffix, wordDict);
+                    for(List<String> list : ways) {
+                        list.add(0, word);
+                    }
+                    resultList.addAll(ways);
+                }
+            }
+            return resultList;
+        }
+    }
+    static class Solver1 {
+        public List<List<String>> solve1(String targetStr, List<String> wordDict) {
+            return memoized(targetStr, wordDict, new HashMap<>());
+        }
+        public List<List<String>> memoized(String targetStr, List<String> wordDict, HashMap<String, List<List<String>>> memo) {
+            if(memo.containsKey(targetStr)) {
+                return memo.get(targetStr);
+            }
+            if(targetStr.isEmpty()) {
+                return Collections.singletonList(new ArrayList<>());
+            }
+            List<List<String>> resultList = new ArrayList<>();
+            for(String word : wordDict) {
+                if(targetStr.startsWith(word)) {
+                    String suffix = targetStr.substring(word.length());
+                    List<List<String>> ways = memoized(suffix, wordDict, memo);
+                    for(List<String> list : ways) {
+                        list.add(0, word);
+                    }
+                    resultList.addAll(ways);
+                }
+            }
+            memo.put(targetStr, resultList);
+            return resultList;
+        }
+    }
+}
---
+++ src/test/java/com/zakcorp/dynamicprogramming/AllConstruct_Test.java
@@ -0,0 +1,30 @@
+package com.zakcorp.dynamicprogramming;
+
+import org.junit.Test;
+
+import java.util.*;
+import static org.junit.Assert.assertEquals;
+
+public class AllConstruct_Test {
+    AllConstruct.Solver p = new AllConstruct.Solver();
+    AllConstruct.Solver1 p1 = new AllConstruct.Solver1();
+//    HowSum.Solver2 p2 = new HowSum.Solver2();
+
+    @Test
+    public void test1() {
+        List<List<String>> expected = new ArrayList<>();
+        expected.add(Arrays.asList("ab", "cd", "ef"));
+        expected.add(Arrays.asList("ab", "c", "def"));
+        expected.add(Arrays.asList("abc", "def"));
+        expected.add(Arrays.asList("abcd", "ef"));
+        assertEquals( expected, p.solve1("abcdef", Arrays.asList("ab", "abc", "cd", "def", "abcd", "ef", "c") ));
+        assertEquals( expected, p1.solve1("abcdef", Arrays.asList("ab", "abc", "cd", "def", "abcd", "ef", "c") ));
+    }
+    @Test
+    public void test3() {
+//        assertEquals( 0, p.solve1("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab",
+//                Arrays.asList("a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa") ));
+        assertEquals( new ArrayList<>(), p1.solve1("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab",
+                Arrays.asList("a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa") ));
+    }
+}
