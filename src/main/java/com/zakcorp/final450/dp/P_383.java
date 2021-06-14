ffc2537e61a36a7f4f373a6cf2fb2a437401f268
user.email <zakirhussainb2693@gmail.com>
2021-06-14 09:14:14 +0530

    FINAL-450 - P_383 - 1

--- src/main/java/com/zakcorp/final450/dp/P_383.java
+++ src/main/java/com/zakcorp/final450/dp/P_383.java
@@ -24,8 +24,21 @@ public class P_383 {
         }
 
         // Using Binomial Coefficient - Time - Linear - O(N)
-        public int solve3() {
-            return 0;
+        public int solve3(int n) {
+            // Based on formula - 2nCn / n + 1
+            long cn = binomialCoefficient(2 * n, n);
+            return (int) (cn / (n + 1));
+        }
+        private long binomialCoefficient(int n, int r) {
+            long[] factDp = new long[n + 1];
+            factDp[0] = 1;
+            for(int i = 1; i <= n; i++) {
+                factDp[i] = i * factDp[i - 1];
+            }
+            long nF = factDp[n];
+            long nrF = factDp[n - r];
+            long rF = factDp[r];
+            return nF / (nrF * rF);
         }
     }
 }
--- src/test/java/com/zakcorp/final450/dp/P_383_Test.java
+++ src/test/java/com/zakcorp/final450/dp/P_383_Test.java
@@ -9,13 +9,16 @@ public class P_383_Test {
     @Test
     public void test1() {
         assertEquals(2, p.solve2(2) );
+        assertEquals(2, p.solve3(2) );
     }
     @Test
     public void test2() {
         assertEquals(14, p.solve2(4) );
+        assertEquals(14, p.solve3(4) );
     }
     @Test
     public void test3() {
         assertEquals(42, p.solve2(5) );
+        assertEquals(42, p.solve3(5) );
     }
 }
