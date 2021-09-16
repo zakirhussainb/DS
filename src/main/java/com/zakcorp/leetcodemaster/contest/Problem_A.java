982850ced4d5e507db02cade834a55b5b8a2d32c
zb <zakirhussain.b@blackboard.com>
2021-09-16 13:16:46 +0530

    Coderbyte problem

--- src/main/java/com/zakcorp/leetcodemaster/contest/Problem_A.java
+++ src/main/java/com/zakcorp/leetcodemaster/contest/Problem_A.java
@@ -7,15 +7,20 @@ import java.util.Map;
 
 public class Problem_A {
     static class Solver {
-        public boolean solve1(String firstWord, String secondWord, String targetWord) {
-            return getSum(firstWord) + getSum(secondWord) == getSum(targetWord);
-        }
-        private int getSum(String str) {
-            String val = "";
-            for(int i = 0; i < str.length(); i++) {
-                val += str.charAt(i) - 'a';
+        public String solve1(String str) {
+            int n = str.length();
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < n; i++) {
+                int count = 1;
+                while( i < n - 1 && str.charAt(i) == str.charAt(i + 1) ) {
+                    count++;
+                    i++;
+                }
+                sb.append(count);
+                sb.append(str.charAt(i));
             }
-            return Integer.parseInt(val);
+
+            return sb.toString();
         }
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/contest/Problem_A_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/contest/Problem_A_Test.java
@@ -8,14 +8,6 @@ public class Problem_A_Test {
     Problem_A.Solver p = new Problem_A.Solver();
     @Test
     public void test1() {
-        assertTrue( p.solve1("acb", "cba", "cdb"));
-    }
-    @Test
-    public void test2() {
-        assertFalse( p.solve1("aaa", "a", "aab"));
-    }
-    @Test
-    public void test3() {
-        assertTrue( p.solve1("aaa", "a", "aaaa"));
+        assertEquals( "2a2b1c1d1e1a", p.solve1("aabbcdea"));
     }
 }
