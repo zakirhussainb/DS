15ff1a98d20230b2f64ddda24674b1111866f8eb
zb <zakirhussain.b@blackboard.com>
2021-10-06 14:38:51 +0530

    Updated

---
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_118.java
@@ -0,0 +1,15 @@
+package com.zakcorp.leetcodemaster;
+
+import java.util.*;
+
+public class Problem_118 {
+    static class Solver {
+//        public List<List<Integer>> solve1(int numRows) {
+//            List<List<Integer>> res = new ArrayList<>();
+//            res.add(List.of(1));
+//            for(int i = 1; i < numRows; i++) {
+//
+//            }
+//        }
+    }
+}
