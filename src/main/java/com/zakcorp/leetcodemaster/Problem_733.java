da1d7535512a6c435363803981309dbd966b63a3
zakirhussainb <zakirhussainb2693@gmail.com>
2022-03-16 17:29:01 +0530

    P_733 BFS soln

--- src/main/java/com/zakcorp/leetcodemaster/Problem_733.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_733.java
@@ -1,5 +1,7 @@
 package com.zakcorp.leetcodemaster;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 /**
@@ -38,14 +40,14 @@ public class Problem_733 {
             if(image[sr][sc] == newColor) {
                 return image;
             }
-            int temp = image[sr][sc];
+            int color = image[sr][sc];
             Stack<int[]> stack = new Stack<>();
             stack.push(new int[]{sr, sc});
             while( !stack.isEmpty() ) {
                 int[] colorIndex = stack.pop();
                 int x = colorIndex[0];
                 int y = colorIndex[1];
-                if( image[x][y] == temp ) {
+                if( image[x][y] == color ) {
                     image[x][y] = newColor;
                     if(x + 1 < image.length)
                         stack.push(new int[]{x + 1, y});
@@ -55,6 +57,33 @@ public class Problem_733 {
                         stack.push(new int[]{x - 1, y});
                     if(y - 1 >= 0)
                         stack.push(new int[]{x, y - 1});
+                }
+            }
+            return image;
+        }
+    }
+
+    static class BFS {
+        public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+            int color = image[sr][sc];
+            if(color == newColor)
+                return image;
+            Queue<int[]> queue = new LinkedList<>();
+            queue.add(new int[]{sr, sc});
+            while( !queue.isEmpty() ) {
+                int[] colorIndex = queue.poll();
+                int x = colorIndex[0];
+                int y = colorIndex[1];
+                if(image[x][y] == color) {
+                    image[x][y] = newColor;
+                    if(x + 1 < image.length)
+                        queue.add(new int[]{x + 1, y});
+                    if(x - 1 >= 0)
+                        queue.add(new int[]{x - 1, y});
+                    if(y + 1 < image[0].length)
+                        queue.add(new int[]{x, y + 1});
+                    if(y - 1 >= 0)
+                        queue.add(new int[]{x, y - 1});
                 }
             }
             return image;
--- src/test/java/com/zakcorp/leetcodemaster/Problem_733_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_733_Test.java
@@ -7,6 +7,8 @@ import static org.junit.Assert.assertArrayEquals;
 public class Problem_733_Test {
     Problem_733.DFSRecursive p = new Problem_733.DFSRecursive();
     Problem_733.DFSIterative p1 = new Problem_733.DFSIterative();
+    Problem_733.BFS p2 = new Problem_733.BFS();
+
     @Test
     public void test1() {
         assertArrayEquals(new int[][]{ {2, 2, 2}, {2, 2, 0}, {2, 0, 1} },
@@ -14,5 +16,20 @@ public class Problem_733_Test {
 
         assertArrayEquals(new int[][]{ {2, 2, 2}, {2, 2, 0}, {2, 0, 1} },
                 p1.floodFill(new int[][]{ {1, 1, 1}, {1, 1, 0}, {1, 0, 1} }, 1, 1, 2));
+
+        assertArrayEquals(new int[][]{ {2, 2, 2}, {2, 2, 0}, {2, 0, 1} },
+                p2.floodFill(new int[][]{ {1, 1, 1}, {1, 1, 0}, {1, 0, 1} }, 1, 1, 2));
+    }
+
+    @Test
+    public void test2() {
+        assertArrayEquals(new int[][]{{0, 0, 0}, {0, 1, 1}},
+                p.floodFill(new int[][]{{0, 0, 0}, {0, 1, 1}}, 1, 1, 1));
+
+        assertArrayEquals(new int[][]{{0, 0, 0}, {0, 1, 1}},
+                p1.floodFill(new int[][]{{0, 0, 0}, {0, 1, 1}}, 1, 1, 1));
+
+        assertArrayEquals(new int[][]{{0, 0, 0}, {0, 1, 1}},
+                p2.floodFill(new int[][]{{0, 0, 0}, {0, 1, 1}}, 1, 1, 1));
     }
 }
