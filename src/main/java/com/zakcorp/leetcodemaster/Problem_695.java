2d2a7c2cb0ecaee304112a46352cfd212f5b5630
zakirhussainb <zakirhussainb2693@gmail.com>
2022-03-20 10:54:45 +0530

    P_695 DFS Iterative

--- src/main/java/com/zakcorp/leetcodemaster/Problem_695.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_695.java
@@ -1,38 +1,39 @@
 package com.zakcorp.leetcodemaster;
 
+import java.util.Stack;
+
 public class Problem_695 {
-    static class Solver {
-        int max = 0;
+    static class DFSRecursive {
         public int solve1(int[][] grid) {
+            int maxArea = 0;
             for(int i = 0; i < grid.length; i++) {
                 for(int j = 0; j < grid[i].length; j++) {
                     if(grid[i][j] == 1) {
                         int[] sum = {0};
                         dfs(grid, i, j, sum);
+                        maxArea = Math.max(maxArea, sum[0]);
                     }
                 }
             }
-            return max;
+            return maxArea;
         }
         private void dfs(int[][] grid, int r, int c, int[] sum) {
-            if(r < 0 || c < 0 || r >= grid.length || c >= grid[r].length) {
+            if(r < 0 || c < 0 || r >= grid.length || c >= grid[r].length)
                 return;
-            }
-            if(grid[r][c] == 0) {
+
+            if(grid[r][c] == 0)
                 return;
-            }
-            if(grid[r][c] == 1) {
-                sum[0]++;
-                max = Math.max(max, sum[0]);
-            }
+
+            sum[0]++;
             grid[r][c] = 0;
+
             dfs(grid, r + 1, c, sum);
             dfs(grid, r - 1, c, sum);
             dfs(grid, r, c + 1, sum);
             dfs(grid, r, c - 1, sum);
         }
     }
-    static class Solver1 {
+    static class DFSRecursive1 {
         public int maxAreaOfIsland(int[][] grid) {
             int maxArea = 0;
             for(int r = 0; r < grid.length; r++) {
@@ -53,6 +54,43 @@ public class Problem_695 {
             grid[r][c] = 0;
             return dfs(grid, r + 1, c) + dfs(grid, r - 1, c)
                     + dfs(grid, r, c + 1) + dfs(grid, r, c - 1) + 1;
+        }
+    }
+    static class DFSIterative {
+        public int maxAreaOfIsland(int[][] grid) {
+            int maxArea = 0;
+            for(int r = 0; r < grid.length; r++) {
+                for(int c = 0; c < grid[0].length; c++) {
+                    if(grid[r][c] == 1) {
+                        int area = dfs(grid, r, c);
+                        maxArea = Math.max(maxArea, area);
+                    }
+                }
+            }
+            return maxArea;
+        }
+        private int dfs(int[][] grid, int i, int j) {
+            int area = 0;
+            Stack<int[]> stack = new Stack<>();
+            stack.push(new int[]{i, j});
+            while( !stack.isEmpty() ) {
+                int[] index = stack.pop();
+                int r = index[0];
+                int c = index[1];
+                if(grid[r][c] == 1) {
+                    if(r + 1 < grid.length)
+                        stack.push(new int[]{r + 1, c});
+                    if(r - 1 >= 0)
+                        stack.push(new int[]{r - 1, c});
+                    if(c + 1 < grid[0].length)
+                        stack.push(new int[]{r, c + 1});
+                    if(c - 1 >= 0)
+                        stack.push(new int[]{r, c - 1});
+                    area++;
+                    grid[r][c] = 0;
+                }
+            }
+            return area;
         }
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_695_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_695_Test.java
@@ -5,8 +5,9 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class Problem_695_Test {
-    Problem_695.Solver p = new Problem_695.Solver();
-    Problem_695.Solver1 p1 = new Problem_695.Solver1();
+    Problem_695.DFSRecursive p = new Problem_695.DFSRecursive();
+    Problem_695.DFSRecursive1 p1 = new Problem_695.DFSRecursive1();
+    Problem_695.DFSIterative p2 = new Problem_695.DFSIterative();
 
     @Test
     public void test1() {
@@ -30,6 +31,16 @@ public class Problem_695_Test {
                 {0,0,0,0,0,0,0,1,1,1,0,0,0},
                 {0,0,0,0,0,0,0,1,1,0,0,0,0}
         }));
+        assertEquals(6, p2.maxAreaOfIsland(new int[][]{
+                {0,0,1,0,0,0,0,1,0,0,0,0,0},
+                {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                {0,1,1,0,1,0,0,0,0,0,0,0,0},
+                {0,1,0,0,1,1,0,0,1,0,1,0,0},
+                {0,1,0,0,1,1,0,0,1,1,1,0,0},
+                {0,0,0,0,0,0,0,0,0,0,1,0,0},
+                {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                {0,0,0,0,0,0,0,1,1,0,0,0,0}
+        }));
     }
     @Test
     public void test2() {
@@ -38,6 +49,15 @@ public class Problem_695_Test {
         }));
         assertEquals(0, p1.maxAreaOfIsland(new int[][]{
                 {0,0,0,0,0,0,0,0}
+        }));
+    }
+    @Test
+    public void test3() {
+        assertEquals(0, p2.maxAreaOfIsland(new int[][]{
+                {1, 0, 0},
+                {0, 1, 0},
+                {0, 1, 1},
+                {1, 1, 1}
         }));
     }
 }
