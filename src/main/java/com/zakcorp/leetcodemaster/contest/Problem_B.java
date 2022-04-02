1cae5380e5dfebd7cfb7e90806ce509cf7a75e86
zakirhussainb <zakirhussainb2693@gmail.com>
2022-04-02 20:46:31 +0530

    Contest - Biweekly 75 - Problem B

--- src/main/java/com/zakcorp/leetcodemaster/contest/Problem_B.java
+++ src/main/java/com/zakcorp/leetcodemaster/contest/Problem_B.java
@@ -1,33 +1,31 @@
 package com.zakcorp.leetcodemaster.contest;
 
-import com.zakcorp.graphs.algorithms.PrimsAlgorithm;
-import com.zakcorp.leetcodemaster.Problem_207;
-
 import java.util.*;
 
 public class Problem_B {
     static class Solver {
-        public String solve1(String str, int x) {
-            StringBuilder res = new StringBuilder();
-            int m = str.length();
-            for(int i = 1; i < m; i++) {
-                if(str.charAt(i - 1)  - '0' <= x && str.charAt(i)  - '0' <= x) {
-                    System.out.println("ONE");
-                    res.append(str.charAt(i - 1));
-                    res.append(x);
-                } else if(str.charAt(i - 1)  - '0' <= x && str.charAt(i)  - '0' >= x) {
-                    System.out.println("TWO");
-                } else if(str.charAt(i - 1)  - '0' >= x && str.charAt(i)  - '0' <= x) {
-                    System.out.println("THREE");
-                    res.append(str.charAt(i - 1));
-                } else if(str.charAt(i - 1)  - '0' >= x && str.charAt(i)  - '0' >= x) {
-                    System.out.println("FOUR");
-                    res.append(str.charAt(i - 1));
-                }
+        public int solve1(int[] arr) {
+            int n = arr.length;
+            if(n == 1)
+                return arr[0];
+            Queue<Integer> queue = new LinkedList<>();
+            for(int j = 1; j < n; j++) {
+                int val = (arr[j - 1] + arr[j]) % 10;
+                queue.add(val);
             }
-            res.append(str.charAt(str.length() - 1));
-            res.append(x);
-            return res.toString();
+            for(int k = 2; k < n; k++) {
+                int size = queue.size();
+                int z = 1;
+                while(z < size) {
+                    int x = queue.isEmpty() ? 0 : queue.poll();
+                    int y = queue.isEmpty() ? 0 : queue.peek();
+                    int val = (x + y) % 10;
+                    queue.add(val);
+                    z++;
+                }
+                queue.poll();
+            }
+            return queue.isEmpty() ? 0 : queue.peek();
         }
     }
 }
