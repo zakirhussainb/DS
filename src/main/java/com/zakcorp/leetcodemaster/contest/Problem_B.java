6082bc237087c30c91ef5ec736bca8188f6d3450
zakirhussainb <zakirhussainb2693@gmail.com>
2022-05-28 21:50:35 +0530

    BWC - 79 Problem B

--- src/main/java/com/zakcorp/leetcodemaster/contest/Problem_B.java
+++ src/main/java/com/zakcorp/leetcodemaster/contest/Problem_B.java
@@ -1,73 +1,35 @@
 package com.zakcorp.leetcodemaster.contest;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Problem_B {
-    static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode() {
+    static class Solver {
+        public String solve1(String[] messages, String[] senders) {
+            Map<String, Integer> map = new HashMap<>();
+            for(int i = 0; i < senders.length; i++) {
+                int wordCount = getWordsCountInMessage(messages[i]);
+                if(!map.containsKey(senders[i])) {
+                    map.put(senders[i], wordCount);
+                } else {
+                    map.put(senders[i], map.get(senders[i]) + wordCount);
+                }
+            }
+            PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>( new SortByValuesThenByKeyComparator() );
+            pq.addAll( map.entrySet() );
+            return pq.poll().getKey();
         }
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
+        private int getWordsCountInMessage(String message) {
+            String[] words = message.split("\\s+");
+            return words.length;
         }
     }
-    static class Solver {
-
-        // for each node in the tree, we will maintain three values
-        class State {
-            // count of nodes in the subtree
-            int nodeCount;
-
-            // sum of values in the subtree
-            int valueSum;
-
-            // max average found in the subtree
-            int average;
-
-            State(int nodes, int sum, int average) {
-                this.nodeCount = nodes;
-                this.valueSum = sum;
-                this.average = average;
-            }
-        }
-
-        public int solve1(TreeNode root) {
-            int[] counter = new int[]{0};
-            helper(root, counter);
-            return counter[0];
-        }
-
-        private State helper(TreeNode root, int[] counter) {
-
-            if (root == null) {
-                return new State(0, 0, 0);
-            }
-
-            // postorder traversal, solve for both child nodes first.
-            State left = helper(root.left, counter);
-            State right = helper(root.right, counter);
-
-            // now find nodeCount, valueSum and maxAverage for current node `root`
-            int nodeCount = left.nodeCount + right.nodeCount + 1;
-            int sum = left.valueSum + right.valueSum + root.val;
-            int average = sum / nodeCount;
-
-            if(average == root.val) {
-                counter[0]++;
-            }
-
-            return new State(nodeCount, sum, average);
+    /*  IF TWO ENTRIES HAVE SAME VALUES THEN
+      -> THIS COMPARATOR WILL FIRST SORT IN DESCENDING ORDER OF VALUES
+      -> THEN IT WILL SORT IN DESCENDING ORDER OF KEYS -> i.e. lexicographically the largest word*/
+    static class SortByValuesThenByKeyComparator implements Comparator<Map.Entry<String, Integer>> {
+        @Override
+        public int compare(Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) {
+            return e1.getValue().equals(e2.getValue()) ? e2.getKey().compareTo(e1.getKey()) : e2.getValue() - e1.getValue();
         }
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/contest/Problem_B_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/contest/Problem_B_Test.java
@@ -8,18 +8,34 @@ public class Problem_B_Test {
     Problem_B.Solver p = new Problem_B.Solver();
     @Test
     public void test1() {
-        Problem_B.TreeNode t = new Problem_B.TreeNode(4);
-        t.left = new Problem_B.TreeNode(8);
-        t.left.left = new Problem_B.TreeNode(0);
-        t.left.right = new Problem_B.TreeNode(1);
-        t.right = new Problem_B.TreeNode(5);
-        t.right.right = new Problem_B.TreeNode(6);
-        assertEquals( 5, p.solve1(t));
+        assertEquals("Alice", p.solve1(new String[]{"Hello userTwooo","Hi userThree","Wonderful day Alice","Nice day userThree"},
+                new String[]{"Alice","userTwo","userThree","Alice"}));
     }
-
     @Test
     public void test2() {
-        Problem_B.TreeNode t = new Problem_B.TreeNode(1);
-        assertEquals( 1, p.solve1(t));
+        assertEquals("Charlie", p.solve1(new String[]{"How is leetcode for everyone","Leetcode is useful for practice"},
+                new String[]{"Bob","Charlie"}));
+    }
+    @Test
+    public void test3() {
+        assertEquals("o", p.solve1(new String[]{"p mmV","Lb WE u H x f YT","Rey","q","Y R","Ng","mP lo oA l jd y","CP y r N wHA","Bt l","Y S xv MNS","WG vz","S pl l Dz fX D TpI","zGq","z S D"},
+                new String[]{"QMnVn","QMnVn","nkq","o","EEM","QMnVn","o","nkq","Gd","Gd","nkq","o","EEM","EEM"}));
+    }
+    @Test
+    public void test4() {
+        assertEquals("FnZd", p.solve1(new String[]{"tP x M VC h lmD","D X XF w V","sh m Pgl","pN pa","C SL m G Pn v","K z UL B W ee","Yf yo n V U Za f np","j J sk f qr e v t","L Q cJ c J Z jp E","Be a aO","nI c Gb k Y C QS N","Yi Bts","gp No g s VR","py A S sNf","ZS H Bi De dj dsh","ep MA KI Q Ou"},
+                new String[]{"OXlq","IFGaW","XQPeWJRszU","Gb","HArIr","Gb","FnZd","FnZd","HArIr","OXlq","IFGaW","XQPeWJRszU","EMoUs","Gb","EMoUs","EMoUs"}));
+    }
+    @Test
+    public void test5() {
+        assertEquals("a", p.solve1(new String[]{"a"},
+                new String[]{"a"}));
+    }
+    @Test
+    public void test6() {
+        assertEquals("zADZhwSwFj", p.solve1(new String[]{"l","E","e","L","U","B","H","H","l","I","E","c","s","t","s","u","r","F","Q","v","K","x","M","r","o","k","o","E","x","f"},
+                new String[]{"TYproPIptp","ZinKBnptgb","MabHvYDOvr","KJsXlcnoLX","IMjvhaCNQU","qWTfyhzuge","CBCPpuHdKb","HpDxmDTUwp","SgOTFxFYpl","PctQGYkMuz","KqAEViZnnc","DVTKOJLZPc","InJQqHsICO",
+                        "bNhncDzkLu","IvbKSkMTWM","nOjJjZjQTa","ekqBBwyCYb","zADZhwSwFj","LswFXzVDyF","bhIcpZBHWb","mesrFSlmqJ","OZZKylOPUj","OVDIlCRBro","IIhwLrjgHo","HrXZYYiXnT","pmHsmhKLoB","ppIkHHnBqQ",
+                        "gNGHfhhxEL","VSZuisvhuQ","qaPorTEGcL"}));
     }
 }
