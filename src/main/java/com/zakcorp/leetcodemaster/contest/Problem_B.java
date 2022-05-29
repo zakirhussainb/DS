c1d8db5ef962548abb7d8878b663f0b901fc2d98
zakirhussainb <zakirhussainb2693@gmail.com>
2022-05-29 13:06:29 +0530

    WC - 295 P_B

--- src/main/java/com/zakcorp/leetcodemaster/contest/Problem_B.java
+++ src/main/java/com/zakcorp/leetcodemaster/contest/Problem_B.java
@@ -1,35 +1,44 @@
 package com.zakcorp.leetcodemaster.contest;
 
-import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Problem_B {
     static class Solver {
-        public String solve1(String[] messages, String[] senders) {
-            Map<String, Integer> map = new HashMap<>();
-            for(int i = 0; i < senders.length; i++) {
-                int wordCount = getWordsCountInMessage(messages[i]);
-                if(!map.containsKey(senders[i])) {
-                    map.put(senders[i], wordCount);
-                } else {
-                    map.put(senders[i], map.get(senders[i]) + wordCount);
+        private static final String regex = "\\$[0-9\\(\\)]+$";
+        public String solve1(String sentence, int discount) {
+            String[] split = sentence.split("\\s+");
+            for(int i = 0; i < split.length; i++) {
+                long count = split[i].chars().filter(ch -> ch == '$').count();
+                if(count > 1) {
+                    continue;
+                }
+                Pattern pattern = Pattern.compile(regex);
+                Matcher matcher = pattern.matcher(split[i]);
+                boolean matchFound = matcher.find();
+                StringBuilder sb = new StringBuilder();
+                if(matchFound) {
+                    if(discount == 100) {
+                        split[i] = "$0.00";
+                    } else {
+                        long num = Long.parseLong(split[i].substring(1));
+                        double value = (double) num * (discount / 100.0);
+                        value = num - value;
+                        String r = String.format("%1$,.2f", value);
+                        r = r.replaceAll(",", "");
+                        sb.append("$");
+                        sb.append(r);
+                        split[i] = sb.toString();
+                    }
                 }
             }
-            PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>( new SortByValuesThenByKeyComparator() );
-            pq.addAll( map.entrySet() );
-            return pq.poll().getKey();
-        }
-        private int getWordsCountInMessage(String message) {
-            String[] words = message.split("\\s+");
-            return words.length;
-        }
-    }
-    /*  IF TWO ENTRIES HAVE SAME VALUES THEN
-      -> THIS COMPARATOR WILL FIRST SORT IN DESCENDING ORDER OF VALUES
-      -> THEN IT WILL SORT IN DESCENDING ORDER OF KEYS -> i.e. lexicographically the largest word*/
-    static class SortByValuesThenByKeyComparator implements Comparator<Map.Entry<String, Integer>> {
-        @Override
-        public int compare(Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) {
-            return e1.getValue().equals(e2.getValue()) ? e2.getKey().compareTo(e1.getKey()) : e2.getValue() - e1.getValue();
+            StringBuilder res = new StringBuilder();
+            for(int i = 0; i < split.length; i++) {
+                res.append(split[i]);
+                res.append(" ");
+            }
+            res.deleteCharAt(res.length() - 1);
+            return res.toString();
         }
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/contest/Problem_B_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/contest/Problem_B_Test.java
@@ -8,34 +8,32 @@ public class Problem_B_Test {
     Problem_B.Solver p = new Problem_B.Solver();
     @Test
     public void test1() {
-        assertEquals("Alice", p.solve1(new String[]{"Hello userTwooo","Hi userThree","Wonderful day Alice","Nice day userThree"},
-                new String[]{"Alice","userTwo","userThree","Alice"}));
+        assertEquals("there are $0.50 $1.00 and 5$ candies in the shop",
+                p.solve1("there are $1 $2 and 5$ candies in the shop", 50));
     }
     @Test
     public void test2() {
-        assertEquals("Charlie", p.solve1(new String[]{"How is leetcode for everyone","Leetcode is useful for practice"},
-                new String[]{"Bob","Charlie"}));
+        assertEquals("1 2 $0.00 4 $0.00 $0.00 7 8$ $0.00 $10$",
+                p.solve1("1 2 $3 4 $5 $6 7 8$ $9 $10$", 100));
     }
     @Test
     public void test3() {
-        assertEquals("o", p.solve1(new String[]{"p mmV","Lb WE u H x f YT","Rey","q","Y R","Ng","mP lo oA l jd y","CP y r N wHA","Bt l","Y S xv MNS","WG vz","S pl l Dz fX D TpI","zGq","z S D"},
-                new String[]{"QMnVn","QMnVn","nkq","o","EEM","QMnVn","o","nkq","Gd","Gd","nkq","o","EEM","EEM"}));
+        assertEquals("ka3caz4837h6ada4 r1 $547.82",
+                p.solve1("ka3caz4837h6ada4 r1 $602", 9));
     }
     @Test
     public void test4() {
-        assertEquals("FnZd", p.solve1(new String[]{"tP x M VC h lmD","D X XF w V","sh m Pgl","pN pa","C SL m G Pn v","K z UL B W ee","Yf yo n V U Za f np","j J sk f qr e v t","L Q cJ c J Z jp E","Be a aO","nI c Gb k Y C QS N","Yi Bts","gp No g s VR","py A S sNf","ZS H Bi De dj dsh","ep MA KI Q Ou"},
-                new String[]{"OXlq","IFGaW","XQPeWJRszU","Gb","HArIr","Gb","FnZd","FnZd","HArIr","OXlq","IFGaW","XQPeWJRszU","EMoUs","Gb","EMoUs","EMoUs"}));
+        assertEquals("$2658129.12 5q $2113353.36",
+                p.solve1("$7383692 5q $5870426", 64));
     }
     @Test
     public void test5() {
-        assertEquals("a", p.solve1(new String[]{"a"},
-                new String[]{"a"}));
+        assertEquals("706hzu76jjh7yufr5x9ot60v149k5 $5509377493.92 pw2o $4.32",
+                p.solve1("706hzu76jjh7yufr5x9ot60v149k5 $7651913186 pw2o $6", 28));
     }
     @Test
     public void test6() {
-        assertEquals("zADZhwSwFj", p.solve1(new String[]{"l","E","e","L","U","B","H","H","l","I","E","c","s","t","s","u","r","F","Q","v","K","x","M","r","o","k","o","E","x","f"},
-                new String[]{"TYproPIptp","ZinKBnptgb","MabHvYDOvr","KJsXlcnoLX","IMjvhaCNQU","qWTfyhzuge","CBCPpuHdKb","HpDxmDTUwp","SgOTFxFYpl","PctQGYkMuz","KqAEViZnnc","DVTKOJLZPc","InJQqHsICO",
-                        "bNhncDzkLu","IvbKSkMTWM","nOjJjZjQTa","ekqBBwyCYb","zADZhwSwFj","LswFXzVDyF","bhIcpZBHWb","mesrFSlmqJ","OZZKylOPUj","OVDIlCRBro","IIhwLrjgHo","HrXZYYiXnT","pmHsmhKLoB","ppIkHHnBqQ",
-                        "gNGHfhhxEL","VSZuisvhuQ","qaPorTEGcL"}));
+        assertEquals("$2$3 $10.00 $100.00 $1.00 200 $33.00 33$ $$ $99.00 $99999.00 $9999999999.00",
+                p.solve1("$2$3 $10 $100 $1 200 $33 33$ $$ $99 $99999 $9999999999", 0));
     }
 }
