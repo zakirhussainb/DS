a194a2f24bd3f2d1960b392d0afc68478dc0f7aa
zakirhussainb <zakirhussainb2693@gmail.com>
2022-06-05 11:55:27 +0530

    WC-296 P_B

--- src/main/java/com/zakcorp/leetcodemaster/contest/Problem_B.java
+++ src/main/java/com/zakcorp/leetcodemaster/contest/Problem_B.java
@@ -1,44 +1,41 @@
 package com.zakcorp.leetcodemaster.contest;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.*;
 
 public class Problem_B {
     static class Solver {
-        private static final String regex = "\\$[0-9\\(\\)]+$";
-        public String solve1(String sentence, int discount) {
-            String[] split = sentence.split("\\s+");
-            for(int i = 0; i < split.length; i++) {
-                long count = split[i].chars().filter(ch -> ch == '$').count();
-                if(count > 1) {
-                    continue;
-                }
-                Pattern pattern = Pattern.compile(regex);
-                Matcher matcher = pattern.matcher(split[i]);
-                boolean matchFound = matcher.find();
-                StringBuilder sb = new StringBuilder();
-                if(matchFound) {
-                    if(discount == 100) {
-                        split[i] = "$0.00";
-                    } else {
-                        long num = Long.parseLong(split[i].substring(1));
-                        double value = (double) num * (discount / 100.0);
-                        value = num - value;
-                        String r = String.format("%1$,.2f", value);
-                        r = r.replaceAll(",", "");
-                        sb.append("$");
-                        sb.append(r);
-                        split[i] = sb.toString();
-                    }
+        public int solve1(int[] arr, int k) {
+            PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+            for (int num : arr) {
+                pq.add(num); // O(log N) * O(N) => O(N log N) -> sorting
+            }
+
+            int base = pq.peek();
+            int ans = 0;
+
+            while (!pq.isEmpty()) { // O (N)
+                int val = pq.poll(); //  O(log N) * O(N)
+                if ((base - val) > k) {
+                    ans++;
+                    base = val;
                 }
             }
-            StringBuilder res = new StringBuilder();
-            for(int i = 0; i < split.length; i++) {
-                res.append(split[i]);
-                res.append(" ");
+//            O(n log N) + O(n log N)
+            return ans + 1;
+        }
+        public int solve2(int[] arr, int k) {
+            Arrays.sort(arr); // O(N log N)
+            int n = arr.length;
+            int base = arr[n - 1];
+            int ans = 0;
+            for(int i = n - 1; i >= 0; i--) { // O(N)
+                if ((base - arr[i]) > k) {
+                    ans++;
+                    base = arr[i];
+                }
             }
-            res.deleteCharAt(res.length() - 1);
-            return res.toString();
+//            O(N) + O(n log N)
+            return ans + 1;
         }
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/contest/Problem_B_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/contest/Problem_B_Test.java
@@ -8,32 +8,17 @@ public class Problem_B_Test {
     Problem_B.Solver p = new Problem_B.Solver();
     @Test
     public void test1() {
-        assertEquals("there are $0.50 $1.00 and 5$ candies in the shop",
-                p.solve1("there are $1 $2 and 5$ candies in the shop", 50));
+        assertEquals(2, p.solve1(new int[]{3,6,1,2,5}, 2));
+        assertEquals(2, p.solve2(new int[]{3,6,1,2,5}, 2));
     }
     @Test
     public void test2() {
-        assertEquals("1 2 $0.00 4 $0.00 $0.00 7 8$ $0.00 $10$",
-                p.solve1("1 2 $3 4 $5 $6 7 8$ $9 $10$", 100));
+        assertEquals(2, p.solve1(new int[]{1,2,3}, 1));
+        assertEquals(2, p.solve2(new int[]{1,2,3}, 1));
     }
     @Test
     public void test3() {
-        assertEquals("ka3caz4837h6ada4 r1 $547.82",
-                p.solve1("ka3caz4837h6ada4 r1 $602", 9));
-    }
-    @Test
-    public void test4() {
-        assertEquals("$2658129.12 5q $2113353.36",
-                p.solve1("$7383692 5q $5870426", 64));
-    }
-    @Test
-    public void test5() {
-        assertEquals("706hzu76jjh7yufr5x9ot60v149k5 $5509377493.92 pw2o $4.32",
-                p.solve1("706hzu76jjh7yufr5x9ot60v149k5 $7651913186 pw2o $6", 28));
-    }
-    @Test
-    public void test6() {
-        assertEquals("$2$3 $10.00 $100.00 $1.00 200 $33.00 33$ $$ $99.00 $99999.00 $9999999999.00",
-                p.solve1("$2$3 $10 $100 $1 200 $33 33$ $$ $99 $99999 $9999999999", 0));
+        assertEquals(3, p.solve1(new int[]{2,2,4,5}, 0));
+        assertEquals(3, p.solve2(new int[]{2,2,4,5}, 0));
     }
 }
