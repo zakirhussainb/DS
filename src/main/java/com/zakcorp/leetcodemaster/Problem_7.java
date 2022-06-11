241c879c6e6d152a942d9ca450ea28832a37af38
zakirhussainb <zakirhussainb2693@gmail.com>
2022-06-12 01:46:48 +0530

    Problem_7 updated

--- src/main/java/com/zakcorp/leetcodemaster/Problem_7.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_7.java
@@ -1,16 +1,24 @@
 package com.zakcorp.leetcodemaster;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Problem_7 {
     static class Solver {
+        // Basic Brute Force -> Should not be used -> because this is using long;
         public int solve1(int x) {
+            long ans = 0;
+            while(x != 0) {
+                ans = (ans * 10) + (x % 10);
+                x = x / 10;
+            }
+            if(ans > Integer.MAX_VALUE || ans < Integer.MIN_VALUE)
+                return 0;
+            return (int)ans;
+        }
+        public int solve2(int x) {
             int ans = 0;
             while(x != 0) {
                 int tail = x % 10;
                 int newAns = (ans * 10) + tail;
-                if( (newAns - tail) / 10 != ans) {
+                if( (newAns - tail) / 10 != ans) { // TO AVOID INTEGER OVERFLOW/UNDERFLOW
                     return 0;
                 }
                 ans = newAns;
