a414d42edcd6834901dfe1a4d42ae5829a63e92f
zakirhussainb <zakirhussainb2693@gmail.com>
2022-06-21 01:04:48 +0530

    Problem 198 Updated

--- src/main/java/com/zakcorp/leetcodemaster/Problem_198.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_198.java
@@ -30,5 +30,34 @@ public class Problem_198 {
             memo[n] = Math.max( memoized(n - 2, arr, memo) + arr[n], memoized(n - 1, arr, memo) );;
             return memo[n];
         }
+        private final Map<Integer, Integer> memo = new HashMap<>();
+        public int solve2(int[] arr) {
+            return Math.max( recur(0, arr), recur(1, arr) );
+        }
+        private int recur(int i, int[] arr) {
+            if(i >= arr.length)
+                return 0;
+            if(i == arr.length - 1)
+                return arr[i];
+            if(memo.containsKey(i)) {
+                return memo.get(i);
+            }
+            memo.put(i, arr[i] + Math.max( recur(i + 2, arr), recur(i + 3, arr) ) );
+            return memo.get(i);
+        }
+    }
+    // Tabulation DP
+    static class Solver2 {
+        public int solve1(int[] arr) {
+            int n = arr.length;
+            if(n == 0)
+                return 0;
+            int[] dp = new int[n + 1];
+            dp[1] = arr[0];
+            for(int i = 2; i <= n; i++) {
+                dp[i] = Math.max(dp[i - 1], dp[i - 2] + arr[i - 1]);
+            }
+            return dp[n];
+        }
     }
 }
