47d126c0173aa6f52c71120d3f350f82900c0949
zakirhussainb <zakirhussainb2693@gmail.com>
2022-07-02 20:08:05 +0530

    Problem_54 comments

--- src/main/java/com/zakcorp/leetcodemaster/Problem_54.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_54.java
@@ -10,19 +10,23 @@ public class Problem_54 {
             int startRow = 0, startCol = 0, endRow = m - 1, endCol = n - 1;
             List<Integer> output = new ArrayList<>();
             while(output.size() < m * n) {
+                // Traverse Right
                 for(int i = startCol; i <= endCol; i++) {
                     output.add( matrix[startRow][i] );
                 }
-
+                // Traverse Down
                 for(int i = startRow + 1; i <= endRow; i++) {
                     output.add( matrix[i][endCol] );
                 }
 
+                // While Traversing on the left side and the top side, don't forget to check whether the row or col
+                // still exists inorder to prevent duplicates
                 if(startRow < endRow && startCol < endCol) {
+                    // Traverse Left
                     for(int i = endCol - 1; i > startCol; i--) {
                         output.add( matrix[endRow][i] );
                     }
-
+                    // Traverse Up
                     for(int i = endRow; i > startRow; i--) {
                         output.add( matrix[i][startCol] );
                     }
