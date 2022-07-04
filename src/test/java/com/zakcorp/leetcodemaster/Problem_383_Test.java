18fc3bd884edff58d732325e90fc9f5ff37d7bf0
zakirhussainb <zakirhussainb2693@gmail.com>
2022-07-04 16:25:19 +0530

    Problem 383

--- src/test/java/com/zakcorp/leetcodemaster/Problem_383_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_383_Test.java
@@ -2,13 +2,20 @@ package com.zakcorp.leetcodemaster;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class Problem_383_Test {
     Problem_383.Solver p = new Problem_383.Solver();
     @Test
     public void test1() {
         assertTrue( p.solve1("aab", "baa") );
+    }
+    @Test
+    public void test2() {
+        assertTrue( p.solve1("bg", "efjbdfbdgfjhhaiigfhbaejahgfbbgbjagbddfgdiaigdadhcfcj") );
+    }
+    @Test
+    public void test3() {
+        assertFalse( p.solve1("aa", "ab") );
     }
 }
