b2ae652859260523e571a0496744d834a08f1126
zakirhussainb <zakirhussainb2693@gmail.com>
2022-07-07 09:47:24 +0530

    Updated problem 67

--- src/main/java/com/zakcorp/leetcodemaster/Problem_67.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_67.java
@@ -2,28 +2,30 @@ package com.zakcorp.leetcodemaster;
 
 public class Problem_67 {
     static class Solver {
+        /*
+        1. Similar to what you do for adding two numbers, same code.
+        2. The only change is there you do sum % 10 and here since it is binary, you have to do,
+        → sum % 2 and carry as sum / 2
+         */
         public String addBinary(String a, String b) {
             StringBuilder sb = new StringBuilder();
             int i = a.length() - 1;
             int j = b.length() - 1;
             int carry = 0;
-            while(i >= 0 || j >= 0) {
+            while(i >= 0 || j >= 0 || carry != 0) {
                 int sum = carry;
-                if(j >= 0){
-                    sum = sum + b.charAt(j) - '0';
-                    j--;
-                }
                 if(i >= 0) {
-                    sum = sum + a.charAt(i) - '0';
+                    sum += a.charAt(i) - '0';
                     i--;
                 }
-                sb.append(sum % 2);
+                if(j >= 0){
+                    sum += b.charAt(j) - '0';
+                    j--;
+                }
+                sb.insert(0, sum % 2);
                 carry = sum / 2;
             }
-            if(carry != 0){
-                sb.append(carry);
-            }
-            return sb.reverse().toString();
+            return sb.toString();
         }
     }
 }
