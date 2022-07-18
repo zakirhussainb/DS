13d58b35b1d543b49e1c096967c601ba6698f0fb
zakirhussainb <zakirhussainb2693@gmail.com>
2022-07-18 10:17:48 +0530

    LC Problem 76 Solution 2

--- src/main/java/com/zakcorp/leetcodemaster/Problem_76.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_76.java
@@ -41,5 +41,46 @@ public class Problem_76 {
             }
             return minLength == Integer.MAX_VALUE ? "" : s.substring(boundary[0], boundary[1] + 1);
         }
+        public String solve2(String s, String t) {
+            if(s.length() < t.length())
+                return "";
+            if(s.equals(t))
+                return s;
+            int[] countTMap = new int[128];
+            int need = 0;
+            for(char ch : t.toCharArray()) {
+                countTMap[ch]++;
+            }
+            for (int num : countTMap) {
+                if (num != 0)
+                    need++;
+            }
+            int[] window = new int[128];
+            int have = 0;
+            int[] boundary = {-1, -1};
+            int minLength = Integer.MAX_VALUE;
+            int start = 0;
+            for(int end = 0; end < s.length(); end++) {
+                char endCh = s.charAt(end);
+                window[endCh]++;
+                if(countTMap[endCh] != 0 && window[endCh] == countTMap[endCh]) {
+                    have++;
+                }
+                while(have == need) {
+                    if(end - start + 1 < minLength) {
+                        boundary[0] = start;
+                        boundary[1] = end;
+                        minLength = end - start + 1;
+                    }
+                    char startCh = s.charAt(start);
+                    window[startCh]--;
+                    if(countTMap[startCh] != 0 && window[startCh] < countTMap[startCh]) {
+                        have--;
+                    }
+                    start++;
+                }
+            }
+            return minLength == Integer.MAX_VALUE ? "" : s.substring(boundary[0], boundary[1] + 1);
+        }
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_76_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_76_Test.java
@@ -9,25 +9,31 @@ public class Problem_76_Test {
     @Test
     public void test1() {
         assertEquals("BANC", p.solve1("ADOBECODEBANC", "ABC"));
+        assertEquals("BANC", p.solve2("ADOBECODEBANC", "ABC"));
     }
     @Test
     public void test2() {
         assertEquals("a", p.solve1("a", "a"));
+        assertEquals("a", p.solve2("a", "a"));
     }
     @Test
     public void test3() {
         assertEquals("", p.solve1("a", "aa"));
+        assertEquals("", p.solve2("a", "aa"));
     }
     @Test
     public void test4() {
         assertEquals("aa", p.solve1("aa", "aa"));
+        assertEquals("aa", p.solve2("aa", "aa"));
     }
     @Test
     public void test5() {
         assertEquals("", p.solve1("a", "b"));
+        assertEquals("", p.solve2("a", "b"));
     }
     @Test
     public void test6() {
         assertEquals("baa", p.solve1("bbaa", "aba"));
+        assertEquals("baa", p.solve2("bbaa", "aba"));
     }
 }
