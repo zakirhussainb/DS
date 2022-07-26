6cfdb6eedee9167167f3a9c17cacad470b4af295
zakirhussainb <zakirhussainb2693@gmail.com>
2022-07-27 00:26:47 +0530

    LC Problem 283 Updated with alternate solution

--- src/main/java/com/zakcorp/leetcodemaster/Problem_283.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_283.java
@@ -19,5 +19,18 @@ public class Problem_283 {
             }
             return arr;
         }
+        public int[] solve2(int[] arr) {
+            int n = arr.length;
+            int k = 0;
+            for(int i = 0; i < n; i++) {
+                if(arr[i] != 0) {
+                    arr[k++] = arr[i];
+                }
+            }
+            for(int i = k; i < n; i++) {
+                arr[i] = 0;
+            }
+            return arr;
+        }
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_283_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_283_Test.java
@@ -17,4 +17,16 @@ public class Problem_283_Test {
         assertEquals(new int[]{1,3,12,0,0}, p.moveZeroes(new int[]{1}));
         assertEquals(new int[]{1,3,12,0,0}, p.moveZeroes(new int[]{}));
     }
+    @Test
+    public void test2() {
+        Problem_283.Solver p = new Problem_283.Solver();
+        assertEquals(new int[]{1,3,12,0,0}, p.solve2(new int[]{0, 1, 0, 3, 12}));
+        assertEquals(new int[]{1,3,12,0,0}, p.solve2(new int[]{0,-1,0,-3,-12}));
+        assertEquals(new int[]{1,3,12,0,0}, p.solve2(new int[]{0,0,1}));
+        assertEquals(new int[]{1,3,12,0,0}, p.solve2(new int[]{1,0,0}));
+        assertEquals(new int[]{1,3,12,0,0}, p.solve2(new int[]{1,0,1}));
+        assertEquals(new int[]{1,3,12,0,0}, p.solve2(new int[]{0}));
+        assertEquals(new int[]{1,3,12,0,0}, p.solve2(new int[]{1}));
+        assertEquals(new int[]{1,3,12,0,0}, p.solve2(new int[]{}));
+    }
 }
