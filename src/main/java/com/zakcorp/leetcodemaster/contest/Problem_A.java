da6418f3742c99c64947fedb1ab30a40a9c70942
zakirhussainb <zakirhussainb2693@gmail.com>
2022-08-07 08:38:53 +0530

    WC 305 Problem A

--- src/main/java/com/zakcorp/leetcodemaster/contest/Problem_A.java
+++ src/main/java/com/zakcorp/leetcodemaster/contest/Problem_A.java
@@ -4,30 +4,20 @@ import java.util.*;
 
 public class Problem_A {
     static class Solver {
-        public List<List<Integer>> solve1(int[][] items1, int[][] items2) {
-            TreeMap<Integer, Integer> map = new TreeMap<>();
-            for(int[] item1 : items1) {
-                int val = item1[0];
-                int weight = item1[1];
-                map.put(val, weight);
-            }
-            for(int[] item2 : items2) {
-                int val = item2[0];
-                int weight = item2[1];
-                if(map.containsKey(val)) {
-                    map.put(val, map.get(val) + weight);
-                } else {
-                    map.put(val, weight);
+        public int solve1(int[] arr, int diff) {
+            int n = arr.length;
+            int c = 0;
+            for(int i = 0; i < n; i++) {
+                for(int j = i + 1; j < n; j++) {
+                    for(int k = j + 1; k < n; k++) {
+                        if(i < j && j < k) {
+                            if(arr[j] - arr[i] == diff && arr[k] - arr[j] == diff)
+                                c++;
+                        }
+                    }
                 }
             }
-            List<List<Integer>> res = new ArrayList<>();
-            for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
-                List<Integer> list = new ArrayList<>();
-                list.add(entry.getKey());
-                list.add(entry.getValue());
-                res.add(list);
-            }
-            return res;
+            return c;
         }
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/contest/Problem_A_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/contest/Problem_A_Test.java
@@ -8,7 +8,10 @@ public class Problem_A_Test {
     Problem_A.Solver p = new Problem_A.Solver();
     @Test
     public void test1() {
-        System.out.println(p.solve1(new int[][]{{1,1},{4,5},{3,8}},
-                new int[][]{{1,5},{3,1}}));
+        assertEquals(2, p.solve1(new int[]{0,1,4,6,7,10}, 3));
+    }
+    @Test
+    public void test2() {
+        assertEquals(2, p.solve1(new int[]{4,5,6,7,8,9}, 2));
     }
 }
