b6ae99c2519ea75c1541b18ea807783cb6373d76
zakirhussainb <zakirhussainb2693@gmail.com>
2022-08-07 18:05:25 +0530

    LC Problem 339

--- src/main/java/com/zakcorp/leetcodemaster/Problem_339.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_339.java
@@ -1,12 +1,31 @@
 package com.zakcorp.leetcodemaster;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Problem_339 {
+    public interface NestedInteger {
+
+        // @return true if this NestedInteger holds a single integer, rather than a nested list.
+        public boolean isInteger();
+
+        // @return the single integer that this NestedInteger holds, if it holds a single integer
+        // Return null if this NestedInteger holds a nested list
+        public Integer getInteger();
+
+        // Set this NestedInteger to hold a single integer.
+        public void setInteger(int value);
+
+        // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+        public void add(NestedInteger ni);
+
+        // @return the nested list that this NestedInteger holds, if it holds a nested list
+        // Return empty list if this NestedInteger holds a single integer
+        public List<NestedInteger> getList();
+    }
+
     static class Solver {
         // https://leetcode.com/submissions/detail/419841352/
-        /*public int depthSum(List<NestedInteger> nestedList) {
+        public int solve1(List<NestedInteger> nestedList) {
             int[] result = {0};
             dfs(nestedList, 1, result);
             return result[0];
@@ -19,6 +38,23 @@ public class Problem_339 {
                     dfs(ni.getList(), level + 1, result);
                 }
             }
-        }*/
+        }
+    }
+
+    static class Solver1{
+        public int solve1(List<NestedInteger> nestedList) {
+            return dfs(nestedList, 1);
+        }
+        private int dfs(List<NestedInteger> list, int depth) {
+            int total = 0;
+            for (NestedInteger nested: list) {
+                if(nested.isInteger()) {
+                    total += nested.getInteger() * depth;
+                } else {
+                    total += dfs(nested.getList(), depth+1);
+                }
+            }
+            return total;
+        }
     }
 }
