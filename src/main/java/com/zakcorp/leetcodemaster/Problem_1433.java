0c58f394f86a12957962988ed9b3f646b899bec6
zakirhussainb <zakirhussainb2693@gmail.com>
2022-09-10 00:27:46 +0530

    LC Problem 1433 1

--- src/main/java/com/zakcorp/leetcodemaster/Problem_1433.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_1433.java
@@ -29,4 +29,33 @@ public class Problem_1433
             return s1Counter == s1.length() || s2Counter == s1.length();
         }
     }
+    /*
+    Can be done without sorting the arrays also.
+        -> Create two individual hash arrays to store the occurrences of characters in a string.
+        -> Calculate the difference of arr1_i - arr2_i and add it to s.
+        -> if s is negative then return false.
+        -> Now Check for the arr2_i - arr1_i.
+        Note : You need to check both scenarios, because it is mentioned in the question that s2 can break s1 or vice-versa.
+     */
+    static class Solver1 {
+        public boolean solve1(String s1, String s2) {
+            int[] dt1 = new int[26];
+            int[] dt2 = new int[26];
+            for(int i = 0; i < s1.length(); i++) {
+                dt1[s1.charAt( i ) - 'a']++;
+                dt2[s2.charAt( i ) - 'a']++;
+            }
+            return check(dt1, dt2) || check(dt2, dt1);
+        }
+        private boolean check(int[] dt1, int[] dt2) {
+            int s = 0;
+            for (int i = 0; i < 26; i++) {
+                int diff = dt1[i] - dt2[i];
+                s += diff;
+                if (s < 0)
+                    return false;
+            }
+            return true;
+        }
+    }
 }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_1433_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_1433_Test.java
@@ -7,24 +7,30 @@ import org.junit.Test;
 public class Problem_1433_Test
 {
     Problem_1433.Solver p = new Problem_1433.Solver();
+    Problem_1433.Solver1 p1 = new Problem_1433.Solver1();
     @Test
     public void test1() {
         assertTrue(p.solve1("abc", "xya"));
+        assertTrue(p1.solve1("abc", "xya"));
     }
     @Test
     public void test2() {
         assertFalse(p.solve1("abe", "acd"));
+        assertFalse(p1.solve1("abe", "acd"));
     }
     @Test
     public void test3() {
         assertTrue(p.solve1("leetcodee", "interview"));
+        assertTrue(p1.solve1("leetcodee", "interview"));
     }
     @Test
     public void test4() {
         assertTrue(p.solve1("szy", "cid"));
+        assertTrue(p1.solve1("szy", "cid"));
     }
     @Test
     public void test5() {
         assertTrue(p.solve1("yopumzgd", "pamntyya"));
+        assertTrue(p1.solve1("yopumzgd", "pamntyya"));
     }
 }
