8abd7ca4ec4d8a0a887db44541524dc122142763
zakirhussainb <zakirhussainb2693@gmail.com>
2022-09-24 22:23:45 +0530

    L7 All Kinds of Patterns in recursion

--- src/main/java/com/zakcorp/striver_series/recursion/L7_AllKindsOfPatternsInRecursion.java
+++ src/main/java/com/zakcorp/striver_series/recursion/L7_AllKindsOfPatternsInRecursion.java
@@ -4,31 +4,93 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class L7_AllKindsOfPatternsInRecursion {
-    /*
-    Concept : given an array -> [1, 1, 2] and K = 2
-    We need to print all the subsequences for this array whose sum is 2
-    Output :
-    [1, 1]
-    [2]
-     */
-    public void printAllSubsequencesWhoseSumIsK(int[] arr, int k) {
-        recursivePrint(0, arr, new ArrayList<>(), new int[]{0}, k);
+    static class PrintAllSubsequencesWhoseSumIsK {
+        /*
+           Concept : given an array -> [1, 1, 2] and K = 2
+           We need to print all the subsequences for this array whose sum is 2
+           Output :
+           [1, 1]
+           [2]
+        */
+        public void printAllSubsequencesWhoseSumIsK(int[] arr, int k) {
+            recursivePrint(0, arr, new ArrayList<>(), new int[]{0}, k);
+        }
+
+        private void recursivePrint(int index, int[] arr, List<Integer> list, int[] sum, int k) {
+            if(index >= arr.length) {
+                if(sum[0] == k) {
+                    System.out.println(new ArrayList<>(list));
+                }
+                return;
+            }
+            // take or pick the element in this index into the subsequence
+            list.add(arr[index]);
+            sum[0] += arr[index];
+            recursivePrint(index + 1, arr, list, sum, k);
+            // not take or not pick the element -> probably the last element in the list
+            list.remove(list.size() - 1);
+            sum[0] -= arr[index];
+            recursivePrint(index + 1, arr, list, sum, k);
+        }
     }
 
-    private void recursivePrint(int index, int[] arr, List<Integer> list, int[] sum, int k) {
-        if(index >= arr.length) {
-            if(sum[0] == k) {
-                System.out.println(new ArrayList<>(list));
-            }
-            return;
+    static class PrintAnyOneSubsequencesWhoseSumIsK {
+        /*
+           Concept : given an array -> [1, 1, 2] and K = 2
+           We need to print all the subsequences for this array whose sum is 2
+           Output :
+           [1, 1]
+        */
+        public void printAnyOneSubsequenceWhoseSumIsK(int[] arr, int k) {
+            recursivePrint(0, arr, new ArrayList<>(), new int[]{0}, k);
         }
-        // take or pick the element in this index into the subsequence
-        list.add(arr[index]);
-        sum[0] += arr[index];
-        recursivePrint(index + 1, arr, list, sum, k);
-        // not take or not pick the element -> probably the last element in the list
-        list.remove(list.size() - 1);
-        sum[0] -= arr[index];
-        recursivePrint(index + 1, arr, list, sum, k);
+
+        private boolean recursivePrint(int index, int[] arr, List<Integer> list, int[] sum, int k) {
+            if(index >= arr.length) {
+                if(sum[0] == k) {
+                    System.out.println(new ArrayList<>(list));
+                    return true;
+                }
+                return false;
+            }
+            // take or pick the element in this index into the subsequence
+            list.add(arr[index]);
+            sum[0] += arr[index];
+            if ( recursivePrint(index + 1, arr, list, sum, k) )
+                return true;
+            // not take or not pick the element -> probably the last element in the list
+            list.remove(list.size() - 1);
+            sum[0] -= arr[index];
+            return recursivePrint(index + 1, arr, list, sum, k);
+        }
+    }
+
+    static class CountAllTheSubsequencesWhoseSumIsK {
+        /*
+           Concept : given an array -> [1, 1, 2] and K = 2
+           We need to count all the subsequences for this array whose sum is 2
+           Output :
+           2 since, [1,1] and [2]
+        */
+        public int countAllSubsequenceWhoseSumIsK(int[] arr, int k) {
+            return recursiveCount(0, arr, new int[]{0}, k);
+        }
+
+        private int recursiveCount(int index, int[] arr, int[] sum, int k) {
+            if(index >= arr.length) {
+                if(sum[0] == k) {
+                    return 1;
+                }
+                return 0;
+            }
+            // take or pick the element in this index into the subsequence
+            sum[0] += arr[index];
+            int left = recursiveCount(index + 1, arr, sum, k);
+            // not take or not pick the element -> here you need to subtract from the sum
+            sum[0] -= arr[index];
+            int right = recursiveCount(index + 1, arr, sum, k);
+            return left + right;
+        }
+
     }
 }
--- src/test/java/com/zakcorp/striver_series/recursion/L7_AllKindsOfPatternsinRecursion_Test.java
+++ src/test/java/com/zakcorp/striver_series/recursion/L7_AllKindsOfPatternsinRecursion_Test.java
@@ -3,16 +3,31 @@ package com.zakcorp.striver_series.recursion;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class L7_AllKindsOfPatternsinRecursion_Test
 {
-    private L7_AllKindsOfPatternsInRecursion l7;
+    private L7_AllKindsOfPatternsInRecursion.PrintAllSubsequencesWhoseSumIsK l7_1;
+    private L7_AllKindsOfPatternsInRecursion.PrintAnyOneSubsequencesWhoseSumIsK l7_2;
+    private L7_AllKindsOfPatternsInRecursion.CountAllTheSubsequencesWhoseSumIsK l7_3;
     @Before
     public void setup() {
-        l7 = new L7_AllKindsOfPatternsInRecursion();
+        l7_1 = new L7_AllKindsOfPatternsInRecursion.PrintAllSubsequencesWhoseSumIsK();
+        l7_2 = new L7_AllKindsOfPatternsInRecursion.PrintAnyOneSubsequencesWhoseSumIsK();
+        l7_3 = new L7_AllKindsOfPatternsInRecursion.CountAllTheSubsequencesWhoseSumIsK();
     }
     @Test
     public void test_printAllSubsequencesWhoseSumIsK() {
-        l7.printAllSubsequencesWhoseSumIsK(new int[]{1,1,2}, 2);
+        l7_1.printAllSubsequencesWhoseSumIsK(new int[]{1,1,2}, 2);
     }
 
+    @Test
+    public void test_printAnyOneSubsequencesWhoseSumIsK() {
+        l7_2.printAnyOneSubsequenceWhoseSumIsK(new int[]{1,1,2}, 2);
+    }
+
+    @Test
+    public void test_countAllSubsequencesWhoseSumIsK() {
+        assertEquals(2, l7_3.countAllSubsequenceWhoseSumIsK(new int[]{1,1,2}, 2));
+    }
 }
