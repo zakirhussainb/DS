c7d254e5d5f86584afab2855ab927808b8804338
zakirhussainb <zakirhussainb2693@gmail.com>
2022-10-10 20:21:42 +0530

    LC Problem 985

--- src/main/java/com/zakcorp/leetcodemaster/Problem_985.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_985.java
@@ -6,7 +6,56 @@ public class Problem_985 {
     // TLE for this solution
     static class Solver {
         public int[] sumEvenAfterQueries(int[] arr, int[][] queries) {
-            return null;
+            int[] res = new int[queries.length];
+            int i = 0;
+            for(int[] query : queries) {
+                int val = query[0];
+                int index = query[1];
+                arr[index] = arr[index] + val;
+                res[i++] = getEvenSum(arr);
+            }
+            return res;
+        }
+        private int getEvenSum(int[] arr) {
+            int sum = 0;
+            for(int num : arr) {
+                if(num % 2 == 0)
+                    sum += num;
+            }
+            return sum;
+        }
+    }
+    // TLE for this solution
+    static class Solver1 {
+        public int[] sumEvenAfterQueries(int[] arr, int[][] queries) {
+            int[] res = new int[queries.length];
+            int evenSum = getEvenSum(arr);
+            for(int i = 0; i < queries.length; i++) {
+                int val = queries[i][0];
+                int index = queries[i][1];
+                if(arr[index] % 2 == 0) {
+                    evenSum -= arr[index];
+                    arr[index] += val;
+                    if(arr[index] % 2 == 0) {
+                        evenSum += arr[index];
+                    }
+                } else {
+                    arr[index] += val;
+                    if(arr[index] % 2 == 0) {
+                        evenSum += arr[index];
+                    }
+                }
+                res[i] = evenSum;
+            }
+            return res;
+        }
+        private int getEvenSum(int[] arr) {
+            int sum = 0;
+            for(int num : arr) {
+                if(num % 2 == 0)
+                    sum += num;
+            }
+            return sum;
         }
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_985_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_985_Test.java
@@ -6,8 +6,10 @@ import static org.junit.Assert.assertArrayEquals;
 
 public class Problem_985_Test {
     Problem_985.Solver p = new Problem_985.Solver();
+    Problem_985.Solver1 p1 = new Problem_985.Solver1();
     @Test
     public void test1() {
         assertArrayEquals(new int[]{8, 6, 2, 4}, p.sumEvenAfterQueries(new int[]{1, 2, 3, 4}, new int[][]{{1,0},{-3,1},{-4,0},{2,3}}));
+        assertArrayEquals(new int[]{8, 6, 2, 4}, p1.sumEvenAfterQueries(new int[]{1, 2, 3, 4}, new int[][]{{1,0},{-3,1},{-4,0},{2,3}}));
     }
 }
