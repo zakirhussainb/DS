d8d2320b4b146c1f556ad6d62d8fe603463ef6eb
zakirhussainb <zakirhussainb2693@gmail.com>
2022-10-22 14:09:00 +0530

    Striver Recursion Series - Completed Sudoku Solver

---
+++ src/main/java/com/zakcorp/striver_series/recursion/L14_NQueens.java
@@ -0,0 +1,8 @@
+package com.zakcorp.striver_series.recursion;
+
+public class L14_NQueens
+{
+    // Refer problems 51 and 52 from Leetcode
+    // There are two solutions
+    // https://youtu.be/i05Ju7AftcM?list=PLgUwDviBIf0rGlzIn_7rsaR2FQ5e6ZOL9
+}
---
+++ src/main/java/com/zakcorp/striver_series/recursion/L15_SudokuSolver.java
@@ -0,0 +1,8 @@
+package com.zakcorp.striver_series.recursion;
+
+public class L15_SudokuSolver
+{
+    // Refer problem 37 from Leetcode
+    // There is one solution
+    // https://youtu.be/FWAIf_EVUKE?list=PLgUwDviBIf0rGlzIn_7rsaR2FQ5e6ZOL9
+}
