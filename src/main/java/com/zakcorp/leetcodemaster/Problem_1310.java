ade7410f2a18ebc3640e9959aafa6eef60b0b206
zakirhussainb <zakirhussainb2693@gmail.com>
2022-12-08 00:37:30 +0530

    LC 1310

--- src/main/java/com/zakcorp/leetcodemaster/Problem_1310.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_1310.java
@@ -5,17 +5,23 @@ public class Problem_1310 {
     //  https://www.geeksforgeeks.org/prefix-sum-array-implementation-applications-competitive-programming/
     static class Solver {
         public int[] solve1(int[] arr, int[][] queries) {
-//            int[] prefixXor = new int[arr.length + 1];
-//            for(int i = 0; i < arr.length; i++) {
-//                prefixXor[i + 1] = prefixXor[i] ^ arr[i];
-//            }
-//
-//            int[] ans = new int[arr.length];
-//            for(int i = 0; i < queries.length; i++) {
-//                ans[i] = prefixXor[queries[i][0]] ^ prefixXor[queries[i][1] + 1];
-//            }
-//            return ans;
-            return null;
+            int n = arr.length;
+            int[] prefixXor = new int[n];
+            prefixXor[0] = arr[0];
+            for(int i = 1; i < arr.length; i++) {
+                prefixXor[i] = prefixXor[i - 1] ^ arr[i];
+            }
+
+            int[] ans = new int[queries.length];
+            for(int i = 0; i < queries.length; i++) {
+                int left = queries[i][0];
+                int right = queries[i][1];
+                if(left == 0)
+                    ans[i] = prefixXor[right];
+                else
+                    ans[i] = prefixXor[right] ^ prefixXor[left - 1];
+            }
+            return ans;
         }
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_1310_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_1310_Test.java
@@ -10,4 +10,8 @@ public class Problem_1310_Test {
     public void test1() {
         assertArrayEquals(new int[]{2,7,14,8}, p.solve1(new int[]{1,3,4,8}, new int[][]{ {0,1},{1,2},{0,3},{3,3} } ));
     }
+    @Test
+    public void test2() {
+        assertArrayEquals(new int[]{16,16,16}, p.solve1(new int[]{16}, new int[][]{ {0,0},{0,0},{0,0} } ));
+    }
 }
