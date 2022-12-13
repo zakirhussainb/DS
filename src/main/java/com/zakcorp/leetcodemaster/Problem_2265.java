60965938a66320c5b18c0c0be2027048b0f8a359
zakirhussainb <zakirhussainb2693@gmail.com>
2022-12-13 23:08:18 +0530

    Solution 2 for 2265

--- src/main/java/com/zakcorp/leetcodemaster/Problem_2265.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_2265.java
@@ -41,4 +41,45 @@ public class Problem_2265
             return new int[]{x, totalNodes};
         }
     }
+
+    static class Solver1 {
+        // for each node in the tree, we will maintain three values
+        static class State {
+            // count of nodes in the subtree
+            int nodeCount;
+            // sum of values in the subtree
+            int sum;
+            // max average found in the subtree
+            int avg;
+            public State(int nodeCount, int sum, int avg) {
+                this.nodeCount = nodeCount;
+                this.sum = sum;
+                this.avg = avg;
+            }
+        }
+
+        public int averageOfSubtree(TreeNode root) {
+            int[] counter = { 0 };
+            postorder(root, counter);
+            return counter[0];
+        }
+
+        private State postorder(TreeNode root, int[] counter) {
+            if(root == null) {
+                return new State( 0, 0, 0 );
+            }
+            // postorder traversal, solve for both child nodes first.
+            State left = postorder( root.left, counter );
+            State right = postorder( root.right, counter );
+
+            // now find nodeCount, valueSum and maxAverage for current node `root`
+            int nodeCount = left.nodeCount + right.nodeCount + 1;
+            int sum = left.sum + right.sum + root.val;
+            int average = sum / nodeCount;
+            if(average == root.val) {
+                counter[0]++;
+            }
+            return new State( nodeCount, sum, average );
+        }
+    }
 }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_2265_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_2265_Test.java
@@ -9,12 +9,14 @@ import org.junit.Test;
 public class Problem_2265_Test
 {
     Problem_2265.Solver p = new Problem_2265.Solver();
+    Problem_2265.Solver1 p1 = new Problem_2265.Solver1();
     @Test
     public void test1() {
         TreeNode root = new TreeNode(3);
         root.left = new TreeNode(4);
         root.right = new TreeNode(2);
         assertEquals(3, p.solve1(root));
+        assertEquals(3, p1.averageOfSubtree(root));
     }
     @Test
     public void test2() {
@@ -25,5 +27,6 @@ public class Problem_2265_Test
         root.left.right = new TreeNode(1);
         root.right.right = new TreeNode(6);
         assertEquals(5, p.solve1(root));
+        assertEquals(5, p1.averageOfSubtree(root));
     }
 }
