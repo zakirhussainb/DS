0d371a3d1d28bcca4cfc0606c3d679e24bb0d37a
zakirhussainb <zakirhussainb2693@gmail.com>
2022-12-23 09:52:03 +0530

    LC 1328 - Solution 1

--- src/main/java/com/zakcorp/leetcodemaster/Problem_1328.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_1328.java
@@ -48,4 +48,29 @@ public class Problem_1328
             return true;
         }
     }
+    static class Solver1 {
+        /*
+        Check half of the string
+        Replace a non 'a' character to 'a'
+        To handle cases like, 'aabaa', 'aa', 'aba' -> set the last character to b
+         */
+        public String solve1(String palindrome) {
+            int n = palindrome.length();
+            if(n == 1)
+                return "";
+
+            char[] chArr = palindrome.toCharArray();
+            // Since we are traversing for only half of the string. 'aabaa' will be skipped, and it will not be replaced here.
+            for(int i = 0; i < n / 2; i++){
+                if(chArr[i] != 'a'){
+                    chArr[i] = 'a';
+                    return new String(chArr);
+                }
+            }
+            // By the time it reaches here, the possible cases are 'aaa', 'aba', 'aabaa', etc.
+            // So we should replace the last character to achieve the smallest one possible, in this case, 'b'
+            chArr[n - 1] = 'b';
+            return new String(chArr);
+        }
+    }
 }
