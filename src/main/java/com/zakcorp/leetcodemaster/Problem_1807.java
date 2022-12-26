7d9537b76ed6ea8ac67efa9f7b396ceeafbe64dc
zakirhussainb <zakirhussainb2693@gmail.com>
2022-12-26 08:05:45 +0530

    LC 1807 - 1

--- src/main/java/com/zakcorp/leetcodemaster/Problem_1807.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_1807.java
@@ -6,33 +6,28 @@ public class Problem_1807
 {
     static class Solver {
         public String solve1(String s, List<List<String>> knowledge) {
-            Map<String, String> map = constructKnowledgeMap(knowledge);
+            Map<String, String> map = new HashMap<>();
+            for(List<String> k : knowledge) {
+                map.put(k.get( 0 ), k.get( 1 ));
+            }
             int i = 0;
             int n = s.length();
             StringBuilder sb = new StringBuilder();
             while(i < n) {
                 if(s.charAt( i ) == '(') {
                     i += 1;
-                    StringBuilder sm = new StringBuilder();
+                    StringBuilder s1 = new StringBuilder();
                     while(s.charAt( i ) != ')') {
-                        sm.append( s.charAt( i ) );
+                        s1.append( s.charAt( i ) );
                         i++;
                     }
-                    sb.append( map.getOrDefault( sm.toString(), "?" ) );
+                    sb.append( map.getOrDefault( s1.toString(), "?" ) );
                 } else {
                     sb.append( s.charAt( i ) );
                 }
                 i++;
             }
             return sb.toString();
-        }
-
-        private Map<String, String> constructKnowledgeMap( List<List<String>> knowledge ) {
-            Map<String, String> map = new HashMap<>();
-            for(List<String> k : knowledge) {
-                map.put(k.get( 0 ), k.get( 1 ));
-            }
-            return map;
         }
     }
 }
