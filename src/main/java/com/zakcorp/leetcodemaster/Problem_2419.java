27c936da5679b609d221073beb0aa16828ef2de5
zakirhussainb <zakirhussainb2693@gmail.com>
2022-12-31 08:14:14 +0530

    LC 2419 - Updated

--- src/main/java/com/zakcorp/leetcodemaster/Problem_2419.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_2419.java
@@ -18,5 +18,30 @@ public class Problem_2419
             }
             return maxLength;
         }
+
+        /*
+        TODO : The below solution is wrong. You must not sort the array.
+        [311155,311155,311155,311155,311155,311155,311155,311155,201191,311155]
+        The output from the below program is 9.
+        Whereas the subarray with maximum Length is just 8.
+         */
+        public int solve2(int[] arr) {
+            Arrays.sort( arr );
+            int n = arr.length;
+            int maxLength = 1;
+            int maxNum = arr[n - 1];
+            int i = n - 1;
+            while(i >= 0) {
+                if(arr[i] < maxNum) {
+                    break;
+                }
+                while(arr[i] == arr[i - 1]) {
+                    i--;
+                }
+                maxLength = Math.max( maxLength, n - i );
+                i--;
+            }
+            return maxLength;
+        }
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_2419_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_2419_Test.java
@@ -10,9 +10,11 @@ public class Problem_2419_Test
     @Test
     public void test1() {
         assertEquals(2, p.solve1(new int[]{1,2,3,3,2,2}));
+        assertEquals(2, p.solve2(new int[]{1,2,3,3,2,2}));
     }
     @Test
     public void test2() {
         assertEquals(1, p.solve1(new int[]{1,2,3,4}));
+        assertEquals(1, p.solve2(new int[]{1,2,3,4}));
     }
 }
