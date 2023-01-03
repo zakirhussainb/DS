99c36701bc79910415bd311bef5818b85cb2cc17
zakirhussainb <zakirhussainb2693@gmail.com>
2023-01-03 07:39:34 +0530

    LC 1759

--- src/main/java/com/zakcorp/leetcodemaster/Problem_1759.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_1759.java
@@ -6,7 +6,44 @@ public class Problem_1759
 {
     static class Solver {
         public int solve1(String str) {
-            return 0;
+            Map<String, Long> map = new HashMap<>();
+            int i = 0, n = str.length();
+            int mod = 1_000_000_007;
+            while(i < n) {
+                StringBuilder sb = new StringBuilder();
+                int start = i;
+                while(i + 1 < n && str.charAt( i ) == str.charAt( i + 1 )) {
+                    sb.append( str.charAt( i ) );
+                    i++;
+                }
+                sb.append( str.charAt( i ) );
+                String s = sb.toString();
+                long m = i - start + 1;
+                long x = (m * (m + 1) / 2) % mod;
+                if(!map.containsKey( s )) {
+                    map.put(s, x);
+                } else {
+                    map.put(s, map.get( s ) + x);
+                }
+                i++;
+            }
+            long sum = 0;
+            for(long num : map.values()) {
+                sum = (sum + num) % mod;
+            }
+            return (int)sum;
+        }
+        public int solve2(String str) {
+            int sum = 0, count = 0;
+            for(int i = 0; i < str.length(); i++) {
+                if(i > 0 && str.charAt( i - 1 ) == str.charAt( i )) {
+                    count++;
+                } else {
+                    count = 1;
+                }
+                sum = (sum + count) % 1_000_000_007;
+            }
+            return sum;
         }
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_1759_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_1759_Test.java
@@ -10,9 +10,26 @@ public class Problem_1759_Test
     @Test
     public void test1() {
         assertEquals(3, p.solve1("bb"));
+        assertEquals(3, p.solve2("bb"));
     }
     @Test
     public void test2() {
         assertEquals(13, p.solve1("abbcccaa"));
+        assertEquals(13, p.solve2("abbcccaa"));
+    }
+    @Test
+    public void test3() {
+        assertEquals(16, p.solve1("abbcccaabb"));
+        assertEquals(16, p.solve2("abbcccaabb"));
+    }
+    @Test
+    public void test4() {
+        assertEquals(2, p.solve1("xy"));
+        assertEquals(2, p.solve2("xy"));
+    }
+    @Test
+    public void test5() {
+        assertEquals(15, p.solve1("zzzzz"));
+        assertEquals(15, p.solve2("zzzzz"));
     }
 }
