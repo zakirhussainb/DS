de975ef2ed40e289c39f43a802d4bf18995cedfb
zakirhussainb <zakirhussainb2693@gmail.com>
2023-01-03 08:35:07 +0530

    LC 1750 Solved

--- src/main/java/com/zakcorp/leetcodemaster/Problem_1750.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_1750.java
@@ -1,21 +1,32 @@
 package com.zakcorp.leetcodemaster;
 
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
+import java.util.Stack;
 
 public class Problem_1750 {
     static class Solver {
-        public String solve1(String str, int key) {
-            int al = 26;
-            for(int i = 0; i < str.length(); i++) {
-                int pos = str.charAt(i) - 'a';
-                pos = pos % 26;
-                pos = pos + key;
-
+        public int solve1(String str) {
+            int n = str.length();
+            int i = 0, j = n - 1;
+            Stack<Character> st = new Stack<>();
+            while(i < j) {
+                if(!st.isEmpty()) {
+                    char stCh = st.pop();
+                    while (i < j && str.charAt( i ) == stCh) {
+                        i++;
+                    }
+                    while (j > i && str.charAt( j ) == stCh) {
+                        j--;
+                    }
+                }
+                if(str.charAt( i ) == str.charAt( j )) {
+                    st.push( str.charAt( i ) );
+                    i++;
+                    j--;
+                } else {
+                    break;
+                }
             }
-            return "";
+            return Math.max( j - i + 1, 0 );
         }
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_1750_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_1750_Test.java
@@ -9,6 +9,28 @@ public class Problem_1750_Test {
     Problem_1750.Solver p = new Problem_1750.Solver();
     @Test
     public void test1() {
-        assertEquals("zab", p.solve1("xyz", 2));
+        assertEquals(2, p.solve1("ca"));
+    }
+    @Test
+    public void test2() {
+        assertEquals(0, p.solve1("cabaabac"));
+    }
+    @Test
+    public void test3() {
+        assertEquals(3, p.solve1("aabccabba"));
+    }
+    @Test
+    public void test4() {
+        assertEquals(109, p.solve1("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbacccabbabccaccbacaaccacacccaccbbbacaabb" +
+                                   "ccbbcbcbcacacccccccbcbbabccaacaabacbbaccccbabbcbccccaccacaccbcbbcbcccabaaaabbbbbbbbbbbbbbb"));
+    }
+    @Test
+    public void test5() {
+        assertEquals(0, p.solve1("abbbbbbbbbbbbbbbbbbba"));
+    }
+
+    @Test
+    public void test6() {
+        assertEquals(0, p.solve1("bbbbbbbbbbbbbbbbbbb"));
     }
 }
