b9d2d8329aced3001ba1d7c2495bcad2fba26d55
zakirhussainb <zakirhussainb2693@gmail.com>
2023-01-03 08:42:23 +0530

    LC 1750 Updated

--- src/main/java/com/zakcorp/leetcodemaster/Problem_1750.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_1750.java
@@ -28,5 +28,20 @@ public class Problem_1750 {
             }
             return Math.max( j - i + 1, 0 );
         }
+
+        public int solve2(String str) {
+            int n = str.length();
+            int i = 0, j = n - 1;
+            while(i < j && str.charAt( i ) == str.charAt( j )) {
+                char ch = str.charAt( i ); // You don't need a stack here to remember the last character.
+                while(i <= j && str.charAt( i ) == ch) {
+                    i++;
+                }
+                while(i <= j && str.charAt(j) == ch) {
+                    j--;
+                }
+            }
+            return j - i + 1;
+        }
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/Problem_1750_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/Problem_1750_Test.java
@@ -10,27 +10,34 @@ public class Problem_1750_Test {
     @Test
     public void test1() {
         assertEquals(2, p.solve1("ca"));
+        assertEquals(2, p.solve2("ca"));
     }
     @Test
     public void test2() {
         assertEquals(0, p.solve1("cabaabac"));
+        assertEquals(0, p.solve2("cabaabac"));
     }
     @Test
     public void test3() {
         assertEquals(3, p.solve1("aabccabba"));
+        assertEquals(3, p.solve2("aabccabba"));
     }
     @Test
     public void test4() {
         assertEquals(109, p.solve1("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbacccabbabccaccbacaaccacacccaccbbbacaabb" +
                                    "ccbbcbcbcacacccccccbcbbabccaacaabacbbaccccbabbcbccccaccacaccbcbbcbcccabaaaabbbbbbbbbbbbbbb"));
+        assertEquals(109, p.solve2("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbacccabbabccaccbacaaccacacccaccbbbacaabb" +
+                                   "ccbbcbcbcacacccccccbcbbabccaacaabacbbaccccbabbcbccccaccacaccbcbbcbcccabaaaabbbbbbbbbbbbbbb"));
     }
     @Test
     public void test5() {
         assertEquals(0, p.solve1("abbbbbbbbbbbbbbbbbbba"));
+        assertEquals(0, p.solve2("abbbbbbbbbbbbbbbbbbba"));
     }
 
     @Test
     public void test6() {
         assertEquals(0, p.solve1("bbbbbbbbbbbbbbbbbbb"));
+        assertEquals(0, p.solve2("bbbbbbbbbbbbbbbbbbb"));
     }
 }
