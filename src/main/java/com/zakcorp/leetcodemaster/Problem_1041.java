45e21bd1241462e9e2333dd03753c1733bdd624e
zakirhussainb <zakirhussainb2693@gmail.com>
2023-01-24 08:08:40 +0530

    LC 1041 - Updated with 2nd sol and comments

--- src/main/java/com/zakcorp/leetcodemaster/Problem_1041.java
+++ src/main/java/com/zakcorp/leetcodemaster/Problem_1041.java
@@ -57,4 +57,34 @@ public class Problem_1041 {
             return initial[0] ==0 && initial[1] == 0 || currentDirection != 'N';
         }
     }
+
+    static class Solver1 {
+        public boolean solve1(String instructions) {
+            // we know that robot can move in 4 direction's
+            int[][] dir = new int[][]{ {0, 1}, {-1, 0}, {0, -1}, {1, 0} }; // Up[North], Left[West], Down[South], Right[East]
+            int index = 0; // we have to keep tracking those cases where it's not rotating.
+            // if in the end of execution, we see it's not rotating we will gonna return false;
+            int x = 0, y = 0; // it's origin
+
+            // let's write an for loop & iterate through the instructions. So, we keep 1 instructions at a time.
+            // And we will keep checking the position of the robot after executing particular instruction.
+            for(char ch : instructions.toCharArray()) {
+                // Now, we are checking is it a rotation
+                if(ch == 'L') { // If it's left then update it's head rotation
+                    index = (index + 1) % 4;
+                } else if(ch == 'R') {  // If it's right then update it's head rotation
+                    index = (index + 3) % 4;
+                } else { // We are checking it's direction
+                    x += dir[index][0];
+                    y += dir[index][1];
+                }
+            }
+            // LAST EDIT : for (x == 0 && y == 0) We are checking is our initial position from where we started and end position where we are finishing both are same!
+            // If so,  return true;
+
+            // If the robot perform 1 rotation, it means  "i != 0" then it will guaranteed perform a circle & after certain rotation it will come back to the origin.
+            // But if "i is  0" , it mean's robot is moving straight, hence there is no cycle. It will never come back to origin.
+            return x == 0 && y == 0 || index != 0;
+        }
+    }
 }
