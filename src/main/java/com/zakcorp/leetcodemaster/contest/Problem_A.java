1ff63a152df57aaa73f451b03358088c0442f148
zakirhussainb <zakirhussainb2693@gmail.com>
2023-02-13 18:37:43 +0530

    Test problem

--- src/main/java/com/zakcorp/leetcodemaster/contest/Problem_A.java
+++ src/main/java/com/zakcorp/leetcodemaster/contest/Problem_A.java
@@ -2,22 +2,23 @@ package com.zakcorp.leetcodemaster.contest;
 
 public class Problem_A {
     static class Solver {
-        public long solve1(int[] arr) {
-            int n = arr.length;
-            int l = 0, h = n - 1;
-            long sum = 0;
-            while(l <= h) {
-                String s;
-                if( l == h) {
-                    s = "" + arr[ l ];
-                } else {
-                    s = arr[ l ] + "" + arr[ h ];
+//        # Given an array of positive numbers and a positive number 'k', find the maximum sum of any contiguous subarray of size 'k'.
+//            # Input: [2, 1, 5, 1, 3, 2], k=3
+//            # Output: 9
+//            # Explanation: Subarray with maximum sum is [5, 1, 3].
+        public long solve1(int[] arr, int k) {
+            int start = 0;
+            int maxVal = -1;
+            int sum = 0;
+            for(int end = 0; end < arr.length; end++) {
+                sum += arr[end];
+                if(end - start + 1 >= k) {
+                    maxVal = Math.max(maxVal, sum);
+                    sum -= arr[start];
+                    start++;
                 }
-                sum += Integer.parseInt( s );
-                l++;
-                h--;
             }
-            return sum;
+            return maxVal;
         }
     }
 }
--- src/test/java/com/zakcorp/leetcodemaster/contest/Problem_A_Test.java
+++ src/test/java/com/zakcorp/leetcodemaster/contest/Problem_A_Test.java
@@ -8,14 +8,15 @@ public class Problem_A_Test {
     Problem_A.Solver p = new Problem_A.Solver();
     @Test
     public void test1() {
-        assertEquals(596, p.solve1( new int[] {7,52,2,4}));
+        assertEquals(9, p.solve1( new int[] {2, 1, 5, 1, 3, 2}, 3));
     }
     @Test
     public void test2() {
-        assertEquals(673, p.solve1( new int[] {5,14,13,8,12}));
+        assertEquals(39, p.solve1( new int[] {1, 4, 2, 10, 23, 3, 1, 0, 20}, 4));
     }
+
     @Test
     public void test3() {
-        assertEquals(5, p.solve1( new int[] {5}));
+        assertEquals(-1, p.solve1( new int[] {2, 3}, 3));
     }
 }
