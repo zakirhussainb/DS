b427573be8c185b4d29d76b2d90405a10b93e459
zakirhussainb <zakirhussainb2693@gmail.com>
2023-04-26 09:08:19 +0530

    DP - 7 - Memoization, Tabulation and Space Optimization

--- src/main/java/com/zakcorp/striver_series/dp/DP_7.java
+++ src/main/java/com/zakcorp/striver_series/dp/DP_7.java
@@ -32,4 +32,97 @@ public class DP_7
       return maxPoint;
     }
   }
+
+  static class Solver2 {
+    // Memoization Approach
+    public int maximumPoints(int[][] points) {
+      int days = points.length;
+      int lastPerformedTask = 3;
+      int[][] dp = new int[days][lastPerformedTask + 1];
+      maximumPoints( points, days - 1, lastPerformedTask, dp );
+      return dp[days - 1][lastPerformedTask];
+    }
+
+    private int maximumPoints( int[][] points, int days, int lastPerformedTask, int[][] dp ) {
+      if(days == 0) {
+        int maxPoint = 0;
+        for(int i = 0; i < 3; i++) {
+          if(i != lastPerformedTask) {
+            maxPoint = Math.max( maxPoint, points[days][i] );
+          }
+        }
+        dp[days][lastPerformedTask] = maxPoint;
+        return maxPoint;
+      }
+
+      if(dp[days][lastPerformedTask] != 0) {
+        return dp[days][lastPerformedTask];
+      }
+
+      int maxPoint = 0;
+      for(int i = 0; i < 3; i++) {
+        if(i != lastPerformedTask) {
+          int point = points[days][i] + maximumPoints( points, days - 1, i, dp );
+          maxPoint = Math.max( maxPoint, point );
+        }
+      }
+      dp[days][lastPerformedTask] = maxPoint;
+      return maxPoint;
+    }
+  }
+
+  static class Solver3 {
+    // Tabulation Approach
+    public int maximumPoints(int[][] points) {
+      int days = points.length;
+      int lastPerformedTask = 3;
+      int[][] dp = new int[days][lastPerformedTask + 1];
+      dp[0][0] = Math.max( points[0][1], points[0][2] );
+      dp[0][1] = Math.max( points[0][0], points[0][2] );
+      dp[0][2] = Math.max( points[0][0], points[0][1] );
+      dp[0][3] = Math.max( points[0][0], Math.max( points[0][1], points[0][2] ) );
+
+      for(int day = 1; day < days; day++) {
+        for(int lastTask = 0; lastTask < 4; lastTask++) {
+          dp[day][lastTask] = 0;
+          for(int task = 0; task <= 2; task++) {
+            if(task != lastTask) {
+              int point = points[day][task] + dp[day - 1][task];
+              dp[day][lastTask] = Math.max( dp[day][lastTask], point );
+            }
+          }
+        }
+      }
+      return dp[days - 1][3];
+    }
+  }
+
+  static class Solver4 {
+    // Space Optimization Approach
+    public int maximumPoints(int[][] points) {
+      int days = points.length;
+      int lastPerformedTask = 3;
+      int[] prev = new int[lastPerformedTask + 1];
+      prev[0] = Math.max( points[0][1], points[0][2] );
+      prev[1] = Math.max( points[0][0], points[0][2] );
+      prev[2] = Math.max( points[0][0], points[0][1] );
+      prev[3] = Math.max( points[0][0], Math.max( points[0][1], points[0][2] ) );
+
+      for(int day = 1; day < days; day++) {
+        int[] temp = new int[4];
+        for(int lastTask = 0; lastTask < 4; lastTask++) {
+          temp[lastTask] = 0;
+          for(int task = 0; task <= 2; task++) {
+            if(task != lastTask) {
+              int point = points[day][task] + prev[task];
+              temp[lastTask] = Math.max( temp[lastTask], point );
+            }
+          }
+        }
+        prev = temp;
+      }
+      return prev[3];
+    }
+
+  }
 }
--- src/test/java/com/zakcorp/striver_series/dp/DP_7_Test.java
+++ src/test/java/com/zakcorp/striver_series/dp/DP_7_Test.java
@@ -2,16 +2,22 @@ package com.zakcorp.striver_series.dp;
 
 import static org.junit.Assert.assertEquals;
 
-import com.zakcorp.striver_series.dp.DP_7.Solver1;
+import com.zakcorp.striver_series.dp.DP_7.*;
 
 import org.junit.Test;
 
 public class DP_7_Test
 {
   Solver1 p1 = new Solver1();
+  Solver2 p2 = new Solver2();
+  Solver3 p3 = new Solver3();
+  Solver4 p4 = new Solver4();
   @Test
   public void test1()
   {
-    assertEquals( 11, p1.maximumPoints( new int[][] { {1,2,5},{3,1,1},{3,3,3} } ) );
+//    assertEquals( 11, p1.maximumPoints( new int[][] { {1,2,5},{3,1,1},{3,3,3} } ) );
+//    assertEquals( 11, p2.maximumPoints( new int[][] { {1,2,5},{3,1,1},{3,3,3} } ) );
+//    assertEquals( 11, p3.maximumPoints( new int[][] { {1,2,5},{3,1,1},{3,3,3} } ) );
+    assertEquals( 11, p4.maximumPoints( new int[][] { {1,2,5},{3,1,1},{3,3,3} } ) );
   }
 }
