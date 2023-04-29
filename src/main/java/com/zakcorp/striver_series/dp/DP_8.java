571489f5073b03924fe6799858389166697b8457
zakirhussainb <zakirhussainb2693@gmail.com>
2023-04-29 17:58:40 +0530

    DP - 8 - Tabulation and Space Optmization

--- src/main/java/com/zakcorp/striver_series/dp/DP_8.java
+++ src/main/java/com/zakcorp/striver_series/dp/DP_8.java
@@ -42,4 +42,49 @@ public class DP_8
       return memo[r][c];
     }
   }
+  static class Solver3 {
+    // Tabulation approach
+    public int uniquePaths(int m, int n) {
+      int[][] dp = new int[m][n];
+      for(int r = 0; r < m; r++) {
+         for(int c = 0; c < n; c++) {
+          if(r == 0 && c == 0) {
+            dp[r][c] = 1;
+          } else {
+            int up = 0, left = 0;
+            if(r > 0)
+              up = dp[r - 1][c];
+            if(c > 0)
+              left = dp[r][c - 1];
+            dp[r][c] = up + left;
+          }
+        }
+      }
+      return dp[m - 1][n - 1];
+    }
+  }
+
+  static class Solver4 {
+    // Space Optimization approach
+    public int uniquePaths(int m, int n) {
+      int[] prev = new int[n];
+      for(int r = 0; r < m; r++) {
+        int[] curr = new int[n];
+        for(int c = 0; c < n; c++) {
+          if(r == 0 && c == 0) {
+            curr[c] = 1;
+          } else {
+            int up = 0, left = 0;
+            if(r > 0)
+              up = prev[c];
+            if(c > 0)
+              left = curr[c - 1];
+            curr[c] = up + left;
+          }
+        }
+        prev = curr;
+      }
+      return prev[n - 1];
+    }
+  }
 }
--- src/test/java/com/zakcorp/striver_series/dp/DP_8_Test.java
+++ src/test/java/com/zakcorp/striver_series/dp/DP_8_Test.java
@@ -10,12 +10,15 @@ public class DP_8_Test
 {
   Solver1 p1 = new Solver1();
   Solver2 p2 = new Solver2();
-//  Solver3 p3 = new Solver3();
-//  Solver4 p4 = new Solver4();
+  Solver3 p3 = new Solver3();
+  Solver4 p4 = new Solver4();
+
   @Test
   public void test1()
   {
     assertEquals( 28, p1.uniquePaths(3, 7) );
     assertEquals( 28, p2.uniquePaths(3, 7) );
+    assertEquals( 28, p3.uniquePaths(3, 7) );
+    assertEquals( 28, p4.uniquePaths(3, 7) );
   }
 }
