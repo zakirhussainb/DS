67058248e706ddb7b6b7c248358e51a57f6ceab7
zakirhussainb <zakirhussainb2693@gmail.com>
2023-04-30 15:33:28 +0530

    DP - 11 - Memoization

--- src/main/java/com/zakcorp/striver_series/dp/DP_11.java
+++ src/main/java/com/zakcorp/striver_series/dp/DP_11.java
@@ -1,5 +1,6 @@
 package com.zakcorp.striver_series.dp;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DP_11
@@ -26,5 +27,26 @@ public class DP_11
 
       return triangle.get( r ).get( c ) + Math.min( down, diag );
     }
+  }
+
+  static class Solver2 {
+    // Memoization Approach
+    public int minimumTotal(List<List<Integer>> triangle) {
+      int[][] memo = new int[triangle.size()][triangle.size()];
+      return findMinPathSum( 0, 0, triangle, memo );
+    }
+    private int findMinPathSum(int r, int c, List<List<Integer>> triangle, int[][] memo) {
+      if(r == triangle.size() - 1)
+        return triangle.get( r ).get( c );
+
+      if(memo[r][c] != 0)
+        return memo[r][c];
+
+      int down = findMinPathSum( r + 1, c, triangle, memo );
+      int diag = findMinPathSum( r + 1, c + 1, triangle, memo );
+      memo[r][c] = triangle.get( r ).get( c ) + Math.min( down, diag );
+      return memo[r][c];
+    }
+
   }
 }
--- src/test/java/com/zakcorp/striver_series/dp/DP_11_Test.java
+++ src/test/java/com/zakcorp/striver_series/dp/DP_11_Test.java
@@ -11,7 +11,7 @@ import org.junit.Test;
 public class DP_11_Test
 {
   Solver1 p1 = new Solver1();
-//  Solver2 p2 = new Solver2();
+  Solver2 p2 = new Solver2();
 //  Solver3 p3 = new Solver3();
 //  Solver4 p4 = new Solver4();
 
@@ -24,5 +24,6 @@ public class DP_11_Test
     input.add( Arrays.asList( 6, 5, 7 ) );
     input.add( Arrays.asList( 4, 1, 8, 3 ) );
     assertEquals( 11, p1.minimumTotal(input) );
+    assertEquals( 11, p2.minimumTotal(input) );
   }
 }
