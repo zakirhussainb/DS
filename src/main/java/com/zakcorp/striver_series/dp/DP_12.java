11b5c623695a0f3edacf2fd585e9ba031c1ed070
zakirhussainb <zakirhussainb2693@gmail.com>
2023-05-01 11:27:27 +0530

    DP - 12 - Memoization

--- src/main/java/com/zakcorp/striver_series/dp/DP_12.java
+++ src/main/java/com/zakcorp/striver_series/dp/DP_12.java
@@ -33,4 +33,35 @@ public class DP_12
       return matrix[r][c] + Math.min(down, Math.min( leftDiag, rightDiag));
     }
   }
+
+  static class Solver2 {
+    // Memoization approach
+    public int minimumTotal(int[][] matrix) {
+      int minP = Integer.MAX_VALUE;
+      int m = matrix.length;
+      int n = matrix[0].length;
+      int[][] memo = new int[m][n];
+      for(int c = 0; c < matrix[0].length; c++)
+        minP = Math.min( minP, findMinFallingPathSum( 0, c, matrix, memo ) );
+      return minP;
+    }
+    private int findMinFallingPathSum(int r, int c, int[][] matrix, int[][] memo) {
+      if(r >= matrix.length || r < 0 || c >= matrix[0].length || c < 0)
+        return Integer.MAX_VALUE;
+
+      if(r == matrix.length - 1)
+        return matrix[r][c];
+
+      if(memo[r][c] != 0)
+        return memo[r][c];
+
+      int down = findMinFallingPathSum( r + 1, c, matrix, memo );
+      int leftDiag = findMinFallingPathSum( r + 1, c - 1, matrix, memo );
+      int rightDiag = findMinFallingPathSum( r + 1, c + 1, matrix, memo );
+
+      memo[r][c] = matrix[r][c] + Math.min(down, Math.min( leftDiag, rightDiag));
+
+      return memo[r][c];
+    }
+  }
 }
--- src/test/java/com/zakcorp/striver_series/dp/DP_12_Test.java
+++ src/test/java/com/zakcorp/striver_series/dp/DP_12_Test.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 public class DP_12_Test
 {
   Solver1 p1 = new Solver1();
-//  Solver2 p2 = new Solver2();
+  Solver2 p2 = new Solver2();
 //  Solver3 p3 = new Solver3();
 //  Solver4 p4 = new Solver4();
 
@@ -17,11 +17,13 @@ public class DP_12_Test
   public void test1()
   {
     assertEquals( 13, p1.minimumTotal(new int[][]{ {2,1,3},{6,5,4},{7,8,9} }) );
+    assertEquals( 13, p2.minimumTotal(new int[][]{ {2,1,3},{6,5,4},{7,8,9} }) );
   }
 
   @Test
   public void test2()
   {
     assertEquals( -59, p1.minimumTotal(new int[][]{ {-19, 57},{-40, -5} }) );
+    assertEquals( -59, p2.minimumTotal(new int[][]{ {-19, 57},{-40, -5} }) );
   }
 }
