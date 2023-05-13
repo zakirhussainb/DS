f14e2a2c2cd3ef00da603e0bba53c1aa08d6af59
zakirhussainb <zakirhussainb2693@gmail.com>
2023-05-13 12:29:43 +0530

    DP - 14 Tabulation solution

--- src/main/java/com/zakcorp/striver_series/dp/DP_14.java
+++ src/main/java/com/zakcorp/striver_series/dp/DP_14.java
@@ -65,4 +65,35 @@ public class DP_14
       return take || notTake;
     }
   }
+
+  /*
+  Tabulation Approach
+  1. Figure out the changing states.
+  2. The no. of states represent the no. of nested loops
+  3. Seed the base cases into the dp array
+  3. Index will go from 1 to n - 1 since it is Bottom-Up
+      -> Target will go from 1 to target since it is Bottom-Up
+  4. Finally return dp[n - 1][sum]
+   */
+  static class Solver3 {
+    public boolean isSubsetSum(int[] arr, int sum) {
+      int n = arr.length;
+      boolean[][] dp = new boolean[n + 1][sum + 1];
+      for(int i = 0; i < n; i++)
+        dp[i][0] = true;
+      if(arr[0] <= sum)
+        dp[0][arr[0]] = true;
+      for(int ind = 1; ind < n; ind++) {
+        for(int target = 1; target <= sum; target++) {
+          boolean notTake = dp[ind - 1][target];
+          boolean take = false;
+          if(target >= arr[ind])
+            take = dp[ind - 1][target - arr[ind]];
+
+          dp[ind][target] = take || notTake;
+        }
+      }
+      return dp[n - 1][sum];
+    }
+  }
 }
--- src/test/java/com/zakcorp/striver_series/dp/DP_14_Test.java
+++ src/test/java/com/zakcorp/striver_series/dp/DP_14_Test.java
@@ -10,17 +10,19 @@ public class DP_14_Test
 {
   Solver1 p1 = new Solver1();
   Solver2 p2 = new Solver2();
-//  Solver3 p3 = new Solver3();
+  Solver3 p3 = new Solver3();
 //  Solver4 p4 = new Solver4();
   @Test
   public void test1() {
     assertTrue( p1.isSubsetSum( new int[]{3, 34, 4, 12, 5, 2}, 9 ) );
     assertTrue( p2.isSubsetSum( new int[]{3, 34, 4, 12, 5, 2}, 9 ) );
+    assertTrue( p3.isSubsetSum( new int[]{3, 34, 4, 12, 5, 2}, 9 ) );
   }
 
   @Test
   public void test2() {
     assertFalse( p1.isSubsetSum( new int[]{3, 34, 4, 12, 5, 2}, 30 ) );
     assertFalse( p2.isSubsetSum( new int[]{3, 34, 4, 12, 5, 2}, 30 ) );
+    assertFalse( p3.isSubsetSum( new int[]{3, 34, 4, 12, 5, 2}, 30 ) );
   }
 }
