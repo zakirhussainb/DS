b4d508dc306596ebda6060e2e1047f71d53eac60
zakirhussainb <zakirhussainb2693@gmail.com>
2023-05-29 19:50:17 +0530

    DP 23 - Recursive

---
+++ src/test/java/com/zakcorp/striver_series/dp/DP_23_Test.java
@@ -0,0 +1,19 @@
+package com.zakcorp.striver_series.dp;
+
+import static org.junit.Assert.assertEquals;
+
+import com.zakcorp.striver_series.dp.DP_23.*;
+
+import org.junit.Test;
+
+public class DP_23_Test
+{
+  Solver1 p1 = new Solver1();
+//  Solver2 p2 = new Solver2();
+//  Solver3 p3 = new Solver3();
+//  Solver4 p4 = new Solver4();
+  @Test
+  public void test1() {
+    assertEquals( 11, p1.unboundedKnapSack( 8, new int[] { 1, 3, 4, 5 }, new int[] { 1, 4, 5, 7 }, 4 ) );
+  }
+}
