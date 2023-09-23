e9837167505c3dcc66f6325d708592341f8bade7
zakirhussainb <zakirhussainb2693@gmail.com>
2023-09-23 23:38:21 +0530

    LC_2750 - Added new test cases

--- src/main/go/com/zakcorp/leetcodemaster/P_2750_test.go
+++ src/main/go/com/zakcorp/leetcodemaster/P_2750_test.go
@@ -11,3 +11,15 @@ func TestNumberOfGoodSubarraySplits_1(t *testing.T) {
 	AssertEqualsInt(t, numberOfGoodSubarraySplits_recursive(arr), 3, "Recursive method")
 	AssertEqualsInt(t, numberOfGoodSubarraySplits_memoized(arr), 3, "Memoized method")
 }
+
+func TestNumberOfGoodSubarraySplits_2(t *testing.T) {
+	arr := []int{0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1}
+	AssertEqualsInt(t, numberOfGoodSubarraySplits_recursive(arr), 8, "Recursive method")
+	AssertEqualsInt(t, numberOfGoodSubarraySplits_memoized(arr), 8, "Memoized method")
+}
+
+func TestNumberOfGoodSubarraySplits_3(t *testing.T) {
+	arr := []int{1}
+	AssertEqualsInt(t, numberOfGoodSubarraySplits_recursive(arr), 1, "Recursive method")
+	AssertEqualsInt(t, numberOfGoodSubarraySplits_memoized(arr), 1, "Memoized method")
+}
