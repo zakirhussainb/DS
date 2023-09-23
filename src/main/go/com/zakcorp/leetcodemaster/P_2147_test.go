f09d4167690c9aa0ff5c95b33747cd246fb1ee88
zakirhussainb <zakirhussainb2693@gmail.com>
2023-09-23 23:55:06 +0530

    LC_2147 - Added new test cases

--- src/main/go/com/zakcorp/leetcodemaster/P_2147_test.go
+++ src/main/go/com/zakcorp/leetcodemaster/P_2147_test.go
@@ -10,3 +10,18 @@ func TestNumberOfWays_1(t *testing.T) {
 	AssertEqualsInt(t, numberOfWays_recursive("SSPPSPS"), 3, "Recursive method")
 	AssertEqualsInt(t, numberOfWays_memoized("SSPPSPS"), 3, "Memoized method")
 }
+
+func TestNumberOfWays_2(t *testing.T) {
+	AssertEqualsInt(t, numberOfWays_recursive("PPSPSP"), 1, "Recursive method")
+	AssertEqualsInt(t, numberOfWays_memoized("PPSPSP"), 1, "Memoized method")
+}
+
+func TestNumberOfWays_3(t *testing.T) {
+	AssertEqualsInt(t, numberOfWays_recursive("S"), 0, "Recursive method")
+	AssertEqualsInt(t, numberOfWays_memoized("S"), 0, "Memoized method")
+}
+
+func TestNumberOfWays_4(t *testing.T) {
+	// AssertEqualsInt(t, numberOfWays_recursive("PPPPPSPPSPPSPPPSPPPPSPPPPSPPPPSPPSPPPSPSPPPSPSPPPSPSPPPSPSPPPPSPPPPSPPPSPPSPPPPSPSPPPPSPSPPPPSPSPPPSPPSPPPPSPSPSS"), 919999993, "Recursive method")
+	AssertEqualsInt(t, numberOfWays_memoized("PPPPPSPPSPPSPPPSPPPPSPPPPSPPPPSPPSPPPSPSPPPSPSPPPSPSPPPSPSPPPPSPPPPSPPPSPPSPPPPSPSPPPPSPSPPPPSPSPPPSPPSPPPPSPSPSS"), 919999993, "Memoized method")
+}
