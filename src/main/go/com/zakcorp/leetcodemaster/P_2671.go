844c95dadaf0cfbad7263d00a35003f7eed5697e
zakirhussainb <zakirhussainb2693@gmail.com>
2024-06-26 07:43:09 +0530

    TODO LC 2671

--- src/main/go/com/zakcorp/leetcodemaster/P_2671.go
+++ src/main/go/com/zakcorp/leetcodemaster/P_2671.go
@@ -1,50 +1,45 @@
 package leetcodemaster
 
-import (
-	"fmt"
-	"sort"
-)
+// type FrequencyTracker struct {
+// 	cnt  []int
+// 	freq []int
+// }
 
-type FrequencyTracker struct {
-	cnt  []int
-	freq []int
-}
+// func Constructor() FrequencyTracker {
+// 	return FrequencyTracker{cnt: make([]int, 100001), freq: make([]int, 100001)}
+// }
 
-func Constructor() FrequencyTracker {
-	return FrequencyTracker{cnt: make([]int, 100001), freq: make([]int, 100001)}
-}
+// func (this *FrequencyTracker) Add(number int) {
+// 	this.freq[this.cnt[number]]
+// }
 
-func (this *FrequencyTracker) Add(number int) {
-	this.freq[this.cnt[number]]
-}
+// func (this *FrequencyTracker) DeleteOne(number int) {
+// 	this.hashtable[number]--
+// }
 
-func (this *FrequencyTracker) DeleteOne(number int) {
-	this.hashtable[number]--
-}
+// func (this *FrequencyTracker) HasFrequency(frequency int) bool {
+// 	sort.Ints(this.hashtable)
+// 	result := this.BinarySearch(frequency)
+// 	return result != -1
+// }
 
-func (this *FrequencyTracker) HasFrequency(frequency int) bool {
-	sort.Ints(this.hashtable)
-	result := this.BinarySearch(frequency)
-	return result != -1
-}
-
-// BinarySearch performs a binary search on the sorted slice of the set.
-func (this *FrequencyTracker) BinarySearch(target int) int {
-	fmt.Println("hash...", this.hashtable)
-	low := 1
-	high := len(this.hashtable) - 1
-	for low <= high {
-		med := low + (high-low)/2
-		if this.hashtable[med] < target {
-			low = med + 1
-		} else if this.hashtable[med] > target {
-			high = med - 1
-		} else {
-			return med
-		}
-	}
-	return -1
-}
+// // BinarySearch performs a binary search on the sorted slice of the set.
+// func (this *FrequencyTracker) BinarySearch(target int) int {
+// 	fmt.Println("hash...", this.hashtable)
+// 	low := 1
+// 	high := len(this.hashtable) - 1
+// 	for low <= high {
+// 		med := low + (high-low)/2
+// 		if this.hashtable[med] < target {
+// 			low = med + 1
+// 		} else if this.hashtable[med] > target {
+// 			high = med - 1
+// 		} else {
+// 			return med
+// 		}
+// 	}
+// 	return -1
+// }
 
 /**
  * Your FrequencyTracker object will be instantiated and called as such:
--- src/main/go/com/zakcorp/leetcodemaster/P_2671_test.go
+++ src/main/go/com/zakcorp/leetcodemaster/P_2671_test.go
@@ -1,14 +1,12 @@
 package leetcodemaster
 
-import (
-	"testing"
+// import (
+// 	. "github.com/zakirhussainb/DS/src/main/go/com/zakcorp/utils"
+// )
 
-	. "github.com/zakirhussainb/DS/src/main/go/com/zakcorp/utils"
-)
-
-func TestFrequencyTracker_1(t *testing.T) {
-	obj := Constructor()
-	obj.Add(3)
-	obj.Add(3)
-	AssertTrue(t, obj.HasFrequency(2), "")
-}
+// func TestFrequencyTracker_1(t *testing.T) {
+// 	obj := Constructor()
+// 	obj.Add(3)
+// 	obj.Add(3)
+// 	AssertTrue(t, obj.HasFrequency(2), "")
+// }
