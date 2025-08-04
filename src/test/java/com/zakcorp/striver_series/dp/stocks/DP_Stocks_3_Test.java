a6e358d0a313f027fd71f43110459928c30544f0
zakirhussainb <zakirhussainb2693@gmail.com>
2025-08-05 01:14:46 +0530

    DP Stocks 3 - Tabulation

--- src/test/java/com/zakcorp/striver_series/dp/stocks/DP_Stocks_3_Test.java
+++ src/test/java/com/zakcorp/striver_series/dp/stocks/DP_Stocks_3_Test.java
@@ -7,9 +7,11 @@ import static org.junit.Assert.assertEquals;
 public class DP_Stocks_3_Test {
     DP_Stocks_3.Recursion p1 = new DP_Stocks_3.Recursion();
     DP_Stocks_3.Memoization p2 = new DP_Stocks_3.Memoization();
+    DP_Stocks_3.Tabulation p3 = new DP_Stocks_3.Tabulation();
     @Test
     public void test1() {
 //        assertEquals( 15, p1.solve1(new int[]{4, 2, 7, 1, 11, 5}, 6));
-        assertEquals( 15, p2.solve1(new int[]{4, 2, 7, 1, 11, 5}, 6));
+//        assertEquals( 15, p2.solve1(new int[]{4, 2, 7, 1, 11, 5}, 6));
+        assertEquals( 15, p3.solve1(new int[]{4, 2, 7, 1, 11, 5}, 6));
     }
 }
