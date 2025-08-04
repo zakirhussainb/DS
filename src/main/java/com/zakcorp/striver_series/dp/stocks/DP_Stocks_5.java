8f0e51516a596bdec9244ac9a2a6399794704f5c
zakirhussainb <zakirhussainb2693@gmail.com>
2025-08-05 02:10:38 +0530

    DP Stocks - 5 - Tabulation

--- src/main/java/com/zakcorp/striver_series/dp/stocks/DP_Stocks_5.java
+++ src/main/java/com/zakcorp/striver_series/dp/stocks/DP_Stocks_5.java
@@ -58,4 +58,28 @@ public class DP_Stocks_5 {
             return memo[day][buy];
         }
     }
+
+    static class Tabulation {
+        public int solve1(int[] arr, int n, int fee) {
+            int[][] dp = new int[n + 1][2];
+            for(int day = n - 1; day >= 0; day--) {
+                for(int buy = 0; buy < 2; buy++) {
+                    int op1;
+                    int op2;
+                    if(buy == 0) {
+                        op1 = dp[day + 1][0];
+                        op2 = (-1) * arr[day] + dp[day + 1][1];
+                    } else {
+                        op1 = dp[day + 1][1];
+                        op2 = arr[day] + dp[day + 1][0];
+
+                        op2 -= fee;
+                    }
+                    dp[day][buy] = Math.max(op1, op2);
+                }
+            }
+
+            return dp[0][0];
+        }
+    }
 }
