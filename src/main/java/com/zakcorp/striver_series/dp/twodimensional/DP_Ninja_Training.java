7a1dbddad381e518ae5240505330f612258f682d
zakirhussainb <zakirhussainb2693@gmail.com>
2025-08-06 00:23:28 +0530

    DP 2D Ninja Training - Memoization

--- src/main/java/com/zakcorp/striver_series/dp/twodimensional/DP_Ninja_Training.java
+++ src/main/java/com/zakcorp/striver_series/dp/twodimensional/DP_Ninja_Training.java
@@ -1,5 +1,7 @@
 package com.zakcorp.striver_series.dp.twodimensional;
 
+import java.util.Arrays;
+
 public class DP_Ninja_Training {
     static class Recursion {
         public int solve1(int[][] matrix) {
@@ -24,6 +26,43 @@ public class DP_Ninja_Training {
                 }
             }
             return matrix[day][act] + maxPoints;
+        }
+    }
+
+    static class Memoization {
+        public int solve1(int[][] matrix) {
+            int m = matrix.length;
+            int n = 3;
+            int maxVal = Integer.MIN_VALUE;
+            int[][] memo = new int[m][n + 1];
+            for(int i = 0; i < m; i++) {
+                Arrays.fill(memo[i], -1);
+            }
+            for(int act = 0; act < n; act++) {
+                int val = memoized(0, act, matrix, m, n, memo);
+                maxVal = Math.max(maxVal, val);
+            }
+            return maxVal;
+        }
+        private int memoized(int day, int act, int[][] matrix, int m, int n, int[][] memo) {
+            if(day >= m) {
+                return 0;
+            }
+
+            if(memo[day][act] != -1) {
+                return memo[day][act];
+            }
+
+            int maxPoints = 0;
+            for(int nextAct = 0; nextAct < n; nextAct++) {
+                if(nextAct != act) {
+                    maxPoints = Math.max(maxPoints, memoized(day + 1, nextAct, matrix, m, n, memo));
+                }
+            }
+
+            memo[day][act] = matrix[day][act] + maxPoints;
+
+            return memo[day][act];
         }
     }
 }
--- src/test/java/com/zakcorp/striver_series/dp/twodimensional/DP_Ninja_Training_Test.java
+++ src/test/java/com/zakcorp/striver_series/dp/twodimensional/DP_Ninja_Training_Test.java
@@ -6,8 +6,10 @@ import static org.junit.Assert.*;
 
 public class DP_Ninja_Training_Test {
     DP_Ninja_Training.Recursion p1 = new DP_Ninja_Training.Recursion();
+    DP_Ninja_Training.Memoization p2 = new DP_Ninja_Training.Memoization();
     @Test
     public void test1() {
         assertEquals(210, p1.solve1(new int[][]{{10, 40, 70}, {20, 50, 80}, {30, 60, 90}}));
+        assertEquals(210, p2.solve1(new int[][]{{10, 40, 70}, {20, 50, 80}, {30, 60, 90}}));
     }
 }
