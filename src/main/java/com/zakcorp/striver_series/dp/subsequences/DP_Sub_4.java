00fa93363b5812642ce9b387df5b9a209469825f
zakirhussainb <zakirhussainb2693@gmail.com>
2025-08-08 19:38:14 +0530

    DP Subsequences - 4 - Tabulation

--- src/main/java/com/zakcorp/striver_series/dp/subsequences/DP_Sub_4.java
+++ src/main/java/com/zakcorp/striver_series/dp/subsequences/DP_Sub_4.java
@@ -7,19 +7,22 @@ public class DP_Sub_4 {
     private static final int MOD = 1000_000_007;
     static class Recursion {
         public int solve1(int[] arr, int K) {
-            return recur(arr.length - 1, 0, arr, K);
+            return recur(arr.length - 1,arr, K);
         }
-        private int recur(int ind, int sum, int[] arr, int K) {
-            if(sum == K) {
+        private int recur(int ind, int[] arr, int K) {
+            if(K == 0) {
                 return 1;
             }
 
-            if(ind < 0 || sum > K) {
-                return 0;
+            if(ind == 0) {
+                return arr[0] == K ? 1 : 0;
             }
 
-            int pick = recur(ind - 1, sum + arr[ind], arr, K);
-            int notPick = recur(ind - 1, sum, arr, K);
+            int pick = 0;
+            if(arr[ind] <= K) {
+                pick = recur(ind - 1, arr, K - arr[ind]);
+            }
+            int notPick = recur(ind - 1, arr, K);
 
             return pick + notPick;
         }
@@ -27,31 +30,61 @@ public class DP_Sub_4 {
 
     static class Memoization {
         public int solve1(int[] arr, int K) {
-            int[][] memo = new int[arr.length][K];
+            int[][] memo = new int[arr.length][K + 1];
             for(int i = 0; i < arr.length; i++){
                 Arrays.fill(memo[i], -1);
             }
-            return memoized(arr.length - 1, 0, arr, K, memo);
+            return memoized(arr.length - 1,arr, K, memo);
         }
-        private int memoized(int ind, int sum, int[] arr, int K, int[][] memo) {
-            if(sum == K) {
+        private int memoized(int ind, int[] arr, int K, int[][] memo) {
+            if(K == 0) {
                 return 1;
             }
 
-            if(ind < 0 || sum > K) {
-                return 0;
+            if(ind == 0) {
+                return arr[0] == K ? 1 : 0;
             }
 
-            if(memo[ind][sum] != -1) {
-                return memo[ind][sum];
+            if(memo[ind][K] != -1) {
+                return memo[ind][K];
             }
 
-            int pick = memoized(ind - 1, sum + arr[ind], arr, K, memo);
-            int notPick = memoized(ind - 1, sum, arr, K, memo);
+            int pick = 0;
+            if(arr[ind] <= K) {
+                pick = memoized(ind - 1, arr, K - arr[ind], memo);
+            }
 
-            memo[ind][sum] = (pick + notPick) % MOD;
+            int notPick = memoized(ind - 1, arr, K, memo);
 
-            return memo[ind][sum];
+            memo[ind][K] = (pick + notPick) % MOD;
+
+            return memo[ind][K];
+        }
+    }
+
+    static class Tabulation {
+        public int solve1(int[] arr, int K) {
+            int n = arr.length;
+            int[][] dp = new int[n][K + 1];
+            for(int i = 0; i < n; i++) {
+                dp[i][0] = 1;
+            }
+            if(arr[0] <= K) {
+                dp[0][arr[0]] = 1;
+            }
+            for(int ind = 1; ind < n; ind++) {
+                for(int s = 0; s <= K; s++) {
+                    int pick = 0;
+                    if(arr[ind] <= s) {
+                        pick = dp[ind - 1][s - arr[ind]];
+                    }
+
+                    int notPick = dp[ind - 1][s];
+
+                    dp[ind][s] = (pick + notPick) % MOD;
+                }
+            }
+            return dp[n - 1][K];
         }
     }
 }
