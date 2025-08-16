675b9aa70c87c22c085b3d7bed57593b7bbd593e
zakirhussainb <zakirhussainb2693@gmail.com>
2025-08-16 19:44:55 +0530

    DP LIS - 1 - Corrected Recursion and Memoization

--- src/main/java/com/zakcorp/striver_series/dp/lis/DP_Lis_1.java
+++ src/main/java/com/zakcorp/striver_series/dp/lis/DP_Lis_1.java
@@ -1,6 +1,8 @@
 package com.zakcorp.striver_series.dp.lis;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class DP_Lis_1 {
     static class Recursion {
@@ -8,10 +10,7 @@ public class DP_Lis_1 {
             return recur(0, -1, arr);
         }
         private static int recur(int ind, int prevInd, int[] arr) {
-            if(ind == arr.length - 1) {
-                if(prevInd == -1 || arr[prevInd] < arr[ind]) {
-                    return 1;
-                }
+            if(ind == arr.length) {
                 return 0;
             }
 
@@ -31,33 +30,62 @@ public class DP_Lis_1 {
             int n = arr.length;
             int[][] memo = new int[n][n + 1];
             for(int i = 0; i < n; i++)
-                Arrays.fill(memo[i], Integer.MIN_VALUE);
+                Arrays.fill(memo[i], -1);
 
             return memoized(0, -1, arr, memo);
         }
         private static int memoized(int ind, int prevInd, int[] arr, int[][] memo) {
-            if(ind == arr.length - 1) {
-                if(prevInd == -1 || arr[prevInd] < arr[ind]) {
-                    return 1;
-                }
+            if(ind == arr.length) {
                 return 0;
             }
 
-            if(prevInd != -1 && memo[ind][prevInd] != Integer.MIN_VALUE) {
-                return memo[ind][prevInd];
+            if(memo[ind][prevInd + 1] != -1) {
+                return memo[ind][prevInd + 1];
             }
 
             int notPick = memoized(ind + 1, prevInd, arr, memo);
 
-            int pick = Integer.MIN_VALUE;
+            int pick = 0;
             if(prevInd == -1 || arr[prevInd] < arr[ind]) {
                 pick = memoized(ind + 1, ind, arr, memo) + 1;
             }
 
-            if (prevInd != -1)
-                memo[ind][prevInd] = Math.max(notPick, pick);
+            memo[ind][prevInd + 1] = Math.max(notPick, pick);
 
-            return Math.max(notPick, pick);
+            return memo[ind][prevInd + 1];
+        }
+    }
+
+    static class LIS_BinarySearch {
+        public int solve1(int[] arr) {
+            int n = arr.length;
+            List<Integer> temp = new ArrayList<>();
+            temp.add(arr[0]);
+            for(int i = 1; i < n; i++) {
+                if(arr[i] > temp.get(temp.size() - 1)) {
+                    temp.add(arr[i]);
+                } else {
+                    int ind = lowerBoundBS(temp, arr[i]);
+                    if (ind <= n)
+                        temp.set(ind, arr[i]);
+                }
+            }
+            return temp.size();
+        }
+        private static int lowerBoundBS(List<Integer> arr, int x) {
+            int n = arr.size();
+            int l = 0, h = n - 1;
+            int ans = n;
+            while(l <= h) {
+                int m = l + (h - l) / 2;
+                if(arr.get(m) >= x) {
+                    ans = m;
+                    h = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            }
+            return ans;
         }
     }
 }
--- src/test/java/com/zakcorp/striver_series/dp/lis/DP_Lis_1_Test.java
+++ src/test/java/com/zakcorp/striver_series/dp/lis/DP_Lis_1_Test.java
@@ -7,22 +7,26 @@ import static org.junit.Assert.*;
 public class DP_Lis_1_Test {
     DP_Lis_1.Recursion p1 = new DP_Lis_1.Recursion();
     DP_Lis_1.Memoization p2 = new DP_Lis_1.Memoization();
+    DP_Lis_1.LIS_BinarySearch p3 = new DP_Lis_1.LIS_BinarySearch();
 
     @Test
     public void test1() {
         assertEquals(4, p1.solve1(new int[]{10, 9, 2, 5, 3, 7, 101, 18}));
         assertEquals(4, p2.solve1(new int[]{10, 9, 2, 5, 3, 7, 101, 18}));
+        assertEquals(4, p3.solve1(new int[]{10, 9, 2, 5, 3, 7, 101, 18}));
     }
 
     @Test
     public void test2() {
         assertEquals(4, p1.solve1(new int[]{0, 1, 0, 3, 2, 3}));
         assertEquals(4, p2.solve1(new int[]{0, 1, 0, 3, 2, 3}));
+        assertEquals(4, p3.solve1(new int[]{0, 1, 0, 3, 2, 3}));
     }
 
     @Test
     public void test3() {
         assertEquals(1, p1.solve1(new int[]{7, 7, 7, 7, 7, 7, 7}));
         assertEquals(1, p2.solve1(new int[]{7, 7, 7, 7, 7, 7, 7}));
+        assertEquals(1, p3.solve1(new int[]{7, 7, 7, 7, 7, 7, 7}));
     }
 }
