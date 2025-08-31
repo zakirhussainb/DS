3c68185c024879357d6ef4d1d6d14378ad9e3fca
zakirhussainb <zakirhussainb2693@gmail.com>
2025-08-31 23:26:31 +0530

    DP Strings - 1 - Tabulation

--- src/main/java/com/zakcorp/striver_series/dp/strings/DP_Strings_1.java
+++ src/main/java/com/zakcorp/striver_series/dp/strings/DP_Strings_1.java
@@ -48,4 +48,32 @@ public class DP_Strings_1 {
             return memo[ind1][ind2];
         }
     }
+
+    static class Tabulation {
+        public int solve1(String str1, String str2) {
+            int m = str1.length();
+            int n = str2.length();
+
+            int[][] dp = new int[m + 1][n + 1];
+
+            /*for(int i = 0; i <= m; i++) {
+                dp[0][i] = 0;
+            }
+
+            for(int i = 0; i <= n; i++) {
+                dp[i][0] = 0;
+            }*/
+
+            for (int i = 1; i <= m; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                        dp[i][j] = 1 + dp[i - 1][j - 1];
+                    } else {
+                        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    }
+                }
+            }
+            return dp[m][n];
+        }
+    }
 }
--- src/test/java/com/zakcorp/striver_series/dp/strings/DP_Strings_1_Test.java
+++ src/test/java/com/zakcorp/striver_series/dp/strings/DP_Strings_1_Test.java
@@ -7,22 +7,26 @@ import static org.junit.Assert.*;
 public class DP_Strings_1_Test {
     DP_Strings_1.Recursion p1 = new DP_Strings_1.Recursion();
     DP_Strings_1.Memoization p2 = new DP_Strings_1.Memoization();
+    DP_Strings_1.Tabulation p3 = new DP_Strings_1.Tabulation();
 
     @Test
     public void test1() {
         assertEquals(3, p1.solve1("bdefg", "bfg"));
         assertEquals(3, p2.solve1("bdefg", "bfg"));
+        assertEquals(3, p3.solve1("bdefg", "bfg"));
     }
 
     @Test
     public void test2() {
         assertEquals(2, p1.solve1("mnop", "mnq"));
         assertEquals(2, p2.solve1("mnop", "mnq"));
+        assertEquals(2, p3.solve1("mnop", "mnq"));
     }
 
     @Test
     public void test3() {
         assertEquals(2, p1.solve1("abc", "dafb"));
         assertEquals(2, p2.solve1("abc", "dafb"));
+        assertEquals(2, p3.solve1("abc", "dafb"));
     }
 }
