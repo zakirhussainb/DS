c244a0220c7eb66948553419ceb0d1f329febe55
zakirhussainb <zakirhussainb2693@gmail.com>
2025-09-03 13:02:02 +0530

    DP Strings - 3 - Space Opt

--- src/main/java/com/zakcorp/striver_series/dp/strings/DP_Strings_3.java
+++ src/main/java/com/zakcorp/striver_series/dp/strings/DP_Strings_3.java
@@ -21,4 +21,28 @@ public class DP_Strings_3 {
             return dp[m][n];
         }
     }
+
+    static class SpaceOptimization {
+        public int solve1(String str1) {
+            StringBuilder sb = new StringBuilder(str1);
+            String str2 = sb.reverse().toString();
+
+            int m = str1.length(), n = str2.length();
+            int[] prev = new int[n + 1];
+            int[] curr = new int[n + 1];
+
+            for(int i = 1; i <= m; i++) {
+                for(int j = 1; j <= n; j++) {
+                    if(str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                        curr[j] = 1 + prev[j - 1];
+                    } else {
+                        curr[j] = Math.max(prev[j], curr[j - 1]);
+                    }
+                }
+                System.arraycopy(curr, 0, prev, 0, n + 1);
+            }
+
+            return curr[n];
+        }
+    }
 }
--- src/test/java/com/zakcorp/striver_series/dp/strings/DP_Strings_3_Test.java
+++ src/test/java/com/zakcorp/striver_series/dp/strings/DP_Strings_3_Test.java
@@ -6,24 +6,29 @@ import static org.junit.Assert.assertEquals;
 
 public class DP_Strings_3_Test {
     DP_Strings_3.Tabulation p1 = new DP_Strings_3.Tabulation();
+    DP_Strings_3.SpaceOptimization p2 = new DP_Strings_3.SpaceOptimization();
 
     @Test
     public void test1() {
         assertEquals(4, p1.solve1("eeeme"));
+        assertEquals(4, p2.solve1("eeeme"));
     }
 
     @Test
     public void test2() {
         assertEquals(2, p1.solve1("annb"));
+        assertEquals(2, p2.solve1("annb"));
     }
 
     @Test
     public void test3() {
         assertEquals(1, p1.solve1("s"));
+        assertEquals(1, p2.solve1("s"));
     }
 
     @Test
     public void test4() {
         assertEquals(7, p1.solve1("bbabcbcab"));
+        assertEquals(7, p2.solve1("bbabcbcab"));
     }
 }
