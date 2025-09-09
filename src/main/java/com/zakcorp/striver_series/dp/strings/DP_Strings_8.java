9987fd36fc336bf75d7dc6d1c25072407ab43a62
zakirhussainb <zakirhussainb2693@gmail.com>
2025-09-10 00:38:19 +0530

    DP Strings - 8 - Tabulation

--- src/main/java/com/zakcorp/striver_series/dp/strings/DP_Strings_8.java
+++ src/main/java/com/zakcorp/striver_series/dp/strings/DP_Strings_8.java
@@ -48,4 +48,29 @@ public class DP_Strings_8 {
             return memo[i][j];
         }
     }
+    static class Tabulation {
+        public int solve1(String start, String target) {
+            int m = start.length(), n = target.length();
+
+            int[][] dp = new int[m + 1][n + 1];
+            for(int i = 0; i <= m; i++) {
+                dp[i][0] = i;
+            }
+
+            for(int j = 0; j <= n; j++) {
+                dp[0][j] = j;
+            }
+
+            for(int i = 1; i <= m; i++) {
+                for(int j = 1; j <= n; j++) {
+                    if(start.charAt(i - 1) == target.charAt(j - 1)) {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    } else {
+                        dp[i][j] = 1 + Math.min(dp[i - 1][j], Math.min(dp[i - 1][j - 1], dp[i][j - 1]));
+                    }
+                }
+            }
+            return dp[m][n];
+        }
+    }
 }
--- src/test/java/com/zakcorp/striver_series/dp/strings/DP_Strings_8_Test.java
+++ src/test/java/com/zakcorp/striver_series/dp/strings/DP_Strings_8_Test.java
@@ -7,22 +7,26 @@ import static org.junit.Assert.assertEquals;
 public class DP_Strings_8_Test {
     DP_Strings_8.Recursion p1 = new DP_Strings_8.Recursion();
     DP_Strings_8.Memoization p2 = new DP_Strings_8.Memoization();
+    DP_Strings_8.Tabulation p3 = new DP_Strings_8.Tabulation();
 
     @Test
     public void test1() {
         assertEquals(2, p1.solve1("planet", "plan"));
         assertEquals(2, p2.solve1("planet", "plan"));
+        assertEquals(2, p3.solve1("planet", "plan"));
     }
 
     @Test
     public void test2() {
         assertEquals(4, p1.solve1("abcdefg", "azced"));
         assertEquals(4, p2.solve1("abcdefg", "azced"));
+        assertEquals(4, p3.solve1("abcdefg", "azced"));
     }
 
     @Test
     public void test3() {
         assertEquals(3, p1.solve1("saturday", "sunday"));
         assertEquals(3, p2.solve1("saturday", "sunday"));
+        assertEquals(3, p3.solve1("saturday", "sunday"));
     }
 }
